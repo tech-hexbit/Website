77ca3353555217d60717db2b3f83f0af904e3d2e
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-11 23:11:35 +0530

    Create RegisterHeader.jsx

---
+++ src/components/Register/RegisterHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// css
+import RHCss from "./Css/Register.module.css";
+
+export default function RegisterHeader() {
+  return <div className={RHCss.mDIv}>Register Here</div>;
+}
