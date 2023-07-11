5adfb8919e3493d871ce8430a184c60f9fbac7ed
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-11 23:11:16 +0530

    Create PrivacyContent.jsx

---
+++ src/components/privacy/PrivacyContent.jsx
@@ -0,0 +1,338 @@
+import styles from "./PrivacyContent.module.css";
+
+const PrivacyContent = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Privacy Policy</h1>
+
+      <p>
+        We are committed to safeguarding your privacy online. This Privacy
+        Policy applies to your information that we collect and receive on and
+        through this Site; it does not apply to practices of businesses that we
+        do not own or control or people we do not employ. At no time will we
+        sell or otherwise distribute your information or your
+        personally-identifiable data without your permission. By using this
+        Site, you agree to the terms of this Privacy Policy. Please read the
+        following Privacy Policy to understand how your personal information
+        will be treated as you use this Site and its services. The following
+        discloses our information gathering and dissemination practices.
+      </p>
+
+      <hr />
+
+      <h2>1. Collection of Information</h2>
+
+      <p>
+        a. Information including, but not limited to, User name, address, phone
+        number, fax number, email address, gender, date and/or year of birth and
+        user preferences ("Registration Information") may be collected at the
+        time of User registration on the Platform.
+      </p>
+
+      <p>
+        b. In connection with any communication or transaction and payment
+        services or any other services that you may avail using the Platform,
+        information, including but not limited to, bank account numbers, billing
+        and delivery information, credit/debit card numbers and expiration dates
+        and tracking information from cheques or money orders ("Account
+        Information") may be collected, among other things, to facilitate the
+        sale and purchase as well as the settlement of purchase price of the
+        products or services transacted on or procured through the Platform.
+      </p>
+
+      <p>
+        c. Hexbit record and retain details of Users’ activities on the
+        Platform. Information relating to communication or transactions
+        including, but not limited to, the types and specifications of the
+        goods, pricing and delivery information, any dispute records and any
+        information disclosed in any communication forum provided by us and/or
+        other affiliated companies of Hexbit (“Activities Information”) may be
+        collected as and when the communication and / or transactions are
+        conducted through the Platform.
+      </p>
+
+      <p>
+        d. Hexbit record and retain records of Users’ browsing or buying
+        activities on Platform including but not limited to IP addresses,
+        browsing patterns and User behavioural patterns. In addition, we gather
+        statistical information about the Platform and visitors to the Platform
+        including, but not limited to, IP addresses, browser software, operating
+        system, software and hardware attributes, pages viewed, number of
+        sessions and unique visitors (together "Browsing Information").
+      </p>
+
+      <p>
+        e. Registration Information, Account Information, Activities
+        Information, and Browsing Information are collectively referred to as
+        User Data.
+      </p>
+
+      <p>
+        f. It is mandatory for Users of the Platform to provide certain
+        categories of User Data (as specified at the time of collection). In the
+        event that Users do not provide any or sufficient User Data marked as
+        mandatory, Hexbit may not be able to complete the registration process
+        or provide such Users with Hexbit’s products or services.
+      </p>
+
+      <p>
+        g. When user download, install and use our Mobile Applications, we
+        automatically collect information on the type of device used, operating
+        system version, and the device identifier (or “UDID”). We use mobile
+        analytics software to allow Us to better understand the functionality of
+        our Mobile Software on your phone. This Software may record certain
+        information such as how often you use the application, the events that
+        occur within the application, aggregated usage, performance data, and
+        where the application was downloaded from. We do not link the
+        information we store within the analytics software to any personal data
+        user submit within the Mobile Applications.
+      </p>
+
+      <p>
+        h. We also collect clicks, scrolls, conversion and drop-off on our
+        Websites and Service(s) to render user journey at real-time. Subject to
+        this Notice, we will use such data and Service Data, including without
+        limitation, to (i) assess needs of your business to determine or suggest
+        suitable Service(s); (ii) send you requested information about the
+        Service(s); (iii) respond to customer service requests, questions and
+        concerns; and (iv) for any analytical purposes.
+      </p>
+
+      <h2>2. Use Of User Data</h2>
+
+      <p>a. Verification of User’s identity.</p>
+
+      <p>
+        b. Providing you with the Service(s), administer your account, assess
+        the needs of your business to or suggest suitable Service(s) and respond
+        to service requests, questions or concerns: we process your Personal
+        Data for these purposes to perform our contract with you or based on our
+        Legitimate Interest.
+      </p>
+
+      <p>
+        c. To facilitate communication between Users on the Platform and / or
+        processing Users transactions on the Platform.
+      </p>
+
+      <p>
+        d. To perform analytics, train our algorithms, improve, enhance,
+        develop, support and operate the Services and its availability, compile
+        statistical reports and record insights into usage patterns, develop new
+        products and services using machine learning technologies and more
+        generally to better serve you and be able to provide you with customized
+        content and features.
+      </p>
+
+      <p>
+        e. To send you promotional and marketing communications; to send you
+        communication from the Services(s). On the mobile app: to send receiving
+        push notifications about any events or promotions that we may be running
+        and/or update you about new features to our Service(s).
+      </p>
+
+      <p>
+        f. Facilitate your transactions with other users when you use our
+        Service(s).
+      </p>
+
+      <p>
+        g. To prevent or respond to any misuse of our Services or Applications
+        or any violations of our Terms and any applicable laws.
+      </p>
+
+      <p>
+        h. Based on our contract with you, we will process your personal data to
+        the extent necessary to collect your subscription fees.
+      </p>
+
+      <p>
+        i. Making such disclosures as may be required for any of the above
+        purposes or as required by law, regulations and guidelines or in respect
+        of any investigations, claims or potential claims brought on or against
+        us or against third parties.
+      </p>
+
+      <p>
+        j. Hexbit.ai collects location data to enable LIVE tracking of order and
+        delivery agent even when the app is closed or not in use by the delivery
+        agent.
+      </p>
+
+      <h2>3. Disclosure Of User Data</h2>
+
+      <p>
+        a. User further agrees that Hexbit may disclose and transfer User Data
+        to third party service providers (including but not limited to data
+        entry, database management, promotions, products and services alerts,
+        delivery services, payment extension services, authentication and
+        verification services and logistics services) ("Service Providers").
+        These Service Providers are under a duty of confidentiality to Hexbit
+        and are only permitted to use User Data in connection with the purposes
+        specified in clause 2 herein above.
+      </p>
+
+      <p>
+        b. User(s) agree that Hexbit may disclose and transfer User Data to
+        Hexbit’s affiliated companies and/or their designated Service Providers.
+      </p>
+
+      <p>
+        c. When necessary Hexbit may also disclose and transfer User Data to our
+        professional advisers, law enforcement agencies, insurers, government
+        and regulatory and other organizations.
+      </p>
+
+      <p>
+        d. Any User Data supplied by User will be retained by Hexbit and will be
+        accessible by our employees, any Service Providers engaged by Hexbit and
+        third parties referred to in clause 3 herein, for or in relation to any
+        of the purposes stated in Clause 2 herein above.
+      </p>
+
+      <p>
+        e. All voluntary information uploaded by you on the Platform (including
+        without limitation information about your products, images, remarks,
+        feedbacks etc. ) may be made publicly available on the Platform and
+        therefore accessible by any internet user. Any voluntary information
+        that User disclose to Hexbit becomes public information and User
+        relinquishes any proprietary rights (including but not limited to the
+        rights of confidentiality and copyright) in such information. User
+        should exercise caution when deciding to include personal or proprietary
+        information in the voluntary information that User submits to Hexbit or
+        uploads on the Platform.
+      </p>
+
+      <p>
+        f. Hexbit may share User Data with third parties, including without
+        limitation, banks, financial institutions, credit agencies, or vendors
+        to enable such third parties to offer their products or services to such
+        Users. While Hexbit shall endeavour to have in place internal procedures
+        to keep User Data secure from intruders, there is no guarantee that such
+        measures/procedures can eliminate all of the risks of theft, loss or
+        misuse.
+      </p>
+
+      <p>
+        g. Hexbit may establish relationships with other parties and websites to
+        offer User the benefit of products and services which Hexbit does not
+        offer. Hexbit may offer you access to these other parties and/or their
+        websites. This Privacy Policy does not apply to the products and
+        services enabled or facilitated by such third parties. The privacy
+        policies of those other parties may differ from Hexbit, and Hexbit has
+        no control over the information that User may submit to those third
+        parties. User should read the relevant privacy policy for those third
+        parties before responding to and availing any offers, products or
+        services advertised or provided by those third parties.
+      </p>
+
+      <p>
+        h. Hexbit mat share the user data if we are involved in a merger,
+        reorganization or other fundamental corporate change with a third party,
+        or sell/buy a business unit to/from a third party, or if all or a
+        portion of our business, assets or stock are acquired by a third party,
+        with such third party including at the due diligence stage. In
+        accordance with applicable laws, we will use reasonable efforts to
+        notify you of any transfer of Personal Data to unaffiliated third party.
+      </p>
+
+      <p>
+        i. Hexbit.ai collects location data to enable LIVE tracking of order and
+        delivery agent even when the app is closed or not in use by the delivery
+        agent.
+      </p>
+
+      <h2>4. Cookies And Similar Technology</h2>
+
+      <p>
+        We and our third party advertising partners use cookies and similar
+        technologies in analyzing trends, administering the Website, tracking
+        users’ movements around the Websites, and gathering demographic
+        information about our user base as a whole. We may receive reports based
+        on the use of these technologies by these companies on an individual and
+        aggregated basis. Most web browsers support cookies and users can
+        control the use of cookies at the individual browser level. Please note
+        that if you choose to disable cookies, it may limit your use of certain
+        features or functions on our Websites and services.
+      </p>
+      <p>
+        As is true of most websites, we gather certain information automatically
+        and store it in log files. This information may include internet
+        protocol (IP) addresses, browser type, internet service provider (ISP),
+        referring/exit pages, the files viewed on our Websites (e.g., HTML
+        pages, graphics, etc.), operating system, date/time stamp, and/or
+        clickstream data. We link this automatically Collected data to other
+        data we collect about you.
+      </p>
+      <p>
+        User can determine if and how a cookie will be accepted by configuring
+        the browser which is installed in User’s device. If User choose, User
+        can change those configurations. If User reject all cookies by choosing
+        the cookie-disabling function, User may be required to re-enter
+        information on the Platform more often and certain features of the
+        Platform may be unavailable.
+      </p>
+
+      <h2>5. Security Measures</h2>
+      <p>
+        We use appropriate technical and organizational measures to protect the
+        User Data that we collect and process. The measures we use are designed
+        to provide a level of security appropriate to the risk of processing
+        User Data. No data transmission over the internet or any wireless
+        network can be guaranteed to be perfectly secure. As a result, while
+        Hexbit tries to protect the information it holds, it cannot guarantee
+        the security of any information the User transmits to Hexbit and Users
+        do so at their own risk.
+      </p>
+
+      <h2>6. Other Communications</h2>
+      <p>
+        If you are our Customer, we will send you announcements, email or in
+        product notifications related to the Service(s) on occasions when it is
+        necessary to do so. For instance, if our Service(s) is temporarily
+        suspended for maintenance, we might send you an e-mail.{" "}
+      </p>
+
+      <h2>7. Children’s Personal Data</h2>
+
+      <p>
+        Hexbit does not knowingly collect any Personal Data from children under
+        the age of 18. If you are under the age of 18, please do not submit any
+        Personal Data through our Websites or Service(s). We encourage parents
+        and legal guardians to monitor their children’s Internet usage and to
+        help enforce this Notice by instructing their children never to provide
+        Personal Data through our Service(s) or Websites without their
+        permission. If you have reason to believe that a child under the age of
+        18 has provided Personal Data to us through our Websites or Service(s),
+        please contact us and we will endeavor to delete that information and
+        terminate the child's account from our databases.{" "}
+      </p>
+
+      <h2>8. Amendments</h2>
+
+      <p>
+        Any changes to this Privacy Policy will be communicated by us posting an
+        amended and restated Privacy Policy on the Platform. Once posted on the
+        Platform the new Privacy Policy will be effective immediately. Your
+        continued use of the Platform shall be deemed to be your acceptance to
+        the provisions of the Privacy Policy. User agree that any information
+        Hexbit hold about User (as described in this Privacy Policy and whether
+        or not collected prior to or after the new Privacy Policy became
+        effective) will be governed by the latest version of the Privacy Policy.
+        You can choose to discontinue use of the Websites or Service(s), if you
+        do not accept the terms of this Notice, or any modified version of this
+        Notice.
+      </p>
+
+      <h2>9. Grievance Officer</h2>
+
+      <p>
+        In accordance with Information Technology Act 2000 and rules made there
+        under, the name and contact details of the Grievance Officer are
+        provided below:
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyContent;
