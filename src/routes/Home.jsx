346f9a68746cfcffc4c2dedacb977fd45bc4c4af
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-11 23:12:01 +0530

    Create Home.jsx

---
+++ src/routes/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+// components
+import Header from "./../components/Home/Header";
+import WhoAreWe from "../components/Home/WhoAreWe";
+import WhyUs from "./../components/Home/WhyUs";
+import Modern from "./../components/Home/Modern";
+
+export default function Home() {
+  // scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>HexBit.io - Home</title>
+      </Helmet>
+      <div>
+        <Header />
+        <WhoAreWe />
+        <WhyUs />
+      </div>
+    </>
+  );
+}
