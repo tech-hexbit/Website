fdfc7b60f7e80adfd4c3911caed74bec8bdf2c9c
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-07-27 08:10:52 +0530

    register footer

--- src/components/footer/RegisterFooter.jsx
+++ src/components/footer/RegisterFooter.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 // css
 import FCss from "./Css/RegisterFooter.module.css";
 
-import image from "../../assets/Ellipse86.png";
+import image from "../../assets/RegisterFooter.png";
 
 export default function RegisterFooter() {
   return (
