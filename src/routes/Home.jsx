ad2b9807a635a4597503b922d75b612286ec4372
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-07-31 22:03:36 +0530

    hamburger

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,7 +6,6 @@ import Header from "./../components/Home/Header";
 import WhoAreWe from "../components/Home/WhoAreWe";
 import WhyUs from "./../components/Home/WhyUs";
 import Modern from "./../components/Home/Modern";
-import MobileHeader from "../components/header/MobileHeader";
 
 export default function Home() {
   // scroll to top
@@ -21,7 +20,6 @@ export default function Home() {
       </Helmet>
       <div>
         <Header />
-        <MobileHeader />
         <WhoAreWe />
         <WhyUs />
       </div>
