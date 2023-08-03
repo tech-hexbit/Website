6a3dcd65b2077fe1d97d962f57dff5c3fd348f73
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-03 20:11:19 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,7 @@ import AboutUs from "./../components/About/AboutUs";
 import Team from "../components/About/Team";
 import Business from "../components/About/Business";
 
-import image from "../assets/AboutUs/HEXBIT.png";
+import image from "./../assets/AboutUS/HEXBIT.png";
 
 export default function About() {
   // scroll to top
