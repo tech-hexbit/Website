dff408d267c13de18ec760db8f2011556cdaffe6
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-08-10 23:35:16 +0530

    register

---
+++ src/components/Register/Form1.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import FCss from "./Css/Form.module.css";
+
+export default function Form1(props) {
+  return (
+    <div className={FCss.mainDiv}>
+      <div className={FCss.top}>
+        <div className={FCss.head}>
+          Welcome to <span>Hexbit</span> !
+        </div>
+        <div className={FCss.subHead}>
+          Create your account and start selling with Hexbit
+        </div>
+      </div>
+      <div className={FCss.form}>
+        <div className={FCss.phoneInput}>
+          <div className={FCss.formInputs}>
+            <label htmlFor="phone">Phone</label>
+            <input type="number" id="phone" placeholder="Enter phone no." />
+          </div>
+          <div className={FCss.otp}>
+            <div className={FCss.otpText}>
+              <input type="text" name="" id="" />
+            </div>
+            <div className={FCss.otpButton}>
+              <button>Send otp</button>
+            </div>
+          </div>
+        </div>
+        <div className={FCss.formInputs}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter the email" />
+        </div>
+        <div className={FCss.formInputs}>
+          <label htmlFor="pass">Password</label>
+          <input type="password" id="pass" placeholder="Enter your password" />
+        </div>
+        <div className={FCss.formInputs}>
+          <label htmlFor="busName">Business Name</label>
+          <input type="text" id="busName" placeholder="Your business name" />
+        </div>
+        <div className={FCss.formInputs}>
+          <label htmlFor="license">Importer License</label>
+          <input type="text" id="license" placeholder="Importer License no." />
+        </div>
+        <div className={FCss.formInputs}>
+          <label htmlFor="gstin">GSTIN</label>
+          <input type="text" id="gstin" placeholder="Your GSTIN" />
+        </div>
+      </div>
+      <div className={FCss.button}>
+        <div></div>
+        <div>
+          <button
+            onClick={() => {
+              props.setCount(2);
+            }}
+          >
+            {props.button}{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21"
+              height="10"
+              viewBox="0 0 21 10"
+              fill="none"
+            >
+              <path
+                d="M1.81791 5.97787L16.9094 5.9877L14.0445 8.32675C13.577 8.71004 13.577 9.32919 14.0445 9.71252C14.512 10.0958 15.2792 10.0958 15.7466 9.71252L20.6493 5.69288C21.1168 5.30958 21.1168 4.69044 20.6493 4.30711L15.7466 0.287469C15.2791 -0.095823 14.5119 -0.095823 14.0445 0.287469C13.577 0.670761 13.577 1.28991 14.0445 1.67324L16.9094 4.02217L1.81791 4.01234C1.15862 4.01234 0.619141 4.45461 0.619141 4.9952C0.619141 5.53578 1.15857 5.97805 1.81791 5.97805V5.97787Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Register/RegisterMain.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+// css
+import RFCss from "./Css/Register.module.css";
+
+import GrowCard from "./GrowCard";
+import Form1 from "./Form1";
+import Form2 from "./Form2";
+import icon1 from "../../assets/register/icon1.png";
+import icon2 from "../../assets/register/icon2.png";
+import icon3 from "../../assets/register/icon3.png";
+import icon4 from "../../assets/register/icon4.png";
+
+export default function RegisterMain() {
+  const [count, setCount] = useState(1);
+  return (
+    <div className={RFCss.mainDiv}>
+      <div className={RFCss.left}>
+        <div className={RFCss.heading}>
+          “<span>Welcome</span> To a Revolutionary Full-Stack{" "}
+          <span>Seller Solution.</span>”
+        </div>
+        <div className={RFCss.subHead}>
+          Grow your business with new customers on the <b>Hexbit Ecommerce</b>{" "}
+          network
+        </div>
+        <div className={RFCss.points}>
+          <div>
+            <GrowCard
+              image={icon1}
+              heading="MORE BUYERS"
+              subHead="Access to the entire buyer universe"
+            />
+          </div>
+          <div>
+            <GrowCard
+              image={icon2}
+              heading="ZERO COST"
+              subHead="No upfront cost to get started"
+            />
+          </div>
+          <div>
+            <GrowCard
+              image={icon3}
+              heading="EASY TO USE"
+              subHead="Start selling in no time"
+            />
+          </div>
+          <div>
+            <GrowCard
+              image={icon4}
+              heading="GROW TOUR BUSINESS"
+              subHead="Promote your business with your own webpage"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={RFCss.right}>
+        {count == 1 && <Form1 button="Next" setCount={setCount} />}
+        {count == 2 && <Form2 button="Select Product Categories" />}
+      </div>
+    </div>
+  );
+}
