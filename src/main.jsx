6bf8d98babccf6c23f18822722aac6fb2090d1cd
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-16 01:31:45 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// store
 import { AuthContextProvider } from "./store/auth-context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
