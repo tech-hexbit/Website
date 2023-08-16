85acc6107bebad2905af9683d104d480557964e0
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-16 12:17:27 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
+// import { Blurhash } from "react-blurhash";
 
 // Components
 import Team from "../components/About/Team";
 import AboutUs from "./../components/About/AboutUs";
 import Business from "../components/About/Business";
+import Blur from "./../components/About/Blur/ImageComponent";
 
 // img
 import image from "./../assets/AboutUS/HEXBIT.png";
@@ -17,6 +19,7 @@ export default function About() {
   return (
     <div>
       <img src={image} alt="" width="100%" />
+      <Blur src={image} blur={props.mem.blur} />
       <AboutUs />
       <Team />
       <Business />
