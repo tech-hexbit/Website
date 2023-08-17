b19e91c5a3cf0799c0319d1acfd73591339b6da6
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-17 16:34:29 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 // import { Blurhash } from "react-blurhash";
 
 // Components
@@ -6,23 +6,37 @@ import Team from "../components/About/Team";
 import AboutUs from "./../components/About/AboutUs";
 import Business from "../components/About/Business";
 import Blur from "./../components/About/Blur/ImageComponent";
+import LoadingPage from "./../MicroInteraction/Loading";
 
 // img
 import image from "./../assets/AboutUS/HEXBIT.png";
 
 export default function About() {
+  const [show, load] = useState(true);
   // scroll to top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  useEffect(() => {
+    console.log(show);
+  }, [show]);
+
   return (
-    <div>
+    <>
+      {show ? "" : ""}
+      {/* {show ? (
+        <LoadingPage />
+      ) : (
+        <> */}
       {/* <img src={image} alt="" width="100%" /> */}
-      <Blur src={image} blur="LHL;gVRl9FtQIBt8WBWB_KobWBRj" />
+      <Blur src={image} blur="LHL;gVRl9FtQIBt8WBWB_KobWBRj" load={load} />
       <AboutUs />
       <Team />
       <Business />
-    </div>
+      {/* </>
+      )} */}
+      {}
+    </>
   );
 }
