be150cc74ccec1df10ecb7e9b3b8f8f5f00284ca
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-18 08:18:46 +0530

    Update ImageComponent.jsx

--- src/components/About/Blur/ImageComponent.jsx
+++ src/components/About/Blur/ImageComponent.jsx
@@ -2,40 +2,48 @@ import React, { useState, useEffect } from "react";
 import { Blurhash } from "react-blurhash";
 
 import "./ImageComponent.css";
+import LoadingPage from "./../../../MicroInteraction/Loading";
 
-export default function ImageComponent({ src, blur, load, show }) {
+export default function ImageComponent({ src, blur }) {
+  // const [show, load] = useState(true);
   const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     const img = new Image();
 
     img.onload = () => {
-      imageLoaded ? load(false) : "";
+      // load(false);
       setImageLoaded(true);
     };
     img.src = src;
   }, [src]);
 
-  useEffect(() => {
-    console.log(show + " <- ImageComponent");
-  }, [show]);
+  // useEffect(() => {
+  //   console.log(show + " <- ImageComponent");
+  // }, [show]);
 
-  useEffect(() => {
-    console.log(imageLoaded + " <- imageLoaded");
-    imageLoaded ? "True" : "False";
-  }, [imageLoaded]);
+  // useEffect(() => {
+  //   console.log(imageLoaded + " <- imageLoaded");
+  //   imageLoaded ? "True" : "False";
+  // }, [imageLoaded]);
 
   return (
     <>
-      <div style={{ display: imageLoaded ? "none" : "inline" }}>
-        <Blurhash
+      <div
+        style={{
+          display: imageLoaded ? "none" : "inline",
+          aspectRatio: "3/1.75",
+        }}
+      >
+        {/* <Blurhash
           hash={blur}
           width={1800}
           height={1052}
           resolutionX={32}
           resolutionY={32}
           punch={1}
-        />
+        /> */}
+        <LoadingPage />
       </div>
       <img
         src={src}
