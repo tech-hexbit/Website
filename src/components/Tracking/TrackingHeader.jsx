3017750c7667d4912ffa190329584ea829d209ee
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-23 17:16:50 +0530

    Update TrackingHeader.jsx

--- src/components/Tracking/TrackingHeader.jsx
+++ src/components/Tracking/TrackingHeader.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+// css
+import THCss from "./Css/TrackingHeader.module.css";
+
 export default function TrackingHeader(props) {
-  return <div>Order ID : {props.id}</div>;
+  return (
+    <>
+      <p className={THCss.titlePTag}>
+        Order ID :<span>{props.id}</span>
+      </p>
+    </>
+  );
 }
