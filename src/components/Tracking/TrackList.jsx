984dbb52afd813a3ca257ff543e1af4d075172b3
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-23 19:00:29 +0530

    Update TrackList.jsx

--- src/components/Tracking/TrackList.jsx
+++ src/components/Tracking/TrackList.jsx
@@ -6,7 +6,10 @@ import THCss from "./Css/TrackingHeader.module.css";
 export default function TrackList(props) {
   return (
     <div className={THCss.listmDiv}>
-      <div className={THCss.circleDiv}></div>
+      <div className={THCss.logoDiv}>
+        <div className={THCss.circleDiv}></div>
+        <div className={THCss.lineDiv}></div>
+      </div>
       <div className={THCss.allContent}>
         <p className={THCss.title}>{props.title}</p>
         <p className={THCss.des}>{props.des}</p>
