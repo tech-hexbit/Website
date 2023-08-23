d2fc61db12c972a87624bf6726afbf5440779c5c
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-24 00:28:24 +0530

    Update Founders.jsx

--- src/components/About/Founders.jsx
+++ src/components/About/Founders.jsx
@@ -5,8 +5,13 @@ import TeamCss from "./Css/Founders.module.css";
 import Title from "../Home/Title";
 import TeamMemCard from "./TeamMemCard";
 
+// img
 import image1 from "../../assets/AboutUS/Team/AnujPrakash.png";
 import image2 from "../../assets/AboutUS/Team/TathagatChoudhary.png";
+import Timg1 from "./../../assets/AboutUS/Team/tathagatgr1.png";
+import Timg2 from "./../../assets/AboutUS/Team/tathagatgr2.png";
+import Aimg1 from "./../../assets/AboutUS/Team/anujgr1.png";
+import Aimg2 from "./../../assets/AboutUS/Team/anujgr2.png";
 
 export default function Team() {
   return (
@@ -17,11 +22,15 @@ export default function Team() {
           image={image1}
           name="Anuj Prakash"
           designation="CEO & Co-founder"
+          img1={Aimg1}
+          img2={Aimg2}
         />
         <TeamMemCard
           image={image2}
           name="Tathagat Choudhary"
           designation="COO & Co-founder"
+          img1={Timg1}
+          img2={Timg2}
         />
       </div>
     </div>
