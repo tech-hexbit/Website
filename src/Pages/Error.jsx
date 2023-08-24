d86f836e7c73f4725001656a1107c0f488f4a643
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-24 21:30:56 +0530

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useRouteError } from "react-router-dom";
 
 const Error = () => {
   // scroll to top
@@ -7,18 +6,7 @@ const Error = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  const error = useRouteError();
-  console.error(error);
-
-  return (
-    <div>
-      <h1>Oops</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  return <div>Error 404</div>;
 };
 
 export default Error;
