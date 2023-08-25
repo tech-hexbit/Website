e461cd3887d24ba4b1c61a6cc16825f0e2ef48eb
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-25 10:26:03 +0530

    Update AddProduct.jsx

--- src/Pages/User/AddProduct.jsx
+++ src/Pages/User/AddProduct.jsx
@@ -1,6 +1,8 @@
-// import React from "react";
+import React from "react";
 
 import UserSideBar from "../../components/userLoggedIn/UserSideBar";
+
+import SCss from "./css/sales.module.css";
 
 export default function AddProduct() {
   return (
