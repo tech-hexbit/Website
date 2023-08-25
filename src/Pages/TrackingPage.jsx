0078f1760fac1324f27d2ea973269a3e55220df9
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-25 11:07:58 +0530

    Update TrackingPage.jsx

--- src/Pages/TrackingPage.jsx
+++ src/Pages/TrackingPage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 //components
-import Header from "./../components/Tracking/TrackingHeader";
+import TrackingHeader from "./../components/Tracking/TrackingHeader";
 
 export default function TrackingPage() {
   const { id } = useParams();
   return (
     <div>
-      <Header id={id} />
+      <TrackingHeader id={id} />
     </div>
   );
 }
