4ecec3a53e592907021d06d2be79042558b4e267
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-28 23:07:22 +0530

    Create Profile.jsx

---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// components
+import UserSideBar from "../../components/userLoggedIn/UserSideBar";
+
+export default function Profile() {
+  return <div>Profile</div>;
+}
