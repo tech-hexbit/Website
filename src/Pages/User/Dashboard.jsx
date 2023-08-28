c5f295e9b349e3fa05d3949f28460e7511e25329
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-29 01:05:05 +0530

    Update Dashboard.jsx

--- src/Pages/User/Dashboard.jsx
+++ src/Pages/User/Dashboard.jsx
@@ -1,4 +1,5 @@
-// import React from "react";
+import React, { useEffect } from "react";
+
 import UserSideBar from "../../components/userLoggedIn/UserSideBar";
 import BestSellers from "../../components/userLoggedIn/dashboard/BestSellers";
 import Description from "../../components/userLoggedIn/dashboard/Description";
@@ -6,6 +7,10 @@ import RecentOrders from "../../components/userLoggedIn/dashboard/RecentOrders";
 import Revenue from "../../components/userLoggedIn/dashboard/Revenue";
 
 export default function Dashboard() {
+  // scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div style={{ display: "flex", backgroundColor: "#F7F8FA" }}>
       <UserSideBar />
