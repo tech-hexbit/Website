ad3b97453ea95f3f0e2f37c5cc564b4e0eced8aa
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-30 17:13:44 +0530

    Update Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
 // components
 import UserSideBar from "./../components/userLoggedIn/UserSideBar";
@@ -8,15 +9,18 @@ import UserSideBar from "./../components/userLoggedIn/UserSideBar";
 import PCss from "./Css/Profile.module.css";
 
 export default function Profile() {
+  const [currTab, setCurrTab] = useState("me");
+
   // scroll to top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
     <div className={PCss.mDiv}>
-      <UserSideBar />
+      <UserSideBar currTab={currTab} setCurrTab={setCurrTab} />
       <div className={PCss.CDiv}>
-        <Header />
+        <Routes></Routes>
+        {/* <Header /> */}
       </div>
     </div>
   );
