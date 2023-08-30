69650a1d3d723dbd4bfed6e01526f99797d0cb13
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-30 17:28:39 +0530

    Create Doughnut.jsx

---
+++ src/components/Dashboard/DashboardCom/charts/Doughnut.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+import "chart.js/auto";
+import { Doughnut } from "react-chartjs-2";
+
+export default function DoughnutChart() {
+  const data = {
+    labels: ["Direct", "Google", "Apple"],
+    datasets: [
+      {
+        data: [50, 25, 25],
+        backgroundColor: ["#7E22CE", "#D8B4FE", "#DBEAFE"],
+      },
+    ],
+  };
+
+  const chartOptions = {
+    cutout: "95%",
+  };
+
+  return (
+    <div>
+      <Doughnut data={data} options={chartOptions} />
+    </div>
+  );
+}
