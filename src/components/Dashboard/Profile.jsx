71138e32b90260f96996e4b9d0f76930fbeb999e
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-30 17:28:51 +0530

    Update Profile.jsx

--- src/components/Dashboard/Profile.jsx
+++ src/components/Dashboard/Profile.jsx
@@ -11,27 +11,3 @@ export default function Profile() {
     </>
   );
 }
-
-// import React, { useEffect } from "react";
-
-// // components
-// import UserSideBar from "./../components/userLoggedIn/UserSideBar";
-// import Header from "./../components/Profile/Header";
-
-// // Css
-// import PCss from "./Css/Profile.module.css";
-
-// export default function Profile() {
-//   // scroll to top
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, []);
-//   return (
-//     <div className={PCss.mDiv}>
-//       <UserSideBar />
-//       <div className={PCss.CDiv}>
-//         <Header />
-//       </div>
-//     </div>
-//   );
-// }
