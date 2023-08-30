b0721acd025bdd7bea96313d2ad310f7a8d521f5
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-30 17:33:39 +0530

    Update DashboardMain.jsx

--- src/components/Dashboard/DashboardMain.jsx
+++ src/components/Dashboard/DashboardMain.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+// components
+import Revenue from "./DashboardCom/Revenue";
+import Description from "./DashboardCom/Description";
+import BestSellers from "./DashboardCom/BestSellers";
+import RecentOrders from "./DashboardCom/RecentOrders";
+
 export default function DashboardMain() {
-  return <div>DashboardMain</div>;
+  return (
+    <>
+      <Description />
+      <Revenue />
+      <RecentOrders />
+      <BestSellers />
+    </>
+  );
 }
