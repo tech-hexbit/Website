d2d82191f352b4dae945f2f0b035ff9d650edbf3
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-31 02:25:03 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 
 // css
@@ -15,50 +15,54 @@ const Header = () => {
   const redirect = useNavigate();
 
   return (
-    <div className={styles.mDiv}>
-      <div className={styles.mContDiv}>
-        <Link to="/" className="LinkStyle">
-          <div className={styles.titleP}>
-            {/* HEXBIT.io */}
-            <img src={imgLogo} alt="" className={styles.imgLogo} />
-          </div>
-        </Link>
+    <>
+      <div className={styles.mDiv}>
+        <div className={styles.mContDiv}>
+          <Link to="/" className="LinkStyle">
+            <div className={styles.titleP}>
+              {/* HEXBIT.io */}
+              <img src={imgLogo} alt="" className={styles.imgLogo} />
+            </div>
+          </Link>
 
-        <div className={styles.rightDiv}>
-          <NavLink to="/" className="LinkStyle">
-            Home
-          </NavLink>
-          <NavLink to="/AboutUs" className="LinkStyle">
-            <p>About Us</p>
-          </NavLink>
-          <NavLink to="/contact" className="LinkStyle">
-            <p>Contact Us</p>
-          </NavLink>
-          {authCtx.isLoggedIn ? (
-            <>
-              <NavLink to="/me" className="LinkStyle">
-                <img
-                  src={authCtx.user.image}
-                  alt=""
-                  className={styles.imgAuth}
-                />
-              </NavLink>
-            </>
-          ) : (
-            <>
-              <NavLink to="/signIn" className="LinkStyle">
-                <p>Login</p>
-              </NavLink>
-              <NavLink to="/register" className="LinkStyle">
-                <p className={styles.registerPTag}>
-                  <b>Register for free</b>
-                </p>
-              </NavLink>
-            </>
-          )}
+          <div className={styles.rightDiv}>
+            <NavLink to="/" className="LinkStyle">
+              Home
+            </NavLink>
+            <NavLink to="/AboutUs" className="LinkStyle">
+              <p>About Us</p>
+            </NavLink>
+            <NavLink to="/contact" className="LinkStyle">
+              <p>Contact Us</p>
+            </NavLink>
+            {authCtx.isLoggedIn ? (
+              <>
+                <NavLink to="/me" className="LinkStyle">
+                  <img
+                    src={authCtx.user.image}
+                    alt=""
+                    className={styles.imgAuth}
+                  />
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink to="/signIn" className="LinkStyle">
+                  <p>Login</p>
+                </NavLink>
+                <NavLink to="/register" className="LinkStyle">
+                  <p className={styles.registerPTag}>
+                    <b>Register for free</b>
+                  </p>
+                </NavLink>
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className={styles.replaceDiv}></div>
+    </>
   );
 };
 
