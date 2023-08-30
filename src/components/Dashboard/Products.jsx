447876021f4e125a53d1b4c2c9ff8dd8c22567c4
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-30 17:50:02 +0530

    Update Products.jsx

--- src/components/Dashboard/Products.jsx
+++ src/components/Dashboard/Products.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+// components
+import Display from "./product/Display";
+import Filter from "./product/Filter";
+
+// css
+import pdtCSS from "./Css/products.module.css";
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <Display />
+      <Filter />
+    </>
+  );
 }
