b8d583cfe28f9823427a851d9d77a1acacd46307
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-30 17:33:41 +0530

    Update Profile.jsx

--- src/components/Dashboard/Profile.jsx
+++ src/components/Dashboard/Profile.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 
 // components
-import Header from "./../components/Profile/Header";
 import Header from "./Profile/Header";
 
 export default function Profile() {
