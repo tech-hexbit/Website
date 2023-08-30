c203e12d6eeacef6013e4cc905c2361afc93822d
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-31 02:00:30 +0530

    Update Description.jsx

--- src/components/Dashboard/DashboardCom/Description.jsx
+++ src/components/Dashboard/DashboardCom/Description.jsx
@@ -45,9 +45,6 @@ export default function Description() {
             </div>
           </div>
         </div>
-        {/* <div className={DCss.button}>
-          <button>+ Add product</button>
-        </div> */}
       </div>
       <div className={DCss.bottom}>
         {data.map((element, i) => {
