b2e16d7c03f7841367f997ffb6d0e3d64c7f33c7
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-30 16:50:39 +0530

    Update Dashboard.jsx

--- src/Pages/User/Dashboard.jsx
+++ src/Pages/User/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
-// components
-import UserSideBar from "../../components/userLoggedIn/UserSideBar";
-import BestSellers from "../../components/userLoggedIn/dashboard/BestSellers";
-import Description from "../../components/userLoggedIn/dashboard/Description";
-import RecentOrders from "../../components/userLoggedIn/dashboard/RecentOrders";
-import Revenue from "../../components/userLoggedIn/dashboard/Revenue";
+// // components
+// import UserSideBar from "../../components/userLoggedIn/UserSideBar";
+// import BestSellers from "../../components/userLoggedIn/dashboard/BestSellers";
+// import Description from "../../components/userLoggedIn/dashboard/Description";
+// import RecentOrders from "../../components/userLoggedIn/dashboard/RecentOrders";
+// import Revenue from "../../components/userLoggedIn/dashboard/Revenue";
+
+import DashboardMain from "./../../components/Dashboard/DashboardMain";
+import Categories from "./../../components/Dashboard/Categories";
 
 // Css
 import PCss from "./../Css/Profile.module.css";
