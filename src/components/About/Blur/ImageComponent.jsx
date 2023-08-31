21cc2ae7c7b14256bcb38d818d7567d695302010
Anshuman <21052232@kiit.ac.in>
2023-09-01 03:02:19 +0530

    responive-ans

--- src/components/About/Blur/ImageComponent.jsx
+++ src/components/About/Blur/ImageComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Blurhash } from "react-blurhash";
+// import { Blurhash } from "react-blurhash";
 
 import "./ImageComponent.css";
 import LoadingPage from "./../../../MicroInteraction/Loading";
