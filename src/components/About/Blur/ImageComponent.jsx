b3132f69ba016a9dfbaf4f025dceb50e07b5ce2b
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-31 16:55:36 +0530

    Update ImageComponent.jsx

--- src/components/About/Blur/ImageComponent.jsx
+++ src/components/About/Blur/ImageComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Blurhash } from "react-blurhash";
 
-import "./ImageComponent.css";
+import "./Css/ImageComponent.css";
 import LoadingPage from "./../../../MicroInteraction/Loading";
 
 export default function ImageComponent({ src, blur }) {
