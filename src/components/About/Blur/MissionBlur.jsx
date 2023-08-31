00bbede82a42b91c6a67e7f46fa73bd8918e3371
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-31 17:04:28 +0530

    Update MissionBlur.jsx

--- src/components/About/Blur/MissionBlur.jsx
+++ src/components/About/Blur/MissionBlur.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Blurhash } from "react-blurhash";
 
-export default function MissionBlur() {
-  return <div>MissionBlur</div>;
+export default function MissionBlur({ src, blur }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+
+    img.onload = () => {
+      setImageLoaded(true);
+    };
+    img.src = src;
+  }, [src]);
+
+  return (
+    <>
+      <div
+        style={{
+          display: imageLoaded ? "none" : "inline",
+        }}
+      >
+        <Blurhash
+          hash={blur}
+          width={471}
+          height={619.625}
+          resolutionX={32}
+          resolutionY={32}
+          punch={1}
+        />
+      </div>
+      <img
+        src={src}
+        alt=""
+        className="memImg"
+        style={{ display: !imageLoaded ? "none" : "inline" }}
+      />
+    </>
+  );
 }
+
+// import "./css/Core.css";
+
+// export default function ImageComponent() {
+
+//   return (
+//     <>
+
+//     </>
+//   );
+// }
