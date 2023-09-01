ef7bb19c162cbaef04239d6ab035ec21231b6afe
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-02 04:41:15 +0530

    Update Form.jsx

--- src/components/Dashboard/AddProduct/Form.jsx
+++ src/components/Dashboard/AddProduct/Form.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 
 // css
-import FCss from "Form.module.css";
+import FCss from "./Css/Form.module.css";
 
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <div className={FCss.mDiv}>
+      <div className={FCss.left}>
+        <div>
+          <p className={FCss.label}>Product title</p>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter product title"
+            className={FCss.inp}
+          />
+        </div>
+      </div>
+      <div className={FCss.right}></div>
+    </div>
+  );
 }
