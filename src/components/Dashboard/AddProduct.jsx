f22c2708327f39bb4d0b02986a01dc1abb86db5b
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-02 03:57:49 +0530

    Update AddProduct.jsx

--- src/components/Dashboard/AddProduct.jsx
+++ src/components/Dashboard/AddProduct.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+// css
+import ApCss from "./Css/AddProduct.module.css";
+
 export default function AddProduct() {
-  return <div>AddProduct</div>;
+  return (
+    <div className={ApCss.mDiv}>
+      <p className={ApCss.AddHPTag}>Add Product</p>
+    </div>
+  );
 }
