9999a21c393cb4839904880f89c12734d8ed751c
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-03 17:10:37 +0530

    Update Description.jsx

--- src/components/Dashboard/DashboardCom/Description.jsx
+++ src/components/Dashboard/DashboardCom/Description.jsx
@@ -54,7 +54,7 @@ export default function Description() {
 
       if (response.data.success) {
         setOrderDel({
-          totalOrders: response.data.totalOrder,
+          totalOrders: response.data.totalOrders,
           totalAmount: response.data.totalAmount,
           totalNewCustomers: response.data.totalNewCustomers,
         });
@@ -65,6 +65,10 @@ export default function Description() {
       console.log(e);
     }
   };
+
+  useEffect(() => {
+    console.log(orderDel);
+  }, [orderDel]);
 
   return (
     <div className={DCss.mainDiv}>
@@ -81,17 +85,31 @@ export default function Description() {
       </div>
 
       <div className={DCss.bottom}>
-        {data.map((element, i) => {
-          return (
-            <DesCard
-              key={i}
-              heading={element.cardHeading}
-              value={element.cardValue}
-              change={element.changeInValue}
-              arrow={element.arrow}
-            />
-          );
-        })}
+        {/* {data.map((element, i) => { */}
+        {/* return ( */}
+        <DesCard
+          // key={i}
+          heading="Total orders"
+          value={orderDel.totalOrders}
+          change="+5.21%"
+          arrow="increase"
+        />
+        <DesCard
+          // key={i}
+          heading="Total Earnings"
+          value={orderDel.totalAmount}
+          change="+5.21%"
+          arrow="decrease"
+        />
+        <DesCard
+          // key={i}
+          heading="New customers"
+          value={orderDel.totalNewCustomers}
+          change="+5.21%"
+          arrow="increase"
+        />
+        {/* ); */}
+        {/* })} */}
       </div>
     </div>
   );
