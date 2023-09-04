b9097644f8a119e7e1fbbb11c7e7ff1147b12a69
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-05 03:35:23 +0530

    Update DashboardMain.jsx

--- src/components/Dashboard/DashboardMain.jsx
+++ src/components/Dashboard/DashboardMain.jsx
@@ -6,9 +6,6 @@ import Description from "./DashboardCom/Description";
 import BestSellers from "./DashboardCom/BestSellers";
 import RecentOrders from "./DashboardCom/RecentOrders";
 
-// css
-import DBCss from "./Css/DashB.module.css";
-
 export default function DashboardMain() {
   const [defaultSet, setDefaultSet] = useState("RecentOrders");
 
