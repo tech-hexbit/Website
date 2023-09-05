d3c7a759b8da6cc080528ad3a19075348837212d
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-06 00:03:31 +0530

    Update BestSellers.jsx

--- src/components/Dashboard/DashboardCom/BestSellers.jsx
+++ src/components/Dashboard/DashboardCom/BestSellers.jsx
@@ -94,64 +94,68 @@ export default function BestSellers() {
         </div>
       ) : (
         <>
-          <div className={BSCss.table}>
-            <table className={BSCss.tableTag}>
-              <tr>
-                <th id={BSCss.th} className={BSCss.product}>
-                  Product
-                </th>
-                <th id={BSCss.th} className={BSCss.amount}>
-                  Amount
-                </th>
-                <th id={BSCss.th} className={BSCss.sales}>
-                  Sales
-                </th>
-                <th id={BSCss.th} className={BSCss.stock}>
-                  Stock
-                </th>
-                <th id={BSCss.th}>Ratings</th>
-              </tr>
+          {orderDel?.length > 0 ? (
+            <div className={BSCss.table}>
+              <table className={BSCss.tableTag}>
+                <tr>
+                  <th id={BSCss.th} className={BSCss.product}>
+                    Product
+                  </th>
+                  <th id={BSCss.th} className={BSCss.amount}>
+                    Amount
+                  </th>
+                  <th id={BSCss.th} className={BSCss.sales}>
+                    Sales
+                  </th>
+                  <th id={BSCss.th} className={BSCss.stock}>
+                    Stock
+                  </th>
+                  <th id={BSCss.th}>Ratings</th>
+                </tr>
 
-              {data.map((element, i) => {
-                return (
-                  <tr key={i}>
-                    <td data-cell="Product" id={BSCss.td} className="prod">
-                      <div className={BSCss.col1}>
-                        <div className={BSCss.image}>
-                          <img src={element.col1Image} />
+                {data.map((element, i) => {
+                  return (
+                    <tr key={i}>
+                      <td data-cell="Product" id={BSCss.td} className="prod">
+                        <div className={BSCss.col1}>
+                          <div className={BSCss.image}>
+                            <img src={element.col1Image} />
+                          </div>
+                          <div>{element.col1Text}</div>
                         </div>
-                        <div>{element.col1Text}</div>
-                      </div>
-                    </td>
-                    <td data-cell="Amount" id={BSCss.td}>
-                      {element.col2}
-                    </td>
-                    <td data-cell="Sales" id={BSCss.td}>
-                      {element.col3}
-                    </td>
-                    <td data-cell="Stock" id={BSCss.td} className="stock">
-                      <div>
-                        <div>{element.col4Top}</div>
-                        {element.col4Bottom == "Instock" && (
-                          <div style={{ color: "#4BB543" }}>
-                            {element.col4Bottom}
-                          </div>
-                        )}
-                        {element.col4Bottom == "Out of stock" && (
-                          <div style={{ color: "#D0342C" }}>
-                            {element.col4Bottom}
-                          </div>
-                        )}
-                      </div>
-                    </td>
-                    <td data-cell="Ratings" id={BSCss.td}>
-                      {element.col5}
-                    </td>
-                  </tr>
-                );
-              })}
-            </table>
-          </div>
+                      </td>
+                      <td data-cell="Amount" id={BSCss.td}>
+                        {element.col2}
+                      </td>
+                      <td data-cell="Sales" id={BSCss.td}>
+                        {element.col3}
+                      </td>
+                      <td data-cell="Stock" id={BSCss.td} className="stock">
+                        <div>
+                          <div>{element.col4Top}</div>
+                          {element.col4Bottom == "Instock" && (
+                            <div style={{ color: "#4BB543" }}>
+                              {element.col4Bottom}
+                            </div>
+                          )}
+                          {element.col4Bottom == "Out of stock" && (
+                            <div style={{ color: "#D0342C" }}>
+                              {element.col4Bottom}
+                            </div>
+                          )}
+                        </div>
+                      </td>
+                      <td data-cell="Ratings" id={BSCss.td}>
+                        {element.col5}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </table>
+            </div>
+          ) : (
+            <p className={RCss.NoOrders}>No Orders</p>
+          )}
         </>
       )}
     </div>
