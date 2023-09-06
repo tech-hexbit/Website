84d887dc0aec755911e1586264799b651eb0745c
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-06 20:36:36 +0530

    Update CategoriesTabs.jsx

--- src/components/Dashboard/Categories/CategoriesTabs.jsx
+++ src/components/Dashboard/Categories/CategoriesTabs.jsx
@@ -4,5 +4,15 @@ import React from "react";
 import CCss from "./Css/Categories.module.css";
 
 export default function CategoriesTabs(props) {
-  return <div className={CCss.nameDiv}>{props.name}</div>;
+  return (
+    <div
+      className={CCss.nameDiv}
+      id={props.current === props.name ? "activeTabC" : ""}
+      onClick={() => {
+        props.setCurrent(props.name);
+      }}
+    >
+      {props.name}
+    </div>
+  );
 }
