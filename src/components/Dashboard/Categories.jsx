dc9b03052bc14baf05510a5716bb89cfbd6020bc
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-06 20:12:53 +0530

    Update Categories.jsx

--- src/components/Dashboard/Categories.jsx
+++ src/components/Dashboard/Categories.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+// components
+import CategoriesTabs from "./Categories/CategoriesTabs";
+
 export default function Categories() {
-  return <div>Categories</div>;
+  return (
+    <div>
+      <CategoriesTabs name="Ecommerce" />
+      <CategoriesTabs name="Service providers" />
+      <CategoriesTabs name="Education" />
+      <CategoriesTabs name="Food" />
+      <CategoriesTabs name="Automobile" />
+    </div>
+  );
 }
