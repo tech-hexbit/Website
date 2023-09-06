f8cb834710a28c014b87c7d16e7e417e10e30027
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-06 20:47:43 +0530

    Update Gateway.jsx

--- src/components/Dashboard/Gateway.jsx
+++ src/components/Dashboard/Gateway.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+// img
+import d from "./../../assets/test/Group 427320714.png";
+
 export default function Gateway() {
-  return <div>Gateway</div>;
+  return (
+    <div>
+      <img src={d} alt="" className="test" />
+    </div>
+  );
 }
