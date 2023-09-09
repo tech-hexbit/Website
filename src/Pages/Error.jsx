4924849e08fe8260a1c79a272a09b32143cb3ece
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-09 12:31:29 +0530

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,13 +3,21 @@ import React, { useEffect } from "react";
 // css
 import EC from "./Css/Error.module.css";
 
+// img
+import Svg from "./../assets/error/barista_at0v.svg";
+
 const Error = () => {
   // scroll to top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return <div className={EC.mDiv}>Error 404</div>;
+  return (
+    <div className={EC.mDiv}>
+      <img src={Svg} alt="" srcset="" className={EC.imgDiv} />
+      <p className={EC.pTag}>Error 404</p>
+    </div>
+  );
 };
 
 export default Error;
