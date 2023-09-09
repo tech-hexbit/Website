09d53646173b4e0af7a101b6d4f4b1ef6bcabfe4
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-09 12:23:13 +0530

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 
+// css
+import EC from "./Css/Error.module.css";
+
 const Error = () => {
   // scroll to top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return <div>Error 404</div>;
+  return <div className="">Error 404</div>;
 };
 
 export default Error;
