4dd514846da70e370079e64d317ea871065f36f5
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-13 19:41:28 +0530

    Update Form.jsx

--- src/components/Dashboard/AddProduct/Form.jsx
+++ src/components/Dashboard/AddProduct/Form.jsx
@@ -216,38 +216,40 @@ export default function Form() {
 
           {/* Service */}
           <>
-            <p className={FCss.labelMainArrowPTag}>
+            <p className={FCss.labelMainArrowPTag} onClick={}>
               <b>Service</b>
 
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-chevron-down"
-              >
-                <path d="m6 9 6 6 6-6" />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-chevron-up"
-              >
-                <path d="m18 15-6-6-6 6" />
-              </svg>
+              {ServiceOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-chevron-down"
+                >
+                  <path d="m6 9 6 6 6-6" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-chevron-up"
+                >
+                  <path d="m18 15-6-6-6 6" />
+                </svg>
+              )}
             </p>
 
             {ServiceOpen ? (
