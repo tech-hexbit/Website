d55904722f24e7260897a3db76d034c54f1682ea
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-21 12:01:18 +0530

    Update ProductsPage.jsx

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -8,8 +8,14 @@ export default function ProductsPage() {
     <div className={PPCss.mDiv}>
       <p className={PPCss.AddHPTag}>Product Details</p>
 
-      <div className={PPCss.AddHPTag}>
-        <div></div>
+      <div className={PPCss.divDiv}>
+        <div className={PPCss.leftDiv}>
+          <img
+            src="https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1965&q=80"
+            alt=""
+            className={PPCss.productImg}
+          />
+        </div>
         <div></div>
       </div>
     </div>
