f14650ae09e5daa6c1b091dc8c9acc62f878c585
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-21 13:12:37 +0530

    Update Des.jsx

--- src/components/Product/Des.jsx
+++ src/components/Product/Des.jsx
@@ -4,5 +4,16 @@ import React from "react";
 import DCss from "./Css/Des.module.css";
 
 export default function Des() {
-  return <div>Des</div>;
+  return (
+    <>
+      <div className={DCss.mDiv}>
+        <p className={DCss.subTitlePTag}>Description :</p>
+        <p className={DCss.desDPTag}>
+          Midsole:- Core Instant Step In Comfort Long Lasting Reponsive
+          Cushionig And Super Plush Feel. Outsole :- The Rubber Outsole With
+          Perfect Grip And Durability.
+        </p>
+      </div>
+    </>
+  );
 }
