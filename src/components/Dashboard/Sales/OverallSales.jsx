a77bb9b16a2f6e85a242d337adaf60738132f693
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-21 20:44:29 +0530

    Update OverallSales.jsx

--- src/components/Dashboard/Sales/OverallSales.jsx
+++ src/components/Dashboard/Sales/OverallSales.jsx
@@ -416,12 +416,28 @@ export default function OverallSales() {
                           color:
                             val.state == "Accepted"
                               ? "#4BB543"
-                              : val.state == "Created"
+                              : val.state == "In-progress"
                               ? "#3F81E0"
                               : "#D0342C",
                         }}
                       >
                         {val.state}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="lucide lucide-pencil"
+                          className={osCss.lucidePencil}
+                        >
+                          <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+                          <path d="m15 5 4 4" />
+                        </svg>
                       </td>
                       <td>{val.buyer}</td>
                     </tr>
