6c775061301299f999388676ae69515b9df12619
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-21 12:32:07 +0530

    Update ColorBox.jsx

--- src/components/Product/ColorBox.jsx
+++ src/components/Product/ColorBox.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 // css
-import BCss from "./Css/Box.module.css";
+import CBss from "./Css/ColorBox.module.css";
 
 export default function Box(props) {
   return (
-    <div className={BCss.mDiv}>
-      <p className={BCss.title}>{props.title}</p>
-      <p className={BCss.value}>{props.value}</p>
+    <div className={BCss.CBss}>
+      <p className={CBss.subTitlePTag}>Color</p>
     </div>
   );
 }
