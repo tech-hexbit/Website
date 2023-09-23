5ac46d319dfbacfd96f40d175cb5758e279c5b2e
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2023-09-23 10:54:27 +0530

    upload csv

--- src/components/Dashboard/AddProduct.jsx
+++ src/components/Dashboard/AddProduct.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // components
 import Form from "./AddProduct/Form";
 
 // css
 import ApCss from "./Css/AddProduct.module.css";
+import UploadCsvPopup from "./AddProduct/uploadCsvPopup";
+import { Alert } from "../../MicroInteraction/Alert";
 
 export default function AddProduct() {
+  const [showPopup, setShowPopup] = useState(false);
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      setError({
+        mainColor: "",
+        secondaryColor: "",
+        symbol: "",
+        title: "",
+        text: "",
+        val: false,
+      });
+    }, 10000);
+  }, [variants]);
   return (
-    <div className={ApCss.mDiv}>
-      <p className={ApCss.AddHPTag}>Add Product</p>
-      <Form />
-    </div>
+    <>
+      <div className={ApCss.mDiv}>
+        <div className={ApCss.headDiv}>
+          <p className={ApCss.AddHPTag}>Add Product</p>
+          <div className={ApCss.addCsv}>
+            <button onClick={() => setShowPopup(true)}>+ Add bulk</button>
+          </div>
+        </div>
+        <Form />
+        {showPopup && (
+          <UploadCsvPopup setShowPopup={setShowPopup} setError={setError} />
+        )}
+      </div>
+      <Alert variant={variants} val={setError} />
+    </>
   );
 }
