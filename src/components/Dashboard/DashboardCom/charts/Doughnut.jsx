a4856999d4b69dfd378991cd5ffcef63401e4fae
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-24 01:33:47 +0530

    Update Doughnut.jsx

--- src/components/Dashboard/DashboardCom/charts/Doughnut.jsx
+++ src/components/Dashboard/DashboardCom/charts/Doughnut.jsx
@@ -1,7 +1,7 @@
-// import React from "react";
+import React from "react";
 import "chart.js/auto";
-// import { Doughnut } from "react-chartjs-2";
-import "../css/dchart.css"
+import { Doughnut } from "react-chartjs-2";
+import "../css/dchart.css";
 export default function DoughnutChart() {
   const data = {
     labels: ["Direct", "Google", "Apple"],
@@ -19,7 +19,7 @@ export default function DoughnutChart() {
 
   return (
     <div className="d-chart">
-      <Doughnut  data={data} options={chartOptions} />
+      <Doughnut data={data} options={chartOptions} />
     </div>
   );
 }
