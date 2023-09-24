9ed564bbabf2a896770a1740dccffe379407af73
Rishav Singh <rishavrajcrj@gmail.com>
2023-09-24 11:03:35 +0530

    Update SmallLoad.jsx

--- src/MicroInteraction/SmallLoad.jsx
+++ src/MicroInteraction/SmallLoad.jsx
@@ -3,5 +3,16 @@ import React from "react";
 import SmallLoadCss from "./Css/SmallLoad.module.css";
 
 export default function SmallLoad() {
-  return <div>SmallLoad</div>;
+  return (
+    <svg className={SmallLoadCss.spinner} viewBox="0 0 50 50">
+      <circle
+        className={SmallLoadCss.path}
+        cx="24"
+        cy="24"
+        r="20"
+        fill="none"
+        stroke-width="5"
+      ></circle>
+    </svg>
+  );
 }
