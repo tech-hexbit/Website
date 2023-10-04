3f491f1f1af5a70bf1140503b411c5eb7ba6daef
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-04 13:53:23 +0530

    Update Display.jsx

--- src/components/Dashboard/product/Display.jsx
+++ src/components/Dashboard/product/Display.jsx
@@ -14,7 +14,7 @@ import Load from "./../../../MicroInteraction/LoadBlack";
 import DCss from "./Css/display.module.css";
 
 // img
-import { Trash2 } from "lucide-react";
+// import { Trash2 } from "lucide-react";
 
 export default function Display({ filteredlist, setfilteredlist }) {
   const [load, setLoad] = useState(false);
@@ -173,7 +173,24 @@ export default function Display({ filteredlist, setfilteredlist }) {
                                 style={{ marginTop: "-5px" }}
                                 onClick={() => deleteproduct(val._id)}
                               >
-                                <Trash2 />
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  class="lucide lucide-trash-2"
+                                >
+                                  <path d="M3 6h18" />
+                                  <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                  <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                  <line x1="10" x2="10" y1="11" y2="17" />
+                                  <line x1="14" x2="14" y1="11" y2="17" />
+                                </svg>
                               </div>
                             </div>
                           </td>
