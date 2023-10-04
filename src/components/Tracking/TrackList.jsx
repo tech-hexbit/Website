eedf93f3b1d4c915fcda60f9d9d1d6d2d3f9239b
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-04 23:43:14 +0530

    Update TrackList.jsx

--- src/components/Tracking/TrackList.jsx
+++ src/components/Tracking/TrackList.jsx
@@ -4,15 +4,24 @@ import React from "react";
 import THCss from "./Css/TrackingHeader.module.css";
 
 export default function TrackList(props) {
+  console.log(props.state);
   console.log(props.currentState);
 
   return (
-    <div className={THCss.listmDiv}>
-      <div className={THCss.circleDiv}></div>
-      <div className={THCss.allContent}>
-        <p className={THCss.title}>{props.title}</p>
-        <p className={THCss.des}>{props.des}</p>
-      </div>
-    </div>
+    <>
+      {props.state === props.currentState ? (
+        <>
+          <div className={THCss.listmDiv}>
+            <div className={THCss.circleDiv}></div>
+            <div className={THCss.allContent}>
+              <p className={THCss.title}>{props.title}</p>
+              <p className={THCss.des}>{props.des}</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
