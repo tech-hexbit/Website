d95a16b5a4b76c261df91b52c68419a9cff0478e
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-09 12:39:29 +0530

    Update Filter.jsx

--- src/components/Dashboard/product/Filter.jsx
+++ src/components/Dashboard/product/Filter.jsx
@@ -17,6 +17,7 @@ export default function Filter({ filteredlist, setfilteredlist }) {
   const [unique, setunique] = useState([]);
   const [orderDel, setOrderDel] = useState([]);
   const [category, setcategory] = useState([]);
+  const [onFil, offFil] = useState(false);
   const [allcategory, setallcategory] = useState([]);
 
   useEffect(() => {
@@ -164,6 +165,46 @@ export default function Filter({ filteredlist, setfilteredlist }) {
           <option hidden>Rating</option>
         </select> 
       </div>*/}
+
+      <div className={FCss.floatRightArrowDiv}>
+        {onFil ? (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-move-right"
+            >
+              <path d="M18 8L22 12L18 16" />
+              <path d="M2 12H22" />
+            </svg>
+          </>
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-move-left"
+            >
+              <path d="M6 8L2 12L6 16" />
+              <path d="M2 12H22" />
+            </svg>
+          </>
+        )}
+      </div>
     </div>
   );
 }
