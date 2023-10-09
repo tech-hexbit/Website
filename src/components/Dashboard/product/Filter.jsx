5037c87a3ce637625611bb4000be94e51393837a
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-09 13:13:59 +0530

    Update Filter.jsx

--- src/components/Dashboard/product/Filter.jsx
+++ src/components/Dashboard/product/Filter.jsx
@@ -111,7 +111,13 @@ export default function Filter({ filteredlist, setfilteredlist }) {
   return (
     <div className={FCss.mainDiv} id={onFil ? "onCat" : "offCat"}>
       <div className={FCss.div1}>
-        <div className={FCss.heading}>
+        <div
+          className={FCss.heading}
+          id={FCss.filterLabel}
+          onClick={() => {
+            offFil(!onFil);
+          }}
+        >
           {/* Filters */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -181,51 +187,6 @@ export default function Filter({ filteredlist, setfilteredlist }) {
           <option hidden>Rating</option>
         </select> 
       </div>*/}
-
-      <div
-        className={FCss.floatRightArrowDiv}
-        onClick={() => {
-          offFil(!onFil);
-        }}
-      >
-        {onFil ? (
-          <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-move-right"
-            >
-              <path d="M18 8L22 12L18 16" />
-              <path d="M2 12H22" />
-            </svg>
-          </>
-        ) : (
-          <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-move-left"
-            >
-              <path d="M6 8L2 12L6 16" />
-              <path d="M2 12H22" />
-            </svg>
-          </>
-        )}
-      </div>
     </div>
   );
 }
