5556a4abaa0c259301254e9138bfde01d681ca9b
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-10 02:48:01 +0530

    Update Box.jsx

--- src/components/Product/Box.jsx
+++ src/components/Product/Box.jsx
@@ -63,7 +63,22 @@ export default function Box(props) {
               changePost("descriptor.short_desc");
             }}
           >
-            Submit
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="12"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-save"
+            >
+              <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
+              <polyline points="17 21 17 13 7 13 7 21" />
+              <polyline points="7 3 7 8 15 8" />
+            </svg>
           </button>
         </>
       ) : (
