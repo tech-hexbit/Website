1bb63af93eee1605b7fe8308aa13ab816793cb2d
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-10 11:22:08 +0530

    Update Display.jsx

--- src/components/Dashboard/product/Display.jsx
+++ src/components/Dashboard/product/Display.jsx
@@ -233,43 +233,44 @@ export default function Display({ filteredlist, setfilteredlist }) {
         <button
           onClick={() => setCurrentPage(currentPage - 1)}
           disabled={currentPage === 1}
+          className={DCss.btnnb}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="18"
+            height="18"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-chevron-left"
-            className={DCss.btnnb}
+            class="lucide lucide-chevrons-left"
           >
-            <path d="m15 18-6-6 6-6" />
+            <path d="m11 17-5-5 5-5" />
+            <path d="m18 17-5-5 5-5" />
           </svg>
         </button>
         <span>{currentPage}</span>
         <button
           onClick={() => setCurrentPage(currentPage + 1)}
           disabled={max}
-          id="s"
+          className={DCss.btnnb}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="18"
+            height="18"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-chevron-right"
-            className={DCss.btnnb}
+            class="lucide lucide-chevrons-right"
           >
-            <path d="m9 18 6-6-6-6" />
+            <path d="m6 17 5-5-5-5" />
+            <path d="m13 17 5-5-5-5" />
           </svg>
         </button>
       </div>
