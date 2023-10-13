31b424c71c38f454dac68c7f38c37874935b1167
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-13 23:39:42 +0530

    Update OverallSales.jsx

--- src/components/Dashboard/Sales/OverallSales.jsx
+++ src/components/Dashboard/Sales/OverallSales.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 // state
