fbd439364abfb318aa85714b13a51b44420555fa
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-15 19:20:49 +0530

    Update Form.jsx

--- src/components/Dashboard/AddProduct/Form.jsx
+++ src/components/Dashboard/AddProduct/Form.jsx
@@ -246,6 +246,51 @@ export default function Form() {
             text: "Successfully Added",
             val: true,
           });
+
+          setData({
+            name: "",
+            symbol:
+              "https://beebom.com/wp-content/uploads/2021/07/rog-phone-5-review-2.jpg?quality=75&strip=all",
+            short_desc: "",
+            long_desc: "",
+            images:
+              "https://beebom.com/wp-content/uploads/2021/07/rog-phone-5-review-2.jpg?quality=75&strip=all",
+            maximumCount: 0,
+            maximum_value: 0,
+            category_id: "",
+            fulfillment_id: 1,
+            location_id: "asus-store-location-id-1",
+            ondcOrgreturnable: true,
+            ondcOrgcancellable: true,
+            ondcOrgreturn_window: "P7D",
+            ondcOrgseller_pickup_return: false,
+            ondcOrgtime_to_ship: "PT45M",
+            ondcOrgavailable_on_cod: false,
+            ondcOrgcontact_details_consumer_care:
+              "Ramesh1, Koramangala, Bengaluru, ramesh@abc.com, 9876543210",
+            manufacturer_or_packer_name: "",
+            brand_name: "",
+            Discounts: "",
+            Sizes: "",
+            Colors: "",
+            manufacturer_or_packer_address: "123, xyz street, Bengaluru",
+            common_or_generic_name_of_commodity: "asus shoe 1",
+            net_quantity_or_measure_of_commodity_in_pkg: 121,
+            month_year_of_manufacture_packing_import: "08/2022",
+            nutritional_info:
+              "Energy(KCal)-(per 100kg) 420, (per serving 50g)250; Protein(g)-(per 100kg) 12, (per serving 50g) 6",
+            additives_info: "",
+            brand_owner_FSSAI_license_no: "1234567890",
+            other_FSSAI_license_no: "1234567890",
+            importer_FSSAI_license_no: "1234567890",
+            net_quantity: "120g",
+            veg: false,
+            non_veg: false,
+            Status: "",
+            Visibility: "",
+            schedule_Date_and_time: "10/10/2023",
+            StoreID: authCtx.user.StoreID,
+          });
         } else {
           setLoad(false);
 
