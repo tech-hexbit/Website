aebbd29ed1b3d9d2d96dd22c876eb9a6049fda9c
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-17 00:38:16 +0530

    Update Doughnut.jsx

--- src/components/Dashboard/DashboardCom/charts/Doughnut.jsx
+++ src/components/Dashboard/DashboardCom/charts/Doughnut.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Doughnut } from "react-chartjs-2";
 import "chart.js/auto";
+
+// state
+import AuthContext from "./../../../../store/auth-context";
 
 // css
 import "../css/dchart.css";
 
 export default function DoughnutChart() {
+  const [] = useState({
+    labels: [],
+    data: [],
+  });
+
+  const authCtx = useContext(AuthContext);
+
   const data = {
     labels: ["Direct", "Google", "Apple"],
     datasets: [
@@ -19,6 +29,17 @@ export default function DoughnutChart() {
   const chartOptions = {
     cutout: "95%",
   };
+
+  const loadData = async () => {
+    try {
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <div className="d-chart">
