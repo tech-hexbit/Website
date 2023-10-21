3cf8e0c08d528308343df53478c162f8067ae976
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-21 21:29:11 +0530

    Create PersonalInfo.jsx

---
+++ src/components/Dashboard/Profile/PersonalInfo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// css
+import PICss from "./Css/PersonalInfo.module.css";
+
+export default function PersonalInfo() {
+  return <div className={PICss.personalinfotab}>PersonalInfo</div>;
+}
