b2e0b33d27117a1b56196c74154a73d8df5ca30c
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-21 21:32:48 +0530

    Update PersonalInfo.jsx

--- src/components/Dashboard/Profile/PersonalInfo.jsx
+++ src/components/Dashboard/Profile/PersonalInfo.jsx
@@ -3,10 +3,17 @@ import React from "react";
 // css
 import PICss from "./Css/PersonalInfo.module.css";
 
-export default function PersonalInfo() {
+export default function PersonalInfo(props) {
   return (
     <div className={PICss.personalinfotab}>
       <div className={PICss.heading}>Personal info</div>
+
+      <div className={PICss.box}>
+        <div className={PICss.inputheading}>Email</div>
+        <div className={PICss.infodiv}>
+          <div className={PICss.info}>{props.Email}</div>
+        </div>
+      </div>
     </div>
   );
 }
