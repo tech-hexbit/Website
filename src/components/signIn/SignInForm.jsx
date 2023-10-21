4a9d2cea921e2687913baa0f619ad8b8f7338226
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-21 13:29:22 +0530

    Update SignInForm.jsx

--- src/components/signIn/SignInForm.jsx
+++ src/components/signIn/SignInForm.jsx
@@ -20,6 +20,7 @@ import slider1 from "../../assets/slider/Group3.png";
 
 export default function SignInForm() {
   const [input, setInput] = useState({ email: "", password: "" });
+  const [see, hide] = useState(false);
   const [load, setLoad] = useState(false);
   const [variants, setError] = useState({
     mainColor: "",
@@ -157,7 +158,7 @@ export default function SignInForm() {
               </label>
               <br />
               <input
-                type="password"
+                type={see ? "text" : "password"}
                 placeholder="Password"
                 id="password"
                 name="password"
@@ -166,6 +167,43 @@ export default function SignInForm() {
                   setInput({ ...input, password: e.target.value });
                 }}
               />
+              {see ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-eye"
+                  onClick={() => hide(!see)}
+                >
+                  <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
+                  <circle cx="12" cy="12" r="3" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-eye-off"
+                  onClick={() => hide(!see)}
+                >
+                  <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
+                  <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
+                  <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
+                  <line x1="2" x2="22" y1="2" y2="22" />
+                </svg>
+              )}
             </div>
           </div>
           <div className={style.or}>Or</div>
