06d5359c9019acc788cb037d4611318960d7d9ec
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-22 11:34:55 +0530

    Update EditProfileImage.jsx

--- src/components/Dashboard/Profile/EditProfileImage.jsx
+++ src/components/Dashboard/Profile/EditProfileImage.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
+
+// axios
+import axios from "axios";
+
+// state
+import AuthContext from "./../../../store/auth-context";
 
 // css
 import EtCss from "./Css/EditProfile.module.css";
 
 export default function EditProfileImage(props) {
+  const [imageUpload, setImageUpload] = useState();
+
+  const fileInp = useRef(null);
+
+  const authCtx = useContext(AuthContext);
+
+  const handleImage = (e) => {
+    console.log(e.target.files[0]);
+    setImageUpload(e.target.files[0]);
+  };
+
+  const handleClick = () => {
+    fileInp.current.click();
+  };
+
+  const onSubmit = async () => {
+    const formData = new FormData();
+    formData.append("images", imageUpload);
+
+    for (var key of formData.entries()) {
+      console.log(key[0] + ", " + key[1]);
+    }
+
+    try {
+      const response = await axios.post("/api/website/auth/EditMe", formData, {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+      console.log(response);
+
+      if (response.data.success) {
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={EtCss.mDiv}>
       <div className={EtCss.centerDiv}>
@@ -27,9 +70,35 @@ export default function EditProfileImage(props) {
           <path d="m6 6 12 12" />
         </svg>
         <div className={EtCss.upMDiv}>
-          <div>+</div>
+          <div className={EtCss.addimgDivMain}>
+            <input
+              type="file"
+              name="file"
+              onChange={handleImage}
+              style={{ display: "none" }}
+              ref={fileInp}
+            />
+
+            <div className={EtCss.addImgDiv} onClick={handleClick}>
+              <div className={EtCss.textCenter}>
+                <p className={EtCss.dropzoneContent}>+</p>
+              </div>
+            </div>
+          </div>
           <div>
-            <button>Submit</button>
+            {imageUpload ? (
+              <img
+                src={URL.createObjectURL(imageUpload)}
+                alt=""
+                className={EtCss.prevImg}
+              />
+            ) : (
+              ""
+            )}
+
+            <div className={EtCss.SubmitBtn} onClick={onSubmit}>
+              Submit
+            </div>
           </div>
         </div>
       </div>
