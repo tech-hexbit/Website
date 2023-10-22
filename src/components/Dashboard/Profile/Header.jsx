3b8ab2d81852da3a5ababa3a5868c14ffb9e2b64
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-22 10:48:59 +0530

    Update Header.jsx

--- src/components/Dashboard/Profile/Header.jsx
+++ src/components/Dashboard/Profile/Header.jsx
@@ -52,7 +52,25 @@ export default function Header() {
     <>
       <div className={HPCss.background}>
         <div className={HPCss.mDiv}>
-          <img src={authCtx.user.image} alt="" className={HPCss.imgHeaderP} />
+          <div className={HPCss.ImgmDiv}>
+            <img src={authCtx.user.image} alt="" className={HPCss.imgHeaderP} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-pencil"
+              className={HPCss.editIcon}
+            >
+              <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+              <path d="m15 5 4 4" />
+            </svg>
+          </div>
           <div className={HPCss.delHeader}>
             <p className={HPCss.delPName}>
               <b>{authCtx.user.BusinessName}</b>
