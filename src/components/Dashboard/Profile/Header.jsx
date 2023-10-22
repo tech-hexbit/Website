ceb35092f4d6d68cb195e7df50fc0f1bcf9eb4c3
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-22 11:18:06 +0530

    Update Header.jsx

--- src/components/Dashboard/Profile/Header.jsx
+++ src/components/Dashboard/Profile/Header.jsx
@@ -68,7 +68,9 @@ export default function Header() {
               stroke-linejoin="round"
               class="lucide lucide-pencil"
               className={HPCss.editIcon}
-              onClick={() => {}}
+              onClick={() => {
+                setEdit(!showEdit);
+              }}
             >
               <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
               <path d="m15 5 4 4" />
