f142e53438edfde9aad4e1997475ce8455a27389
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-22 11:26:48 +0530

    Update EditProfileImage.jsx

--- src/components/Dashboard/Profile/EditProfileImage.jsx
+++ src/components/Dashboard/Profile/EditProfileImage.jsx
@@ -3,6 +3,30 @@ import React from "react";
 // css
 import EtCss from "./Css/EditProfile.module.css";
 
-export default function EditProfileImage() {
-  return <div>EditProfileImage</div>;
+export default function EditProfileImage(props) {
+  return (
+    <div className={EtCss.mDiv}>
+      <div className={EtCss.centerDiv}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-x"
+          className={EtCss.closeMDiv}
+          onClick={() => {
+            props.setEdit(false);
+          }}
+        >
+          <path d="M18 6 6 18" />
+          <path d="m6 6 12 12" />
+        </svg>
+      </div>
+    </div>
+  );
 }
