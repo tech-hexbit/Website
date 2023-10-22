6a5a000e590a693ed417d1881416f8196bc34132
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-22 11:52:55 +0530

    Update Header.jsx

--- src/components/Dashboard/Profile/Header.jsx
+++ src/components/Dashboard/Profile/Header.jsx
@@ -56,25 +56,30 @@ export default function Header() {
         <div className={HPCss.mDiv}>
           <div className={HPCss.ImgmDiv}>
             <img src={authCtx.user.image} alt="" className={HPCss.imgHeaderP} />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-pencil"
-              className={HPCss.editIcon}
-              onClick={() => {
-                setEdit(!showEdit);
-              }}
-            >
-              <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-              <path d="m15 5 4 4" />
-            </svg>
+
+            {load ? (
+              ""
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-pencil"
+                className={HPCss.editIcon}
+                onClick={() => {
+                  setEdit(!showEdit);
+                }}
+              >
+                <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+                <path d="m15 5 4 4" />
+              </svg>
+            )}
           </div>
           <div className={HPCss.delHeader}>
             <p className={HPCss.delPName}>
