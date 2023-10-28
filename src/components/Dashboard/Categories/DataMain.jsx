a6b59f8026b7b55b8e5f57df5e77afaa65e75af3
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-28 10:27:46 +0530

    Update DataMain.jsx

--- src/components/Dashboard/Categories/DataMain.jsx
+++ src/components/Dashboard/Categories/DataMain.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
+// components
+import DataTab from "./DataTab";
+
 // css
 import DM from "./Css/DataMain.module.css";
 
-export default function DataMain() {
-  return <div>DataMain</div>;
+export default function DataMain(props) {
+  return (
+    <div>
+      <DataTab />
+      <DataTab />
+      <DataTab />
+      <DataTab />
+    </div>
+  );
 }
