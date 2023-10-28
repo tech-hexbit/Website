726b42651094b8fe51213ebb8145e5c8b434f439
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-28 10:37:10 +0530

    Update DataMain.jsx

--- src/components/Dashboard/Categories/DataMain.jsx
+++ src/components/Dashboard/Categories/DataMain.jsx
@@ -9,10 +9,10 @@ import DM from "./Css/DataMain.module.css";
 export default function DataMain(props) {
   return (
     <div className={DM.mDiv}>
-      <DataTab bgColor="#FCEBFF" label="TOTAL ORDERS" val={23456} />
-      <DataTab bgColor="#FDFFE0" label="Total s" val={23456} />
-      <DataTab bgColor="#E4EAFF" label="Total s" val={23456} />
-      <DataTab bgColor="#DCFEF0" label="Total s" val={23456} />
+      <DataTab bgColor="#FCEBFF" label="TOTAL ORDERS" val={123} />
+      <DataTab bgColor="#FDFFE0" label="SHIPPED" val={4} />
+      <DataTab bgColor="#E4EAFF" label="PACKED" val={23} />
+      <DataTab bgColor="#DCFEF0" label="DELIVERED" val={97} />
     </div>
   );
 }
