a6cd7d08b2a68d653ab5c6f0a2b8216eb884a061
Rishav Singh <rishavrajcrj@gmail.com>
2023-11-08 03:51:13 +0530

    Update Revenue.jsx

--- src/components/Dashboard/DashboardCom/Revenue.jsx
+++ src/components/Dashboard/DashboardCom/Revenue.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 
 // axios
 import axios from "axios";
@@ -70,6 +71,9 @@ export default function Revenue() {
           <StackedBar />
         </div>
       </div>
+      <Link className="LinkStyle" id={RCss.moreInfo}>
+        More Info
+      </Link>
     </div>
   );
 }
