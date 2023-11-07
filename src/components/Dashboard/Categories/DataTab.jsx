e5b435d30fb9741f0a2c0c475b05d2a17bed09bd
Rishav Singh <rishavrajcrj@gmail.com>
2023-11-08 05:11:35 +0530

    Update DataTab.jsx

--- src/components/Dashboard/Categories/DataTab.jsx
+++ src/components/Dashboard/Categories/DataTab.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // css
 import DmTCss from "./Css/DataMain.module.css";
@@ -9,8 +10,10 @@ export default function DataTab(props) {
       className={DmTCss.mDivTab}
       style={{ backgroundColor: `${props.bgColor}` }}
     >
-      <p className={DmTCss.valPTagTab}>{props.val}</p>
-      <p className={DmTCss.labelPTagTab}>{props.label}</p>
+      <Link to="/me/sales" className="LinkStyle">
+        <p className={DmTCss.valPTagTab}>{props.val}</p>
+        <p className={DmTCss.labelPTagTab}>{props.label}</p>
+      </Link>
     </div>
   );
 }
