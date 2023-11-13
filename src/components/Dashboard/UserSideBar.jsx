fd002a56a6bbfcfd12cf2aa61d1f091e4335b1e3
Rishav Singh <rishavrajcrj@gmail.com>
2023-11-13 10:47:10 +0530

    Update UserSideBar.jsx

--- src/components/Dashboard/UserSideBar.jsx
+++ src/components/Dashboard/UserSideBar.jsx
@@ -22,8 +22,6 @@ export default function UserSideBar() {
       {authCtx.user.access === 0 ? (
         // Admin
         <>
-          {" "}
-          {/* Categories */}
           <NavLink
             to="/me/Support"
             className={({ isActive }) =>
@@ -38,7 +36,7 @@ export default function UserSideBar() {
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="21"
-                  viewBox="0 0 20 21"
+                  viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
                   stroke-width="2"
