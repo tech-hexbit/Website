d81a46c5a5d7eaea6f02b09a51fab8779e441b68
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-03 21:07:09 +0530

    Update Support.jsx

--- src/components/Dashboard/Support.jsx
+++ src/components/Dashboard/Support.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 // components
 import QA from "./Support/QA";
 
+// state
+import AuthContext from "./.././../store/auth-context";
+
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import Load from "./../../../MicroInteraction/LoadBlack";
+
 // css
 import SupCss from "./Support/Css/Support.module.css";
 
 export default function Support() {
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    setLoad(true);
+
+    try {
+      const response = await axios.get(`/api/website/qna/get`, {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+    } catch (e) {
+      setLoad(false);
+
+      console.log(e);
+    }
+  };
   return (
     <div>
       <p>FAQs</p>
