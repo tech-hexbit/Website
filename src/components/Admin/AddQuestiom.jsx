429c2e41aa8e91d6942c866829db8ca683969c27
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-03 22:18:03 +0530

    Update AddQuestiom.jsx

--- src/components/Admin/AddQuestiom.jsx
+++ src/components/Admin/AddQuestiom.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
-export default function AddQuestiom() {
-  return <div>AddQuestiom</div>;
+export default function AddQuestiom(props) {
+  return (
+    <div>
+      AddQuestiom
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-x"
+        onClick={() => {
+          props.setAdd(false);
+        }}
+      >
+        <path d="M18 6 6 18" />
+        <path d="m6 6 12 12" />
+      </svg>
+    </div>
+  );
 }
