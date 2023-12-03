f11df66f00f7599b043d74103e87dca398475d3e
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-03 20:30:12 +0530

    Create QA.jsx

---
+++ src/components/Admin/QA.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+// css
+import SupCss from "./Css/Support.module.css";
+
+export default function QA() {
+  const [show, hide] = useState(false);
+  return (
+    <div
+      className={SupCss.qaComDiv}
+      onClick={() => {
+        hide(!show);
+      }}
+    >
+      <p>
+        <>Lorem ipsum dolor sit amet consectetur ?</>
+      </p>
+      <p className={SupCss.ansPTag} id={show ? "showqa" : "hideqa"}>
+        <i>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi
+          molestias repellat rerum nam corporis, dignissimos sunt reprehenderit
+          vero illo provident.
+        </i>
+      </p>
+    </div>
+  );
+}
