f0c3572a1ab9fe074ecded00a6e0bf39ee4360f2
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-03 22:43:59 +0530

    Update AddQuestiom.jsx

--- src/components/Admin/AddQuestiom.jsx
+++ src/components/Admin/AddQuestiom.jsx
@@ -5,27 +5,51 @@ import AQCss from "./Css/AddQuestion.module.css";
 
 export default function AddQuestiom(props) {
   return (
-    <div>
-      <p>Add Question(s)</p>
+    <div className={AQCss.mDiv}>
+      <div className={AQCss.titleDiv}>
+        <p>Add Question(s)</p>
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-x"
-        onClick={() => {
-          props.setAdd(false);
-        }}
-      >
-        <path d="M18 6 6 18" />
-        <path d="m6 6 12 12" />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-x"
+          className={AQCss.closeBtn}
+          onClick={() => {
+            props.setAdd(false);
+          }}
+        >
+          <path d="M18 6 6 18" />
+          <path d="m6 6 12 12" />
+        </svg>
+      </div>
+      <div className={AQCss.inpmDiv}>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Question"
+          className={AQCss.inpTag}
+        />
+
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Answer"
+          className={AQCss.inpTag}
+        />
+      </div>
+
+      <div className={AQCss.submitDiv}>
+        <button className={AQCss.SubmitTag}>Submit</button>
+      </div>
     </div>
   );
 }
