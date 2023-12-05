ddc366eb837a2454edfbd1606b9d243111784598
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-05 15:51:24 +0530

    Update Sellers.jsx

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -9,6 +9,9 @@ import axios from "axios";
 // MicroInteraction
 import Load from "./../../MicroInteraction/LoadBlack";
 
+// css
+import SelCss from "./Css/Sellers.module.css";
+
 export default function Sellers() {
   const [data, setData] = useState([]);
   const [load, setLoad] = useState(false);
@@ -20,6 +23,34 @@ export default function Sellers() {
     loadData();
     setRef(false);
   }, [, showRef]);
+
+  const loadData = async () => {
+    setLoad(true);
+
+    try {
+      const response = await axios.get(`/api/website/admin/usersList`, {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+
+      if (response.data.success) {
+        setData(response.data.sellerList);
+
+        setLoad(false);
+      } else {
+        setLoad(false);
+
+        console.log(e);
+      }
+    } catch (e) {
+      setLoad(false);
+
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, []);
 
   return <div>Sellers</div>;
 }
