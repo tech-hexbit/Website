591e9fd07b9020e848d1f84ba9bce6e90e2851b0
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-05 16:23:51 +0530

    Update AllSellers.jsx

--- src/components/Admin/Sellers/AllSellers.jsx
+++ src/components/Admin/Sellers/AllSellers.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
-export default function AllSellers() {
-  return <div>AllSellers</div>;
+// MicroInteraction
+import Load from "./../../../MicroInteraction/LoadBlack";
+
+// css
+import SelCss from "./../Css/Sellers.module.css";
+
+export default function AllSellers(props) {
+  return (
+    <div>
+      {props.load ? (
+        <Load />
+      ) : (
+        <>
+          {props.data ? (
+            <>
+              {props.data.map((val, key) => {
+                return (
+                  <div key={key} className={}>
+                    <p>{val.BusinessName}</p>
+                    <p>{val.Phone}</p>
+                    <p>{val.Email}</p>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            ""
+          )}
+        </>
+      )}
+    </div>
+  );
 }
