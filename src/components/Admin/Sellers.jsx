a77b18589a0fc48ff512a2a5cee3115ec9b98eea
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-05 15:44:18 +0530

    Update Sellers.jsx

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+// state
+import AuthContext from "./.././../store/auth-context";
+
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import Load from "./../../MicroInteraction/LoadBlack";
 
 export default function Sellers() {
   return <div>Sellers</div>;
