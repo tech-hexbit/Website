0eba3ca93fd02281597b3114b395e9601b4c6325
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-05 15:48:01 +0530

    Update Sellers.jsx

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -10,5 +10,16 @@ import axios from "axios";
 import Load from "./../../MicroInteraction/LoadBlack";
 
 export default function Sellers() {
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [showRef, setRef] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    loadData();
+    setRef(false);
+  }, [, showRef]);
+
   return <div>Sellers</div>;
 }
