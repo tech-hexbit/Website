9c3b03ed4cdef3d7c3f00774a9f7631e54403b8c
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-08 04:11:06 +0530

    Update ProductsPage.jsx

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -12,11 +12,15 @@ import Des from "../Product/Des";
 import SizeBox from "../Product/SizeBox";
 import ColorBox from "../Product/ColorBox";
 
+// MicroInteraction
+import Load from "../../MicroInteraction/LoadBlack";
+
 // css
 import PPCss from "./Css/ProductPage.module.css";
 
 export default function ProductsPage(props) {
   const [res, setres] = useState();
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
     loadProducts(props.id);
@@ -26,17 +30,25 @@ export default function ProductsPage(props) {
 
   const loadProducts = async (id) => {
     if (id !== "") {
+      setLoad(true);
+
       try {
         const response = await axios.get(`/api/common/product/details/${id}`, {
           headers: { Authorization: `${authCtx.token}` },
         });
 
         if (response.data.success) {
+          setLoad(false);
+
           setres(response?.data?.ProductDetail);
         } else {
+          setLoad(false);
+
           console.log("error");
         }
       } catch (e) {
+        setLoad(false);
+
         console.log(e);
       }
     }
@@ -69,40 +81,49 @@ export default function ProductsPage(props) {
         Product Details
       </p>
 
-      {res ? (
-        <div className={PPCss.divDiv}>
-          <div className={PPCss.leftDiv}>
-            <img
-              src={res.descriptor.images[0]}
-              alt=""
-              className={PPCss.productImg}
-            />
-          </div>
-          <div className={PPCss.rightDiv}>
-            <p className={PPCss.titleName}>{res.descriptor.name}</p>
-            <p className={PPCss.pID}>{res._id}</p>
-            <p className={PPCss.pSeller}>seller</p>
-            <p className={PPCss.pPublished}>
-              Seller:seller Published on: {res.when.date}
-            </p>
-            <div className={PPCss.boxmDiv}>
-              <Box
-                title="Price"
-                value={`₹ ${res.price.maximum_value}`}
-                up="price.maximum_value"
-              />
-              <Box
-                title="Stock"
-                value={res.quantity.maximum.count}
-                up="quantity.maximum.count"
-              />
-            </div>
-
-            <Des res={res} />
-          </div>
+      {load ? (
+        <div className="loadCenterDiv">
+          <Load />
         </div>
       ) : (
-        <p>No Orders</p>
+        <>
+          {" "}
+          {res ? (
+            <div className={PPCss.divDiv}>
+              <div className={PPCss.leftDiv}>
+                <img
+                  src={res.descriptor.images[0]}
+                  alt=""
+                  className={PPCss.productImg}
+                />
+              </div>
+              <div className={PPCss.rightDiv}>
+                <p className={PPCss.titleName}>{res.descriptor.name}</p>
+                <p className={PPCss.pID}>{res._id}</p>
+                <p className={PPCss.pSeller}>seller</p>
+                <p className={PPCss.pPublished}>
+                  Seller:seller Published on: {res.when.date}
+                </p>
+                <div className={PPCss.boxmDiv}>
+                  <Box
+                    title="Price"
+                    value={`₹ ${res.price.maximum_value}`}
+                    up="price.maximum_value"
+                  />
+                  <Box
+                    title="Stock"
+                    value={res.quantity.maximum.count}
+                    up="quantity.maximum.count"
+                  />
+                </div>
+
+                <Des res={res} />
+              </div>
+            </div>
+          ) : (
+            <p>No Orders</p>
+          )}
+        </>
       )}
     </div>
   );
