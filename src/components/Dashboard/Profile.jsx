e098a0d0569f28d9fdfd1f686d43653ea8563ac7
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-12 19:30:25 +0530

    Update Profile.jsx

--- src/components/Dashboard/Profile.jsx
+++ src/components/Dashboard/Profile.jsx
@@ -4,6 +4,11 @@ import React, { useEffect } from "react";
 import Header from "./Profile/Header";
 
 export default function Profile() {
+  // scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Header />
