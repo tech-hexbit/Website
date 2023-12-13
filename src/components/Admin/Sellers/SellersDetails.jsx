7b479d3215a39defb2b54e25da14977697dc3ae4
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-13 22:56:11 +0530

    Update SellersDetails.jsx

--- src/components/Admin/Sellers/SellersDetails.jsx
+++ src/components/Admin/Sellers/SellersDetails.jsx
@@ -34,41 +34,62 @@ export default function SellersDetails(props) {
           <b>Info</b>
         </p>
 
-        <div className={SDCss.sellerdelDiv}>
-          <p>
-            Business Name: <span>{props.show.val.BusinessName}</span>
-          </p>
-          <p>
-            Email: <span>{props.show.val.Email}</span>
-          </p>
-          <p>
-            Phone Number: <span>+91 {props.show.val.Phone}</span>
-          </p>
-          <p>
-            Importer License: <span>{props.show.val.ImporterLicense}</span>
-          </p>
-          <p>
-            GSTIN: <span>{props.show.val.GSTIN}</span>
-          </p>
-          <p>
-            ShopName: <span>{props.show.val.ShopName}</span>
-          </p>
-          <p>
-            Address: <span>{props.show.val.Address}</span>
-          </p>
-          <p>
-            State: <span>{props.show.val.State}</span>
-          </p>
-          <p>
-            City: <span>{props.show.val.City}</span>
-          </p>
-          <p>
-            Pincode: <span>{props.show.val.Pincode}</span>
-          </p>
+        <div className={SDCss.rowDiv}>
+          <div className={SDCss.sellerdelDiv}>
+            <p>
+              Business Name: <span>{props.show.val.BusinessName}</span>
+            </p>
+            <p>
+              Email: <span>{props.show.val.Email}</span>
+            </p>
+            <p>
+              Phone Number: <span>+91 {props.show.val.Phone}</span>
+            </p>
+            <p>
+              Importer License: <span>{props.show.val.ImporterLicense}</span>
+            </p>
+            <p>
+              GSTIN: <span>{props.show.val.GSTIN}</span>
+            </p>
+            <p>
+              ShopName: <span>{props.show.val.ShopName}</span>
+            </p>
+            <p>
+              Address: <span>{props.show.val.Address}</span>
+            </p>
+            <p>
+              State: <span>{props.show.val.State}</span>
+            </p>
+            <p>
+              City: <span>{props.show.val.City}</span>
+            </p>
+            <p>
+              Pincode: <span>{props.show.val.Pincode}</span>
+            </p>
 
-          <p>
-            Additional Info: <span>{props.show.val.AdditionalInfo}</span>
-          </p>
+            <p>
+              Additional Info: <span>{props.show.val.AdditionalInfo}</span>
+            </p>
+          </div>
+
+          <div className={SDCss.verDiv}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-badge-help"
+            >
+              <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+              <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+              <line x1="12" x2="12.01" y1="17" y2="17" />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
