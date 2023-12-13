3aa2f2d1a6c23eea4110d000e365be59729db4b5
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-13 20:03:29 +0530

    Update Contacted.jsx

--- src/components/Admin/Contacted.jsx
+++ src/components/Admin/Contacted.jsx
@@ -63,12 +63,14 @@ export default function Contacted() {
                 return (
                   <div className={CCss.mapMDiv} key={key}>
                     <div className={CCss.titleDiv}>
-                      <div>
+                      <div className={CCss.titleNameDiv}>
                         <div>{val.name}</div>
                         <div>{val.email}</div>
                       </div>
 
-                      <div>{val.CompanyName}</div>
+                      <div className={CCss.titleCompanyNameDiv}>
+                        {val.CompanyName}
+                      </div>
                     </div>
 
                     <div className={CCss.submainDiv}>
