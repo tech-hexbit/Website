94caf5021d06776c337d640c6f23f10275d19521
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-13 21:31:39 +0530

    Update SellersDetails.jsx

--- src/components/Admin/Sellers/SellersDetails.jsx
+++ src/components/Admin/Sellers/SellersDetails.jsx
@@ -7,26 +7,34 @@ export default function SellersDetails(props) {
   console.log(props.show.val);
   return (
     <div className={SDCss.mDiv}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-move-left"
-        className={SDCss.leftArrow}
-        onClick={() => {
-          props.hide(false);
-        }}
-      >
-        <path d="M6 8L2 12L6 16" />
-        <path d="M2 12H22" />
-      </svg>
-      SellersDetails
+      <div className={SDCss.titleDiv}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-move-left"
+          className={SDCss.leftArrow}
+          onClick={() => {
+            props.hide(false);
+          }}
+        >
+          <path d="M6 8L2 12L6 16" />
+          <path d="M2 12H22" />
+        </svg>
+        SellersDetails
+      </div>
+
+      <div className={SDCss.infoDiv}>
+        <p>
+          <b>Info</b>
+        </p>
+      </div>
     </div>
   );
 }
