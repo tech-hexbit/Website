daf82c76cae45202e2a69fc9afc0cdfcd97362b4
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-13 23:15:14 +0530

    Update SellersDetails.jsx

--- src/components/Admin/Sellers/SellersDetails.jsx
+++ src/components/Admin/Sellers/SellersDetails.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 // css
 import SDCss from "./Css/SellersDetails.module.css";
 
 export default function SellersDetails(props) {
+  const [showVer, setVer] = useState(false);
+
   return (
     <div className={SDCss.mDiv}>
       <div className={SDCss.titleDiv}>
@@ -91,23 +93,32 @@ export default function SellersDetails(props) {
                 <path d="m9 12 2 2 4-4" />
               </svg>
             ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-badge-help"
-                className={SDCss.notVer}
-              >
-                <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-                <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-                <line x1="12" x2="12.01" y1="17" y2="17" />
-              </svg>
+              <>
+                {showVer ? (
+                  <></>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-badge-help"
+                    className={SDCss.notVer}
+                    onClick={() => {
+                      setVer(true);
+                    }}
+                  >
+                    <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                    <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+                    <line x1="12" x2="12.01" y1="17" y2="17" />
+                  </svg>
+                )}
+              </>
             )}
           </div>
         </div>
