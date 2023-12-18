d76705876d9839b90d9d17527359faff167f88d8
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-19 02:26:53 +0530

    Update Des.jsx

--- src/components/Product/Des.jsx
+++ src/components/Product/Des.jsx
@@ -221,28 +221,14 @@ export default function Des(props) {
           </tr>
           <tr>
             <td className={DCss.headingName}>Returnable :</td>
-            <td className={DCss.desName}>
-              {props.res["@ondc/org/returnable"] ? "True" : "False"}
-            </td>
-            <td className={DCss.desName}>
-              <span className={DCss.editBtn}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-pencil"
-                >
-                  <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-                  <path d="m15 5 4 4" />
-                </svg>
-              </span>
-            </td>
+            <UpdateLabel
+              crrValue={props.res["@ondc/org/returnable"]}
+              id={props.id}
+              fieldName="@ondc/org/returnable"
+              placeholder="Select the Return Type"
+              type="select"
+              setChange={props.setChange}
+            />
           </tr>
           <tr>
             <td className={DCss.headingName}>Cancellable :</td>
