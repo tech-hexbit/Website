67598e8971e161e855dd860d5668f8548aebb589
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-19 02:18:06 +0530

    Update Des.jsx

--- src/components/Product/Des.jsx
+++ src/components/Product/Des.jsx
@@ -192,32 +192,18 @@ export default function Des(props) {
           </tr>
           <tr>
             <td className={DCss.headingName}>Brand :</td>
-            <td className={DCss.desName}>
-              {
+            <UpdateLabel
+              crrValue={
                 props.res["@ondc/org/statutory_reqs_packaged_commodities"][
                   "manufacturer_or_packer_name"
                 ]
               }
-            </td>
-            <td className={DCss.desName}>
-              <span className={DCss.editBtn}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-pencil"
-                >
-                  <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-                  <path d="m15 5 4 4" />
-                </svg>
-              </span>
-            </td>
+              id={props.id}
+              fieldName="category_id"
+              placeholder="Updated Category"
+              type="select"
+              setChange={props.setChange}
+            />
           </tr>
           <tr>
             <td className={DCss.headingName}>Weight :</td>
