c1bb1dc4d3766ab8ada2363a9be614aa998515e2
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-18 23:30:43 +0530

    Update UpdateLabel.jsx

--- src/components/Product/UpdateLabel.jsx
+++ src/components/Product/UpdateLabel.jsx
@@ -1,5 +1,37 @@
 import React, { useState, useEffect, useContext } from "react";
 
+// axios
+import axios from "axios";
+
+// state
+import AuthContext from "../../store/auth-context";
+
+// MicroInteraction
+import { Alert } from "./../../MicroInteraction/Alert";
+
+// css
+import DCss from "./Css/Des.module.css";
+
 export default function UpdateLabel() {
-  return <div>UpdateLabel</div>;
+  return (
+    <div>
+      <span className={DCss.editBtn}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-pencil"
+        >
+          <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+          <path d="m15 5 4 4" />
+        </svg>
+      </span>
+    </div>
+  );
 }
