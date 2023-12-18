a2acb1061f047852242665f54d7a7ccabb6ac752
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-18 22:13:19 +0530

    Update Des.jsx

--- src/components/Product/Des.jsx
+++ src/components/Product/Des.jsx
@@ -174,7 +174,26 @@ export default function Des(props) {
       </div> */}
 
       <div className={DCss.mDiv}>
-        <p className={DCss.subTitlePTag}>Product description :</p>
+        <p className={DCss.subTitlePTag}>
+          Product description :
+          <span className={DCss.editBtn}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-pencil"
+            >
+              <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+              <path d="m15 5 4 4" />
+            </svg>
+          </span>
+        </p>
         <table>
           <tr>
             <td className={DCss.headingName}>Category :</td>
