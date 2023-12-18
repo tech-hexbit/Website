3a0b94c077284ca6ea598ae9d293807b0dc42ad2
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-18 22:10:23 +0530

    Update Des.jsx

--- src/components/Product/Des.jsx
+++ src/components/Product/Des.jsx
@@ -143,26 +143,26 @@ export default function Des(props) {
         </p>
       </div>
 
-      <div className={DCss.desDiv2}>
-        {/* <div className={DCss.mDiv}>
+      {/* <div className={DCss.desDiv2}>
+        <div className={DCss.mDiv}>
           <p className={DCss.subTitlePTag}>Features :</p>
           <p className={DCss.desDPTag}>
             Midsole:- Core Instant Step In Comfort Long Lasting Reponsive
             Cushionig And Super Plush Feel. Outsole :- The Rubber Outsole With
             Perfect Grip And Durability.
           </p>
-        </div> */}
-        {/* <div className={DCss.mDiv}>
+        </div>
+        <div className={DCss.mDiv}>
           <p className={DCss.subTitlePTag}>Services :</p>
           <p className={DCss.desDPTag}>
             Midsole:- Core Instant Step In Comfort Long Lasting Reponsive
             Cushionig And Super Plush Feel. Outsole :- The Rubber Outsole With
             Perfect Grip And Durability.
           </p>
-        </div> */}
-      </div>
+        </div>
+      </div> 
 
-      {/* <div className={DCss.mDiv}>
+      <div className={DCss.mDiv}>
         <p className={DCss.subTitlePTag}>
           Special offers & product promotions :
         </p>
