85e93657f738183f1155c8c3568dcd3485103336
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-19 02:42:42 +0530

    Update Des.jsx

--- src/components/Product/Des.jsx
+++ src/components/Product/Des.jsx
@@ -265,28 +265,14 @@ export default function Des(props) {
           </tr>
           <tr>
             <td className={DCss.headingName}>Time To Ship :</td>
-            <td className={DCss.desName}>
-              {props.res["@ondc/org/time_to_ship"]}
-            </td>
-            <td className={DCss.desName}>
-              <span className={DCss.editBtn}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-pencil"
-                >
-                  <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-                  <path d="m15 5 4 4" />
-                </svg>
-              </span>
-            </td>
+            <UpdateLabel
+              crrValue={props.res["@ondc/org/time_to_ship"]}
+              id={props.id}
+              fieldName="@ondc/org/time_to_ship"
+              placeholder="Updated Shipping Time"
+              type="text"
+              setChange={props.setChange}
+            />
           </tr>
           <tr>
             <td className={DCss.headingName}>Available on COD :</td>
