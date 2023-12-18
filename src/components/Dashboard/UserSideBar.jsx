747b3630b5370b7f6ca626fd3e03a34f72b4b083
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-18 20:52:49 +0530

    Update UserSideBar.jsx

--- src/components/Dashboard/UserSideBar.jsx
+++ src/components/Dashboard/UserSideBar.jsx
@@ -247,6 +247,42 @@ export default function UserSideBar() {
             </div>
           </NavLink>
 
+          {/* Payment Request */}
+          <NavLink
+            to="/me/Payment/Details"
+            className={({ isActive }) =>
+              isActive
+                ? "LinkStyle sideBarActive"
+                : "LinkStyle sideBarNonActive"
+            }
+          >
+            <div className={sidebarCSS.navElement}>
+              <div className={sidebarCSS.icons}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-badge-indian-rupee"
+                >
+                  <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                  <path d="M8 8h8" />
+                  <path d="M8 12h8" />
+                  <path d="m13 17-5-1h1a4 4 0 0 0 0-8" />
+                </svg>
+              </div>
+              <div className={sidebarCSS.heading}>
+                <div className={sidebarCSS.arrow}></div>
+                Payment Request
+              </div>
+            </div>
+          </NavLink>
+
           {/* Add product */}
           <NavLink
             to="/me/addProduct"
