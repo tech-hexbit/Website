0e328f8920ae40d4299cdd975ba86cfe9c5c785f
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-21 03:27:23 +0530

    Update PayDetails.jsx

--- src/components/Dashboard/PayDetails.jsx
+++ src/components/Dashboard/PayDetails.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function PayDetails() {
+  // scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return <div>PayDetails</div>;
 }
