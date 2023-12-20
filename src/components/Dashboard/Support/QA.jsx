4431138d1cf3ead0906a063805fa1cbdb404915f
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-21 03:09:09 +0530

    Update QA.jsx

--- src/components/Dashboard/Support/QA.jsx
+++ src/components/Dashboard/Support/QA.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 
 // css
 import SupCss from "./Css/Support.module.css";
 
 export default function QA(props) {
-  const [show, hide] = useState(false);
   return (
-    <div
-      className={SupCss.qaComDiv}
-      onClick={() => {
-        hide(!show);
-      }}
-    >
-      <p className={SupCss.qptag}>
-        <>{props.question}</>
-      </p>
-      <p className={SupCss.ansPTag} id={show ? "showqa" : "hideqa"}>
-        <i>{props.answer}</i>
-      </p>
+    <div className={SupCss.quesboxes}>
+      {data.map((d) => {
+        return (
+          <div className={SupCss.quepart}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-mail"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2" />
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            </svg>
+            <h4 style={{ color: "#53686A" }}>{d.question}</h4>
+            <p>{d.answer}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
