a390de95237f0334b55e5da131a31b8418d1b4af
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-20 20:21:41 +0530

    Update PaymentRequest.jsx

--- src/components/Dashboard/Payment/PaymentRequest.jsx
+++ src/components/Dashboard/Payment/PaymentRequest.jsx
@@ -103,7 +103,6 @@ export default function PaymentRequest() {
           </div>
         </div>
 
-        {/* right */}
         <div className={pr.wrapper}>
           <div className={pr.details_right}>
             <p>
