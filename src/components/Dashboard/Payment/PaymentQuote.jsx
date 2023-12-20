4a4e04ce9fdaba33d3e3256f2ee9f90dc0e27e31
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-20 17:56:02 +0530

    Create PaymentQuote.jsx

---
+++ src/components/Dashboard/Payment/PaymentQuote.jsx
@@ -0,0 +1,14 @@
+import pq from "./Css/PaymentQuote.module.css";
+
+const PaymentQuote = () => {
+  return (
+    <>
+      <p className={pq.endpara}>
+        *Upload Company Authorized GST Sales Invoice In the actions tab upload
+        button,post uploading raise the request.
+      </p>
+    </>
+  );
+};
+
+export default PaymentQuote;
