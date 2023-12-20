c045895cd0da154be88daf7a75850e022556bfad
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-21 03:10:21 +0530

    Update QA.jsx

--- src/components/Dashboard/Support/QA.jsx
+++ src/components/Dashboard/Support/QA.jsx
@@ -6,7 +6,7 @@ import SupCss from "./Css/Support.module.css";
 export default function QA(props) {
   return (
     <div className={SupCss.quesboxes}>
-      {props.data.map((d) => {
+      {props.data.map((val) => {
         return (
           <div className={SupCss.quepart}>
             <svg
@@ -24,8 +24,8 @@ export default function QA(props) {
               <rect width="20" height="16" x="2" y="4" rx="2" />
               <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
             </svg>
-            <h4 style={{ color: "#53686A" }}>{d.question}</h4>
-            <p>{d.answer}</p>
+            <h4 style={{ color: "#53686A" }}>{val.question}</h4>
+            <p>{val.answer}</p>
           </div>
         );
       })}
