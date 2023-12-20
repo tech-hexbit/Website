8b13eb851aab70285c9d6fc07a14d8a82a830e92
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-21 02:55:09 +0530

    Update Box.jsx

--- src/components/Dashboard/Support/Box.jsx
+++ src/components/Dashboard/Support/Box.jsx
@@ -3,6 +3,26 @@ import React from "react";
 // css
 import SupCss from "./Css/Support.module.css";
 
-export default function Box() {
-  return <div>Box</div>;
+export default function Box(props) {
+  return (
+    <div className={SupCss.bx1}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-bell"
+      >
+        <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+        <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" className={SupCss.icon} />
+      </svg>
+      <p style={{ color: "#616161" }}>{props.title}</p>
+      <p style={{ fontWeight: "bold" }}>Getting Started</p>
+    </div>
+  );
 }
