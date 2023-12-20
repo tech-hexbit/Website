bc65fcff6b37e22aa8012aeb2fce52ce94ed6c9d
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-21 02:58:35 +0530

    Update Box.jsx

--- src/components/Dashboard/Support/Box.jsx
+++ src/components/Dashboard/Support/Box.jsx
@@ -21,8 +21,8 @@ export default function Box(props) {
         <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
         <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" className={SupCss.icon} />
       </svg>
-      <p style={{ color: "#616161" }}>{props.title}</p>
-      <p className={SupCss.lablePTag}>Getting Started</p>
+      <p style={{ color: `${props.color}` }}>{props.title}</p>
+      <p className={SupCss.lablePTag}>{props.label}</p>
     </div>
   );
 }
