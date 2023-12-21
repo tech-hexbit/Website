6f2314ac04143a4f36025d3e6e3fb0dbf0f50397
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-21 10:28:57 +0530

    Update QA.jsx

--- src/components/Dashboard/Support/QA.jsx
+++ src/components/Dashboard/Support/QA.jsx
@@ -6,36 +6,41 @@ import SupCss from "./Css/Support.module.css";
 export default function QA(props) {
   return (
     <>
-      {props.data.length > 0 ? "" : ""}
-      <div className={SupCss.quesboxes}>
-        {props.data.map((val) => {
-          return (
-            <div className={SupCss.quepart}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-mail"
-              >
-                <rect width="20" height="16" x="2" y="4" rx="2" />
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-              </svg>
-              <h4 className={SupCss.questionTag}>{val.question}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: val.answer,
-                }}
-              />
-            </div>
-          );
-        })}
-      </div>
+      {props.data.length > 0 ? (
+        <div className={SupCss.quesboxes}>
+          {props.data.map((val) => {
+            return (
+              <div className={SupCss.quepart}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-mail"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2" />
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+                </svg>
+                <h4 className={SupCss.questionTag}>{val.question}</h4>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: val.answer,
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="loadCenterDiv">
+          <p>No Data to Show</p>
+        </div>
+      )}
     </>
   );
 }
