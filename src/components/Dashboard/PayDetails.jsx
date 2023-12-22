433da8973c1906d93d8a81acbdc05e7e3dd84145
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-22 20:34:30 +0530

    Update PayDetails.jsx

--- src/components/Dashboard/PayDetails.jsx
+++ src/components/Dashboard/PayDetails.jsx
@@ -5,7 +5,7 @@ import PaymentList from "./PayDetails/GatewayPaymentList";
 import PaymentTable from "./PayDetails/GatewayPaymentTable";
 
 // css
-import Gate from "../Dashboard/PayDetails/Css/Gateway.module.css";
+import Gate from "./../Dashboard/PayDetails/Css/Gateway.module.css";
 
 export default function PayDetails() {
   // scroll to top
