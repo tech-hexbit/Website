056af2adabfba076d6a004c517ef44b75813a418
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-22 20:32:31 +0530

    Update PayDetails.jsx

--- src/components/Dashboard/PayDetails.jsx
+++ src/components/Dashboard/PayDetails.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect } from "react";
 
+// components
+import PaymentList from "./PayDetails/GatewayPaymentList";
+import PaymentTable from "./PayDetails/GatewayPaymentTable";
+
+// css
+import Gate from "../Dashboard/PayDetails/Css/Gateway.module.css";
+
 export default function PayDetails() {
   // scroll to top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return <div>PayDetails</div>;
+  return (
+    <>
+      <div className={Gate.main}>
+        <PaymentList />
+        <PaymentTable />
+      </div>
+    </>
+  );
 }
