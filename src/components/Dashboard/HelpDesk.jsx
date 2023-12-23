8d77dc2a960d7a71c5e091b77c48c2cbb472162c
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-23 21:08:15 +0530

    Update HelpDesk.jsx

--- src/components/Dashboard/HelpDesk.jsx
+++ src/components/Dashboard/HelpDesk.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function HelpDesk() {
+  // scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return <div>HelpDesk</div>;
 }
