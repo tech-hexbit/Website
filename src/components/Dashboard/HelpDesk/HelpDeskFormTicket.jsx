7b49216515e417943efdff4a280674b13bcb7f5f
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-25 09:47:32 +0530

    Create HelpDeskFormTicket.jsx

---
+++ src/components/Dashboard/HelpDesk/HelpDeskFormTicket.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+// css
+import hdft from "./Css/HelpDeskFormTicket.module.css";
+
+export default function HelpDeskFormTicket() {
+  return <div>HelpDeskFormTicket</div>;
+}
