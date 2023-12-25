c34f91b0c27df76df3bb1c0eaecc0e4a630b4787
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-25 10:46:34 +0530

    Update HelpDesk.jsx

--- src/components/Dashboard/HelpDesk.jsx
+++ src/components/Dashboard/HelpDesk.jsx
@@ -21,27 +21,28 @@ export default function HelpDesk() {
     setSubmitted(true);
   };
 
-  return;
-  <>
-    {!submitted ? (
-      <>
-        <h1 className={hd.heading}>Contact Us</h1>
-        <p className={hd.para}>
-          Just answer a few questions, Helping us answer your queries better. We
-          will surely revert within 48 hrs.
-        </p>
-      </>
-    ) : null}
-    <div className={hd.main}>
+  return (
+    <>
       {!submitted ? (
-        <HelpDeskForm onFormSubmit={handleFormSubmit} />
-      ) : (
-        <HelpDeskFormTicket />
-      )}
-      <div className={hd.submain}>
-        <QuickInquiries />
-        <PastTicket />
+        <>
+          <h1 className={hd.heading}>Contact Us</h1>
+          <p className={hd.para}>
+            Just answer a few questions, Helping us answer your queries better.
+            We will surely revert within 48 hrs.
+          </p>
+        </>
+      ) : null}
+      <div className={hd.main}>
+        {!submitted ? (
+          <HelpDeskForm onFormSubmit={handleFormSubmit} />
+        ) : (
+          <HelpDeskFormTicket />
+        )}
+        <div className={hd.submain}>
+          <QuickInquiries />
+          <PastTicket />
+        </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 }
