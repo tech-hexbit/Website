0d75744589e6572cb715f0fb2b0cf24b8a80063e
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-25 21:30:45 +0530

    Update HelpDeskForm.jsx

--- src/components/Dashboard/HelpDesk/HelpDeskForm.jsx
+++ src/components/Dashboard/HelpDesk/HelpDeskForm.jsx
@@ -16,10 +16,10 @@ import HelpDeskFormTicket from "./HelpDeskFormTicket";
 // css
 import hdf from "./Css/HelpDeskForm.module.css";
 
-export default function HelpDeskForm({ onFormSubmit }) {
+export default function HelpDeskForm(props) {
   const [load, setLoad] = useState(false);
   const [DropShow, hideDrop] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
   const [showloadStore, setloadStore] = useState([]);
   const [loadMIStore, setLoadMIStore] = useState(false);
   const [data, setData] = useState({
@@ -147,165 +147,163 @@ export default function HelpDeskForm({ onFormSubmit }) {
   return (
     <>
       <div className={hdf.main}>
-        {submitted ? (
+        {/* {submitted ? (
           <HelpDeskFormTicket changeState={setSubmitted} />
-        ) : (
-          <div>
-            <form onSubmit={handleSubmit}>
-              {/* Name */}
-              <div className={hdf.flex}>
-                <label htmlFor="fullName">Full Name *</label>
+        ) : ( */}
+        <div>
+          <form onSubmit={handleSubmit}>
+            {/* Name */}
+            <div className={hdf.flex}>
+              <label htmlFor="fullName">Full Name *</label>
+              <input
+                type="text"
+                id="fullName"
+                name="name"
+                placeholder="john david"
+                value={data.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {/* Email */}
+            <div className={hdf.flex}>
+              <label htmlFor="email">Your Email *</label>
+              <input
+                type="email"
+                id="email"
+                name="emailID"
+                placeholder="example@yourmail.com"
+                value={data.emailID}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {/* Store Name */}
+            <div className={hdf.flex}>
+              <label htmlFor="storeName">Store Name *</label>
+              <input
+                type="text"
+                id="storeName"
+                name="StoreName"
+                placeholder="your company name here"
+                value={data.StoreName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {/* Store ID */}
+            <div className={hdf.flex}>
+              <label htmlFor="storeId">Store ID *</label>
+              <div ref={menu} className={hdf.CollegeInpmDIv}>
                 <input
                   type="text"
-                  id="fullName"
-                  name="name"
-                  placeholder="john david"
-                  value={data.name}
+                  id="storeId"
+                  name="StoreID"
+                  placeholder="your store id (located in my profile)"
+                  value={data.StoreID}
                   onChange={handleChange}
+                  className={hdf.storeInp}
+                  spellcheck="true"
+                  autocomplete="off"
+                  onFocus={() => {
+                    hideDrop(true);
+                  }}
                   required
                 />
-              </div>
 
-              {/* Email */}
-              <div className={hdf.flex}>
-                <label htmlFor="email">Your Email *</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="emailID"
-                  placeholder="example@yourmail.com"
-                  value={data.emailID}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              {/* Store Name */}
-              <div className={hdf.flex}>
-                <label htmlFor="storeName">Store Name *</label>
-                <input
-                  type="text"
-                  id="storeName"
-                  name="StoreName"
-                  placeholder="your company name here"
-                  value={data.StoreName}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              {/* Store ID */}
-              <div className={hdf.flex}>
-                <label htmlFor="storeId">Store ID *</label>
-                <div ref={menu} className={hdf.CollegeInpmDIv}>
-                  <input
-                    type="text"
-                    id="storeId"
-                    name="StoreID"
-                    placeholder="your store id (located in my profile)"
-                    value={data.StoreID}
-                    onChange={handleChange}
-                    className={hdf.storeInp}
-                    spellcheck="true"
-                    autocomplete="off"
-                    onFocus={() => {
-                      hideDrop(true);
-                    }}
-                    required
-                  />
-
-                  <div className={hdf.MainDropDown}>
-                    {loadMIStore ? (
-                      <div className="loadCenterDiv">
-                        <Load />
-                      </div>
-                    ) : (
-                      <>
-                        {showloadStore.length > 0 ? (
-                          <>
-                            {showloadStore
-                              .filter((val) => {
-                                if (data.StoreID === "") {
-                                  return val;
-                                } else if (
-                                  val.StoreID._id
-                                    .toLowerCase()
-                                    .includes(data.StoreID.toLowerCase())
-                                ) {
-                                  return val;
-                                }
-                              })
-                              .map((val, key) => {
-                                return (
-                                  <div
-                                    key={key}
-                                    className={hdf.DropDownmDiv}
-                                    id={
-                                      DropShow
-                                        ? "showDropMenuClg"
-                                        : "hideDropMenuClg"
-                                    }
-                                    onClick={() => {
-                                      setData({
-                                        ...data,
-                                        StoreID: val.StoreID._id,
-                                      });
-                                      hideDrop(false);
-                                    }}
-                                  >
-                                    <span className={hdf.span1}>
-                                      {val.StoreID._id}
-                                    </span>
-                                    <span className={hdf.span2}>
-                                      {val.StoreID.StoreName}
-                                    </span>
-                                  </div>
-                                );
-                              })}
-                          </>
-                        ) : (
-                          <div className="loadCenterDiv">
-                            No Available Stores
-                          </div>
-                        )}
-                      </>
-                    )}
-                  </div>
+                <div className={hdf.MainDropDown}>
+                  {loadMIStore ? (
+                    <div className="loadCenterDiv">
+                      <Load />
+                    </div>
+                  ) : (
+                    <>
+                      {showloadStore.length > 0 ? (
+                        <>
+                          {showloadStore
+                            .filter((val) => {
+                              if (data.StoreID === "") {
+                                return val;
+                              } else if (
+                                val.StoreID._id
+                                  .toLowerCase()
+                                  .includes(data.StoreID.toLowerCase())
+                              ) {
+                                return val;
+                              }
+                            })
+                            .map((val, key) => {
+                              return (
+                                <div
+                                  key={key}
+                                  className={hdf.DropDownmDiv}
+                                  id={
+                                    DropShow
+                                      ? "showDropMenuClg"
+                                      : "hideDropMenuClg"
+                                  }
+                                  onClick={() => {
+                                    setData({
+                                      ...data,
+                                      StoreID: val.StoreID._id,
+                                    });
+                                    hideDrop(false);
+                                  }}
+                                >
+                                  <span className={hdf.span1}>
+                                    {val.StoreID._id}
+                                  </span>
+                                  <span className={hdf.span2}>
+                                    {val.StoreID.StoreName}
+                                  </span>
+                                </div>
+                              );
+                            })}
+                        </>
+                      ) : (
+                        <div className="loadCenterDiv">No Available Stores</div>
+                      )}
+                    </>
+                  )}
                 </div>
               </div>
+            </div>
 
-              {/* Subject */}
-              <div className={hdf.flex}>
-                <label htmlFor="subject">Subject *</label>
-                <input
-                  type="text"
-                  id="subject"
-                  name="subject"
-                  placeholder="brief title of your query"
-                  value={data.subject}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
+            {/* Subject */}
+            <div className={hdf.flex}>
+              <label htmlFor="subject">Subject *</label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="brief title of your query"
+                value={data.subject}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-              {/* Message */}
-              <div className={hdf.flex}>
-                <label htmlFor="message">Message *</label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows={10}
-                  placeholder="Brief description of the query min 120 characters"
-                  value={data.message}
-                  onChange={handleChange}
-                  required
-                ></textarea>
-              </div>
+            {/* Message */}
+            <div className={hdf.flex}>
+              <label htmlFor="message">Message *</label>
+              <textarea
+                id="message"
+                name="message"
+                rows={10}
+                placeholder="Brief description of the query min 120 characters"
+                value={data.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
 
-              <button type="submit">{load ? <Load /> : "Submit"}</button>
-            </form>
-          </div>
-        )}
+            <button type="submit">{load ? <Load /> : "Submit"}</button>
+          </form>
+        </div>
+        {/* )} */}
       </div>
 
       <Alert variant={variants} val={setError} />
