ea903c19f41abfbf1c62a6584b470320a87d767b
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-28 10:17:08 +0530

    Update SignInForm.jsx

--- src/components/signIn/SignInForm.jsx
+++ src/components/signIn/SignInForm.jsx
@@ -230,40 +230,6 @@ export default function SignInForm() {
 
           <div className={style.or}>Or</div>
 
-          {/* OTP and Phone Number */}
-          <div className={style.phoneOTP}>
-            <div className={style.inputPO}>
-              <label htmlFor="phone">
-                Phone<span className="requiredSpan">*</span>
-              </label>
-              <br />
-              <div className={style.phoneInputs}>
-                <input
-                  type="text"
-                  placeholder="+91"
-                  disabled
-                  id={style.countryCode}
-                />
-                <input
-                  type="number"
-                  placeholder="XXXXX-XXXXX"
-                  id="phone"
-                  className={style.phone}
-                />
-              </div>
-            </div>
-            <div className={style.inputPO}>
-              <label htmlFor="otp">
-                Enter OTP<span className="requiredSpan">*</span>
-              </label>
-              <br />
-              <div className={style.otpInputs}>
-                <input type="text" placeholder="Enter the otp" id="otp" />
-                <button>Resend OTP</button>
-              </div>
-            </div>
-          </div>
-
           <div className={style.loginDiv}>
             <button onClick={login}>{load ? <Load /> : "Log In"}</button>
 
