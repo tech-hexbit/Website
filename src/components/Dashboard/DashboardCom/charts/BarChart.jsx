63dc410e6e0a1d4db49bacd32bedbaeba2b9ab6d
Rishav Singh <rishavrajcrj@gmail.com>
2023-12-28 16:21:47 +0530

    Update BarChart.jsx

--- src/components/Dashboard/DashboardCom/charts/BarChart.jsx
+++ src/components/Dashboard/DashboardCom/charts/BarChart.jsx
@@ -10,9 +10,33 @@ import AuthContext from "./../../../../store/auth-context";
 import { useContext, useEffect, useState } from "react";
 
 export default function BarChart() {
-  const[daysdata,setDaysData]=useState(false);
+  const [daysdata, setDaysData] = useState(false);
+  const [currColor, setCurrColor] = useState([]);
   const authCtx = useContext(AuthContext);
-  const[days,setDays]=useState([]);
+  const [days, setDays] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get("/api/website/DashBoard/SellerActivity", {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+
+      if (res.data.success) {
+        let randomColors = [];
+
+        randomColors = generateRandomColors(7);
+
+        setCurrColor(randomColors);
+
+        setDays(res.data.days);
+      }
+    } catch (e) {
+      console.log("error in fetching data", e.message);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   function generateRandomColors(length) {
     const colors = [];
@@ -29,51 +53,23 @@ export default function BarChart() {
     return colors;
   }
 
-  const fetchData=async()=>{
-    try{
-      const res=await axios.get('/api/website/DashBoard/SellerActivity',{
-        headers: { Authorization: `${authCtx.token}` },
-      });
-      if(res.data.success){
-        // setDaysData(true);
-        // console.log(daysdata);
-        // console.log(res.data.days);
-        setDays(res.data.days);
-      }
-    }catch(e){
-      console.log('error in fetching data',e.message);
-    }
-  }
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  let randomColors = [];
-        if (days.length > 2) {
-          randomColors = generateRandomColors(data.length - 2);
-          randomColors.push("#d8b4fe");
-          randomColors.push("#f3e8ff");
-        } else {
-          randomColors = ["#d8b4fe", "#f3e8ff"];
-        }
-  console.log(randomColors);
-  console.log('days',days[0]);
-
   const data = {
-    labels: ["Mon", "Tues", "Wed","Thurs","Fri","Sat","Sun"],
+    labels: ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
     datasets: [
       {
         label: "Cost",
         data: days[0],
-        backgroundColor: randomColors,
+        backgroundColor: currColor,
         maxBarThickness: 14,
       },
     ],
   };
+
   const options = {
     maintainAspectRatio: false,
     responsive: true,
   };
+
   return (
     <div className="barChart">
       <Chart data={data} type="bar" options={options} />
