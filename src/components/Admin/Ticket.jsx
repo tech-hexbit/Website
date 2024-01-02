ab4d11bf18136ad0c9d9893a8004366a38d647ef
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-02 19:00:08 +0530

    Update Ticket.jsx

--- src/components/Admin/Ticket.jsx
+++ src/components/Admin/Ticket.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 // components
+import Table from "./Ticket/Table";
 import Header from "./Ticket/Header";
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      Table
+      <Table />
     </div>
   );
 }
