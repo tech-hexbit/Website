4de704576b2bbca0d3ec4e778a0e146fdccd7932
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-03 00:07:39 +0530

    Update PastTicket.jsx

--- src/components/Dashboard/HelpDesk/PastTicket.jsx
+++ src/components/Dashboard/HelpDesk/PastTicket.jsx
@@ -67,19 +67,27 @@ export default function PastTicket() {
     <>
       <div className={pt.main}>
         <h1>Past Ticket</h1>
-        <div className={pt.grid}>
-          <div className={pt.child}>#HX00001</div>
-          <div className={`${pt.child} ${pt.orange}`}>Pending</div>
-          <div className={pt.child}>#HX00002</div>
-          <div className={`${pt.child} ${pt.green}`}>Solved</div>
-          <div className={pt.child}>#HX00003</div>
-          <div className={`${pt.child} ${pt.green}`}>Solved</div>
-          <div className={pt.child}>#HX00004</div>
-          <div className={`${pt.child} ${pt.green}`}>Solved</div>
-          <Link to="/me/help/desk/ViewMore" className={pt.btn}>
-            View More
-          </Link>
-        </div>
+        {load ? (
+          <div className="loadCenterDiv">
+            <Load />
+          </div>
+        ) : (
+          <>
+            <div className={pt.grid}>
+              <div className={pt.child}>#HX00001</div>
+              <div className={`${pt.child} ${pt.orange}`}>Pending</div>
+              <div className={pt.child}>#HX00002</div>
+              <div className={`${pt.child} ${pt.green}`}>Solved</div>
+              <div className={pt.child}>#HX00003</div>
+              <div className={`${pt.child} ${pt.green}`}>Solved</div>
+              <div className={pt.child}>#HX00004</div>
+              <div className={`${pt.child} ${pt.green}`}>Solved</div>
+              <Link to="/me/help/desk/ViewMore" className={pt.btn}>
+                View More
+              </Link>
+            </div>
+          </>
+        )}
       </div>
 
       <Alert variant={variants} val={setError} />
