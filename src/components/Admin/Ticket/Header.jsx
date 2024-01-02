fa9abb886d7e39951779b8290553ead4dbf68e66
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-02 19:05:29 +0530

    Update Header.jsx

--- src/components/Admin/Ticket/Header.jsx
+++ src/components/Admin/Ticket/Header.jsx
@@ -3,6 +3,10 @@ import React from "react";
 // css
 import HCss from "./Css/Header.modue.css";
 
-export default function Header() {
-  return <div>Header</div>;
+export default function Header(props) {
+  return (
+    <>
+      <b className={HCss.salesTicketsAll3}>{props.title}</b>
+    </>
+  );
 }
