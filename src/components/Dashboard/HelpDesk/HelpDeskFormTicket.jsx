871eab2f1579ffd86372c036512395bc08c17094
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-03 00:27:03 +0530

    Update HelpDeskFormTicket.jsx

--- src/components/Dashboard/HelpDesk/HelpDeskFormTicket.jsx
+++ src/components/Dashboard/HelpDesk/HelpDeskFormTicket.jsx
@@ -13,25 +13,6 @@ export default function HelpDeskFormTicket(props) {
   return (
     <>
       <div className={hdft.main}>
-        {/* <h1>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="28"
-            height="48"
-            viewBox="0 0 28 48"
-            fill="none"
-            onClick={() => {
-              props.setSubmitted(false);
-            }}
-          >
-            <path
-              d="M26.5504 1.47734C25.3254 0.252344 23.3504 0.252344 22.1254 1.47734L1.35039 22.2523C0.375391 23.2273 0.375391 24.8023 1.35039 25.7773L22.1254 46.5523C23.3504 47.7774 25.3254 47.7774 26.5504 46.5523C27.7754 45.3274 27.7754 43.3523 26.5504 42.1273L8.45039 24.0023L26.5754 5.87734C27.7754 4.67734 27.7754 2.67734 26.5504 1.47734Z"
-              fill="black"
-            />
-          </svg>
-          Contact us
-        </h1> */}
-
         <div className={hdft.icon}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
