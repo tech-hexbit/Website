91ad41ab498076d2ad693bcafbc7b7991a5d84a1
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-04 19:49:40 +0530

    Update StoreVerify.jsx

--- src/Pages/StoreVerify.jsx
+++ src/Pages/StoreVerify.jsx
@@ -4,5 +4,27 @@ import React from "react";
 import SvCss from "./Css/StoreVerify.module.css";
 
 export default function StoreVerify() {
-  return <div className={SvCss.mDiv}>StoreVerify d</div>;
+  // Account Holder Name
+  // Account Number
+  // Bank Name
+  // City
+  // Branch
+  // Ifsc Code
+  return (
+    <div className={SvCss.mDiv}>
+      <p>Create Your Store</p>
+
+      <div>
+        <div className={SvCss.InpDiv}>
+          <p>Account Holder Name</p>
+          <input type="text" name="" id="" placeholder="Chandler Muriel Bing" />
+        </div>
+
+        <div className={SvCss.InpDiv}>
+          <p> Account Number</p>
+          <input type="text" name="" id="" placeholder="2134XXXXXX1416" />
+        </div>
+      </div>
+    </div>
+  );
 }
