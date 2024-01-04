7f5e9d38f470647b1e6abe0317a8d7b0ec23f0bf
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-04 20:11:45 +0530

    Update StoreVerify.jsx

--- src/Pages/StoreVerify.jsx
+++ src/Pages/StoreVerify.jsx
@@ -6,6 +6,10 @@ import axios from "axios";
 // state
 import AuthContext from "../../store/auth-context";
 
+// MicroInteraction
+import Load from "./../MicroInteraction/Load";
+import { Alert } from "./../MicroInteraction/Alert";
+
 // css
 import SvCss from "./Css/StoreVerify.module.css";
 
@@ -19,42 +23,56 @@ export default function StoreVerify() {
     val: false,
   });
   return (
-    <div className={SvCss.mDiv}>
-      <p className={SvCss.CreateYourStore}>Create Your Store</p>
+    <>
+      <div className={SvCss.mDiv}>
+        <p className={SvCss.CreateYourStore}>Create Your Store</p>
 
-      <div>
-        <div className={SvCss.InpDiv}>
-          <p>Account Holder Name</p>
-          <input type="text" name="" id="" placeholder="Chandler Muriel Bing" />
+        <div>
+          <div className={SvCss.InpDiv}>
+            <p>Account Holder Name</p>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Chandler Muriel Bing"
+            />
+          </div>
+
+          <div className={SvCss.InpDiv}>
+            <p>Account Number</p>
+            <input type="text" name="" id="" placeholder="2134XXXXXX1416" />
+          </div>
+
+          <div className={SvCss.InpDiv}>
+            <p>Bank Name</p>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="State Bank of India"
+            />
+          </div>
+
+          <div className={SvCss.InpDiv}>
+            <p>Branch</p>
+            <input type="text" name="" id="" placeholder="Jamtara Bazar" />
+          </div>
+
+          <div className={SvCss.InpDiv}>
+            <p>IFSC Code</p>
+            <input type="text" name="" id="" placeholder="SBINXXXXXX5" />
+          </div>
+
+          <div className={SvCss.InpDiv}>
+            <p>City</p>
+            <input type="text" name="" id="" placeholder="Jamtara" />
+          </div>
         </div>
 
-        <div className={SvCss.InpDiv}>
-          <p>Account Number</p>
-          <input type="text" name="" id="" placeholder="2134XXXXXX1416" />
-        </div>
-
-        <div className={SvCss.InpDiv}>
-          <p>Bank Name</p>
-          <input type="text" name="" id="" placeholder="State Bank of India" />
-        </div>
-
-        <div className={SvCss.InpDiv}>
-          <p>Branch</p>
-          <input type="text" name="" id="" placeholder="Jamtara Bazar" />
-        </div>
-
-        <div className={SvCss.InpDiv}>
-          <p>IFSC Code</p>
-          <input type="text" name="" id="" placeholder="SBINXXXXXX5" />
-        </div>
-
-        <div className={SvCss.InpDiv}>
-          <p>City</p>
-          <input type="text" name="" id="" placeholder="Jamtara" />
-        </div>
+        <button className={SvCss.SubmitBtn}>Submit</button>
       </div>
 
-      <button className={SvCss.SubmitBtn}>Submit</button>
-    </div>
+      <Alert variant={variants} val={setError} />
+    </>
   );
 }
