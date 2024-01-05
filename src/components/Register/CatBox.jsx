ebbaeb8d0a38d8159dfaa3ebdd96a29f583ae78b
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-05 14:21:49 +0530

    Update CatBox.jsx

--- src/components/Register/CatBox.jsx
+++ src/components/Register/CatBox.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function CatBox(props) {
-  return <div>CatBox</div>;
+  return (
+    <div>
+      <input
+        type="checkbox"
+        name={`${props.val.name}`}
+        id={`${props.val.name}`}
+      />
+      <label htmlFor={`${props.val.name}`}>{props.val.name}</label>
+    </div>
+  );
 }
