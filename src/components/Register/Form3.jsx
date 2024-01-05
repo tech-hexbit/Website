674e84712b3e028cc119d0f46a673279e0d6c4c7
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-05 20:57:42 +0530

    Update Form3.jsx

--- src/components/Register/Form3.jsx
+++ src/components/Register/Form3.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 // MicroInteraction
 import Load from "../../MicroInteraction/LoadBlack";
+import LoadWhite from "../../MicroInteraction/Load";
 import { Alert } from "./../../MicroInteraction/Alert";
 
 // axios
@@ -120,9 +121,9 @@ export default function Form3(props) {
             <div></div>
             <div>
               <button onClick={nextFN}>
-                {load ? (
+                {props.load ? (
                   <>
-                    <Load />
+                    <LoadWhite />
                   </>
                 ) : (
                   <>{props.button}</>
@@ -133,6 +134,7 @@ export default function Form3(props) {
                   height="10"
                   viewBox="0 0 21 10"
                   fill="none"
+                  className={FCss.buttonSvg}
                 >
                   <path
                     d="M1.81791 5.97787L16.9094 5.9877L14.0445 8.32675C13.577 8.71004 13.577 9.32919 14.0445 9.71252C14.512 10.0958 15.2792 10.0958 15.7466 9.71252L20.6493 5.69288C21.1168 5.30958 21.1168 4.69044 20.6493 4.30711L15.7466 0.287469C15.2791 -0.095823 14.5119 -0.095823 14.0445 0.287469C13.577 0.670761 13.577 1.28991 14.0445 1.67324L16.9094 4.02217L1.81791 4.01234C1.15862 4.01234 0.619141 4.45461 0.619141 4.9952C0.619141 5.53578 1.15857 5.97805 1.81791 5.97805V5.97787Z"
