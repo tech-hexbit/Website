cff39782c977046b05bd282d62de34f0f9900d27
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-06 22:42:05 +0530

    Update OrderLayUpdate.jsx

--- src/components/Dashboard/Sales/OrderLayUpdate.jsx
+++ src/components/Dashboard/Sales/OrderLayUpdate.jsx
@@ -19,27 +19,27 @@ export default function OrderLayUpdate(props) {
       <div className={OLCss.mDiv}>
         <div className={OLCss.contendDiv}>
           <p className={OLCss.UpdateOrderPTag}>UPDATE ORDER STATUS</p>
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-x"
-              className={OLCss.closeBtn}
-              onClick={() => {
-                props.setEdit(false);
-              }}
-            >
-              <path d="M18 6 6 18" />
-              <path d="m6 6 12 12" />
-            </svg>
-          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-x"
+            className={OLCss.closeBtn}
+            onClick={() => {
+              props.setEdit(false);
+            }}
+          >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+          </svg>
+
+          <div></div>
         </div>
       </div>
     </>
