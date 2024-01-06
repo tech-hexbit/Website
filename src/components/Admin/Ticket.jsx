8ed8173bfa368e7683966b99e51d7240639af325
Niyojit <niyojitchoudhari@gmail.com>
2024-01-06 23:33:16 +0530

    final2 response

--- src/components/Admin/Ticket.jsx
+++ src/components/Admin/Ticket.jsx
@@ -3,26 +3,52 @@ import TicCss from './Css/Tickets.module.css';
 import TicketHeader from './TicketHeader';
 import TicketRow from './TicketRow';
 
+
 const Ticket = () => {
+  const ticketData = [
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Solved" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Solved" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Solved" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Pending" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Solved" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Pending" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Pending" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Pending" },
+    { id: "#20462", storeId: "#20462", storeName: "#20462", subject: "#20462", date: "13/05/2022", status: "Solved" },
+
+  ];
+
+
+
+
   return (
     <div className={TicCss.mDiv}>
-      <p className={TicCss.TicketsPTag}>Tickets: ALL</p>
+      <div className={TicCss.headContain}>
+
+       <svg width="9"
+            height="14"
+            viewBox="0 0 9 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 13L1 7L8 1"
+              stroke="#031849" stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round" />
+          </svg>
+      <p className={TicCss.TicketsPTag}>Tickets: All</p>
+      </div>
 
       <div className={TicCss.table}>
         <table className={TicCss.tableCatTTag}
-                style={{ borderCollapse: "collapse" }}>
+          style={{ borderCollapse: "collapse" }}>
+
+         
           <TicketHeader />
           <tbody>
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Solved" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Solved" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Pending" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Pending" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Solved" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Pending" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Solved" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Solved" />
-            <TicketRow id="#20462" storeId="#20462" storeName="#20462" subject="#20462" date="13/05/2022" status="Solved" />
-            
+            {ticketData.map((ticket, index) => (
+              <TicketRow key={index} {...ticket} />
+            ))}
+
           </tbody>
         </table>
       </div>
--- src/components/Admin/TicketRow.jsx
+++ src/components/Admin/TicketRow.jsx
@@ -2,30 +2,42 @@ import React from 'react';
 import TicCss from './Css/Tickets.module.css';
 
 const TicketRow = ({ id, storeId, storeName, subject, date, status }) => {
-  return (
-    <tr className={TicCss.frameGroup}>
-      <td data-cell ="Id">{id}</td>
-      <td data-cell ="storeId">{storeId}</td>
-      <td data-cell ="storeName">{storeName}</td>
-      <td data-cell ="subject">{subject}</td>
-      <td data-cell ="date">{date}</td>
-      <td data-cell ="status">
-        <div
-          className={status === 'Solved' ? TicCss.solvedWrapper : TicCss.pendingWrapper}
-        >
-          <div className={status === 'Solved' ? TicCss.solvedHead : TicCss.pendingHead}>{status}</div>
-        </div>
-      </td>
-      <td data-cell ="Action">
-        <div className={TicCss.frameContainer}>
-          <div className={TicCss.tickMarkWrapper}>
-          <img className={TicCss.pathIcon} alt="" src="/src/assets/tickets/path@2x.png" />
-          </div>
-         
-        </div>
-      </td>
-    </tr>
-  );
+    return (
+        <tr className={TicCss.frameGroup}>
+            <td data-cell="Id">{id}</td>
+            <td data-cell="storeId">{storeId}</td>
+            <td data-cell="storeName">{storeName}</td>
+            <td data-cell="subject">{subject}</td>
+            <td data-cell="date">{date}</td>
+            <td data-cell="status">
+                <div
+                    className={status === 'Solved' ? TicCss.solvedWrapper : TicCss.pendingWrapper}
+                >
+                    <div className={status === 'Solved' ? TicCss.solvedHead : TicCss.pendingHead}>{status}</div>
+                </div>
+            </td>
+            <td data-cell="Action">
+                <div className={TicCss.frameContainer}>
+                    <div className={TicCss.tickMarkWrapper}>
+                        <svg width="27"
+                            height="27"
+                            viewBox="0 0 27 27"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path id="Path"
+                                d="M7.2051 21.2979L2.38586 26.1167C1.56691 26.9355 0.166626 26.3556 
+                                0.166626 25.1975V4.39961C0.166626 2.24591 1.91272 0.5 4.06663 0.5H22
+                                .2666C24.4205 0.5 26.1666 2.24591 26.1666 4.39961V17.3983C26.1666 
+                                19.552 24.4205 21.2979 22.2666 21.2979H7.2051Z"
+                                 fill="black" 
+                                 fill-opacity="0.71" />
+                        </svg>
+                    </div>
+
+                </div>
+            </td>
+        </tr>
+    );
 };
 
 export default TicketRow;
