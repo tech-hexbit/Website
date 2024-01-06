34b367dfa58563296e7cfb89b787a090ad9fa9a6
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-06 20:15:25 +0530

    Create OrderLayUpdate.jsx

---
+++ src/components/Dashboard/Sales/OrderLayUpdate.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useContext } from "react";
+
+// state
+import AuthContext from "./../../../store/auth-context";
+
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import Load from "./../../../MicroInteraction/LoadBlack";
+
+// Css
+import osCss from "./Css/OrderLayUpdate.module.css";
+
+export default function OrderLayUpdate(props) {
+  return <div>OrderLayUpdate</div>;
+}
