354255e0e6f4227cb75f114374bd48ec0f9569b0
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-06 12:52:37 +0530

    Update PaymentRequest.jsx

--- src/components/Dashboard/Payment/PaymentRequest.jsx
+++ src/components/Dashboard/Payment/PaymentRequest.jsx
@@ -65,7 +65,6 @@ export default function PaymentRequest() {
                   IfscCode={bank.IfscCode}
                   key={key}
                 />
-                // </div>
               ))}
             </>
           ) : (
