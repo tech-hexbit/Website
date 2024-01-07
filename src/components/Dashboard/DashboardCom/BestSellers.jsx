1f4c1e76a9b13b0b8cc7e4df5bf441cb36d869f9
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-08 00:12:06 +0530

    Update BestSellers.jsx

--- src/components/Dashboard/DashboardCom/BestSellers.jsx
+++ src/components/Dashboard/DashboardCom/BestSellers.jsx
@@ -71,7 +71,6 @@ export default function BestSellers() {
                   <th id={BSCss.th} className={BSCss.sales}>
                     Sales
                   </th>
-                  {/* <th id={BSCss.th}>Ratings</th> */}
                 </tr>
 
                 {orderDel ? (
@@ -95,9 +94,6 @@ export default function BestSellers() {
                           <td data-cell="Amount" id={BSCss.td}>
                             {val.totalSold}
                           </td>
-                          {/* <td data-cell="Ratings" id={BSCss.td}>
-                            {val.rating.toFixed(1)}
-                          </td> */}
                         </tr>
                       );
                     })}
