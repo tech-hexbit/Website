788fcba7fe53d14e7a4e764a8f7437bc02faa94f
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-07 20:19:45 +0530

    Update UpdateState.jsx

--- src/components/Dashboard/Sales/UpdateState.jsx
+++ src/components/Dashboard/Sales/UpdateState.jsx
@@ -36,25 +36,6 @@ export default function UpdateState(props) {
         // }}
         className={osCss.tdStateBlock}
       >
-        <>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-external-link"
-          >
-            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-            <polyline points="15 3 21 3 21 9" />
-            <line x1="10" x2="21" y1="14" y2="3" />
-          </svg>
-        </>
-
         {edit ? (
           <OrderLayUpdate
             setEdit={setEdit}
@@ -64,25 +45,48 @@ export default function UpdateState(props) {
             loadDataState={props.loadDataState}
           />
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-pencil"
-            className={osCss.lucidePencil}
-            onClick={() => {
-              setEdit(!edit);
-            }}
-          >
-            <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-            <path d="m15 5 4 4" />
-          </svg>
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-external-link"
+              className={osCss.lucidePencil}
+              onClick={() => {
+                setEdit(!edit);
+              }}
+            >
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+              <polyline points="15 3 21 3 21 9" />
+              <line x1="10" x2="21" y1="14" y2="3" />
+            </svg>
+
+            {/* <svg
+             xmlns="http://www.w3.org/2000/svg"
+             width="16"
+             height="16"
+             viewBox="0 0 24 24"
+             fill="none"
+             stroke="currentColor"
+             stroke-width="2"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+             class="lucide lucide-pencil"
+             className={osCss.lucidePencil}
+             onClick={() => {
+               setEdit(!edit);
+             }}
+           >
+             <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+             <path d="m15 5 4 4" />
+           </svg> */}
+          </>
         )}
       </td>
     </>
