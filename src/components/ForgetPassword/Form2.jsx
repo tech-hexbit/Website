c0ee1711e75a5b3371b02d5e08a1bb829716f21c
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-08 01:18:05 +0530

    Update Form2.jsx

--- src/components/ForgetPassword/Form2.jsx
+++ src/components/ForgetPassword/Form2.jsx
@@ -4,5 +4,41 @@ import React, { useEffect, useState } from "react";
 import fpstyle from "./CSS/ForgetPassword.module.css";
 
 export default function Form2() {
-  return <div>Form2</div>;
+  return (
+    <>
+      <div className={fpstyle.inputEP}>
+        <label>Create New Password *</label>
+        <br />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          id="password"
+          name="password"
+          value={input.newpwd}
+          onChange={(e) => {
+            setInput({ ...input, newpwd: e.target.value });
+          }}
+        />
+      </div>
+
+      {/* Phone */}
+      <div className={fpstyle.inputPO}>
+        <label htmlFor="password">
+          Confirm New Password
+          <span style={{ color: "#350B5E" }}>*</span>
+        </label>
+        <br />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          id="password"
+          name="password"
+          value={input.confirmpwd}
+          onChange={(e) => {
+            setInput({ ...input, confirmpwd: e.target.value });
+          }}
+        />
+      </div>
+    </>
+  );
 }
