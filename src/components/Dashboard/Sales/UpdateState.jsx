c45256254588c20b68e3aa0ba26b67466861a282
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-07 20:17:08 +0530

    Update UpdateState.jsx

--- src/components/Dashboard/Sales/UpdateState.jsx
+++ src/components/Dashboard/Sales/UpdateState.jsx
@@ -22,21 +22,38 @@ export default function UpdateState(props) {
     <>
       <td
         data-cell="DELIVERY STATUS"
-        style={{
-          color:
-            props.state == "Created"
-              ? "#7925c7"
-              : props.state == "Accepted"
-              ? "#FEC107"
-              : props.state == "In-progress"
-              ? "#3F81E0"
-              : props.state == "Completed"
-              ? "#4bb543"
-              : "#D0342C",
-        }}
+        // style={{
+        //   color:
+        //     props.state == "Created"
+        //       ? "#7925c7"
+        //       : props.state == "Accepted"
+        //       ? "#FEC107"
+        //       : props.state == "In-progress"
+        //       ? "#3F81E0"
+        //       : props.state == "Completed"
+        //       ? "#4bb543"
+        //       : "#D0342C",
+        // }}
         className={osCss.tdStateBlock}
       >
-        <>{props.state}</>
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-external-link"
+          >
+            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+            <polyline points="15 3 21 3 21 9" />
+            <line x1="10" x2="21" y1="14" y2="3" />
+          </svg>
+        </>
 
         {edit ? (
           <OrderLayUpdate
