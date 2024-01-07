2d8bcf8ce5b3efb475bce101bc2cc5186ad0fcec
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-08 01:17:08 +0530

    Update Form2.jsx

---
+++ src/components/ForgetPassword/Form2.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+
+//css
+import fpstyle from "./CSS/ForgetPassword.module.css";
+
+export default function Form2() {
+  return <div>Form2</div>;
+}
