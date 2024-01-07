58ae872e09f541fb0ba048966590e6dfbd6dc9ff
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-08 01:13:18 +0530

    Update PwdChanged.jsx

--- src/components/ForgetPassword/PwdChanged.jsx
+++ src/components/ForgetPassword/PwdChanged.jsx
@@ -5,18 +5,21 @@ import { Link } from "react-router-dom";
 import pwstyle from "./CSS/PwdChanged.module.css";
 
 //assets
-import succIcon from "../../assets/SuccessIcon.png"
+import succIcon from "../../assets/SuccessIcon.png";
 
-
-export default function PwdChanged(){
-    return(
-        <div>
-            <h1 className={pwstyle.pwd}>Password Changed</h1>
-            <div className={pwstyle.container}>
-                <img className={pwstyle.icon} src={succIcon}/>
-            </div>
-            <p className={pwstyle.text}>Password Changed Successfully</p>
-            <p className={pwstyle.head}><Link className={pwstyle.linkStyle} to="/signin">Login now</Link></p>
-        </div>
-    )
+export default function PwdChanged() {
+  return (
+    <div>
+      <h1 className={pwstyle.pwd}>Password Changed</h1>
+      <div className={pwstyle.container}>
+        <img className={pwstyle.icon} src={succIcon} />
+      </div>
+      <p className={pwstyle.text}>Password Changed Successfully</p>
+      <p className={pwstyle.head}>
+        <Link className={pwstyle.linkStyle} to="/signin">
+          Login now
+        </Link>
+      </p>
+    </div>
+  );
 }
