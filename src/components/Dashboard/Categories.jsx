08c6e091c4d83ea27a80f6fcfa5819e9c6584e8e
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-08 20:50:32 +0530

    Update Categories.jsx

--- src/components/Dashboard/Categories.jsx
+++ src/components/Dashboard/Categories.jsx
@@ -78,8 +78,30 @@ export default function Categories() {
   return (
     <div className={Ccss.mDiv}>
       {/* Header */}
-      <div>
+      <div className={Ccss.headerFlex}>
         <Header name="Inventory" />
+        <div className={Ccss.addCsv}>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-arrow-down-up"
+            >
+              <path d="m3 16 4 4 4-4" />
+              <path d="M7 20V4" />
+              <path d="m21 8-4-4-4 4" />
+              <path d="M17 4v16" />
+            </svg>
+            <span className={Ccss.hideTxt}>Low Inventory</span>
+          </button>
+        </div>
       </div>
 
       <DataMain />
