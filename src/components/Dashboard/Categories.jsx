fbfec31d07b201bdac406f5e89447c5f12a60e68
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-08 09:56:55 +0530

    Update Categories.jsx

--- src/components/Dashboard/Categories.jsx
+++ src/components/Dashboard/Categories.jsx
@@ -127,7 +127,28 @@ export default function Categories() {
                             <td data-cell="Return Window">
                               {val["@ondc/org/return_window"]}
                             </td>
-                            <td data-cell="Published on">{val.when.date}</td>
+                            <td data-cell="Published on">
+                              <div className={Ccss.warnDiv}>
+                                {val.when.date}
+
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="16"
+                                  height="16"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  class="lucide lucide-alert-circle"
+                                >
+                                  <circle cx="12" cy="12" r="10" />
+                                  <line x1="12" x2="12" y1="8" y2="12" />
+                                  <line x1="12" x2="12.01" y1="16" y2="16" />
+                                </svg>
+                              </div>
+                            </td>
                           </tr>
                         </>
                       );
