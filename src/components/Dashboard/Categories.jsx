db6b4061f87d9e019945f233f38873bdc1d49d86
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-08 09:58:10 +0530

    Update Categories.jsx

--- src/components/Dashboard/Categories.jsx
+++ src/components/Dashboard/Categories.jsx
@@ -109,9 +109,9 @@ export default function Categories() {
                         <>
                           <tr
                             key={key}
-                            className={
-                              val.quantity.maximum.count <= 5 ? "alertTrue" : ""
-                            }
+                            // className={
+                            //   val.quantity.maximum.count <= 5 ? "alertTrue" : ""
+                            // }
                           >
                             <td data-cell="Name">{val.descriptor.name}</td>
                             <td data-cell="Price">
@@ -131,22 +131,26 @@ export default function Categories() {
                               <div className={Ccss.warnDiv}>
                                 {val.when.date}
 
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="16"
-                                  height="16"
-                                  viewBox="0 0 24 24"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  stroke-width="2"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  class="lucide lucide-alert-circle"
-                                >
-                                  <circle cx="12" cy="12" r="10" />
-                                  <line x1="12" x2="12" y1="8" y2="12" />
-                                  <line x1="12" x2="12.01" y1="16" y2="16" />
-                                </svg>
+                                {val.quantity.maximum.count <= 5 ? (
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="lucide lucide-alert-circle"
+                                  >
+                                    <circle cx="12" cy="12" r="10" />
+                                    <line x1="12" x2="12" y1="8" y2="12" />
+                                    <line x1="12" x2="12.01" y1="16" y2="16" />
+                                  </svg>
+                                ) : (
+                                  ""
+                                )}
                               </div>
                             </td>
                           </tr>
