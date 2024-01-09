78e7ef6752c729f8f31b86e74a7f180df0752aac
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-09 09:43:20 +0530

    Update TopDetails.jsx

--- src/components/Dashboard/Sales/Details/TopDetails.jsx
+++ src/components/Dashboard/Sales/Details/TopDetails.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function TopDetails() {
+// css
+import odcss from "./../../Css/Orderdetails.module.css";
+
+export default function TopDetails({ del }) {
+  console.log(del);
   return (
     <>
       <div className={odcss.details}>
@@ -28,7 +32,7 @@ export default function TopDetails() {
                 <circle cx="12" cy="12" r="10" />
               </svg>
 
-              {res.ONDCBilling.name}
+              {del.ONDCBilling.name}
             </div>
 
             {/* <Mail /> */}
@@ -49,7 +53,7 @@ export default function TopDetails() {
                 <rect width="20" height="16" x="2" y="4" rx="2" />
                 <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
               </svg>
-              {res.ONDCBilling.email}
+              {del.ONDCBilling.email}
             </div>
 
             {/* <Phone /> */}
@@ -69,7 +73,7 @@ export default function TopDetails() {
               >
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
               </svg>
-              {res.ONDCBilling.phone}
+              {del.ONDCBilling.phone}
             </div>
           </div>
         </div>
@@ -79,22 +83,22 @@ export default function TopDetails() {
           <div className={odcss.SubHeading}>Shipping address</div>
           <div className={odcss.ContentDels}>
             <div className={odcss.adress}>
-              <p className={odcss.adl1}>Name: {res.ONDCBilling.address.name}</p>
+              <p className={odcss.adl1}>Name: {del.ONDCBilling.address.name}</p>
               <p className={odcss.adl1}>
-                Building: {res.ONDCBilling.address.building}
+                Building: {del.ONDCBilling.address.building}
               </p>
               <p className={odcss.adl1}>
-                Locality: {res.ONDCBilling.address.locality}
+                Locality: {del.ONDCBilling.address.locality}
               </p>
-              <p className={odcss.adl1}>City: {res.ONDCBilling.address.city}</p>
+              <p className={odcss.adl1}>City: {del.ONDCBilling.address.city}</p>
               <p className={odcss.adl1}>
-                State: {res.ONDCBilling.address.state}
-              </p>
-              <p className={odcss.adl1}>
-                Country: {res.ONDCBilling.address.country}
+                State: {del.ONDCBilling.address.state}
               </p>
               <p className={odcss.adl1}>
-                Area Code: {res.ONDCBilling.address.area_code}
+                Country: {del.ONDCBilling.address.country}
+              </p>
+              <p className={odcss.adl1}>
+                Area Code: {del.ONDCBilling.address.area_code}
               </p>
             </div>
           </div>
@@ -105,23 +109,23 @@ export default function TopDetails() {
           <div className={odcss.SubHeading}>Payment details</div>
           <div className={odcss.ContentDels}>
             <p className={odcss.pl1}>
-              Transaction ID : {res.payment.params.transaction_id}
+              Transaction ID : {del.payment.params.transaction_id}
             </p>
             <p className={odcss.pl1}>
-              Payment method : {res.payment.tl_method}
+              Payment method : {del.payment.tl_method}
             </p>
             <p className={odcss.pl1}>
               Card holder name :{" "}
-              {res.payment["@ondc/org/settlement_details"][0].beneficiary_name}
+              {del.payment["@ondc/org/settlement_details"][0].beneficiary_name}
             </p>
             <p className={odcss.pl1}>
               Card number/ UPI ID :{" "}
               {
-                res.payment["@ondc/org/settlement_details"][0]
+                del.payment["@ondc/org/settlement_details"][0]
                   .settlement_bank_account_no
               }
             </p>
-            <p className={odcss.pl1}>Total amount : ₹ {res.amount}</p>
+            <p className={odcss.pl1}>Total amount : ₹ {del.amount}</p>
           </div>
         </div>
       </div>
