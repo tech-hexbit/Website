e8ed2459fee4cd84f31be507577d7a9c44656149
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-10 01:09:06 +0530

    Update HelpDeskFormTableDetail.jsx

--- src/components/Dashboard/HelpDesk/HelpDeskFormTableDetail.jsx
+++ src/components/Dashboard/HelpDesk/HelpDeskFormTableDetail.jsx
@@ -8,33 +8,46 @@ export default function HelpDeskFormTableDetail({ tableData }) {
   return (
     <div>
       <div>
+        {/* ID */}
         <h3 className={tableDetailStyle.headerText}>
           TICKET ID :#HX{tableData._id.slice(-5)}
         </h3>
+
+        {/* SUBJECT */}
         <h3 className={tableDetailStyle.headerTextSecond}>
           SUBJECT : {tableData.subject}
         </h3>
       </div>
+
       <div className={tableDetailStyle.detailsSection}>
         <div>
+          {/* Name */}
           <div className={tableDetailStyle.detailsText}>
             <p className={tableDetailStyle.name}>Name</p>
             <p className={tableDetailStyle.nameSecond}>{tableData.name}</p>
           </div>
+
+          {/* Email */}
           <div className={tableDetailStyle.detailsText}>
             <p className={tableDetailStyle.name}>Email</p>
             <p className={tableDetailStyle.nameSecond}>{tableData.emailID}</p>
           </div>
+
+          {/* Store Id */}
           <div className={tableDetailStyle.detailsText}>
             <p className={tableDetailStyle.name}>Store Id</p>
             <p className={tableDetailStyle.nameSecond}>
               #{tableData.StoreID.slice(-4)}
             </p>
           </div>
+
+          {/* Store Name */}
           <div className={tableDetailStyle.detailsText}>
             <p className={tableDetailStyle.name}>Store Name</p>
             <p className={tableDetailStyle.nameSecond}>{tableData.StoreName}</p>
           </div>
+
+          {/* Request Time */}
           <div className={tableDetailStyle.timeDate}>
             <p className={tableDetailStyle.reqDate}>Request Time and Date</p>
             <p className={tableDetailStyle.reqDateSecond}>
