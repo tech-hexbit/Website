40a8c5bb84624c9d3ea4c370d8450ca7915f57f8
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-10 23:09:36 +0530

    Update PaymentTable.jsx

--- src/components/Dashboard/Payment/PaymentTable.jsx
+++ src/components/Dashboard/Payment/PaymentTable.jsx
@@ -120,68 +120,69 @@ export default function PaymentTable() {
         ) : (
           <>
             {showData ? (
-              ""
+              <>
+                <table className={pt.trans_table}>
+                  <tr>
+                    <th>Tracking ID</th>
+                    <th>Product</th>
+                    <th>Customer</th>
+                    <th>Date</th>
+                    <th>Amount</th>
+                    <th>Payment Mode</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                  </tr>
+
+                  {showData.map((val, key) => (
+                    <tr key={key}>
+                      <td data-cell="tracking Id">{val.trackingId}</td>
+                      <td data-cell="order id">{val.product}</td>
+                      <td data-cell="name">{val.customer}</td>
+                      <td data-cell="date">{val.date}</td>
+                      <td data-cell="amount">{val.amount}</td>
+                      <td data-cell="payment mode">{val.paymentMode}</td>
+                      <td
+                        className={
+                          val.status === "Delivered & Eligible"
+                            ? pt.processed
+                            : val.status === "Delivered"
+                            ? pt.pending
+                            : pt.rejected
+                        }
+                        data-cell="status"
+                      >
+                        {val.status}
+                      </td>
+                      <td data-cell="action">
+                        <label className={pt.labelDiv}>
+                          <input type="checkbox" className={pt.CheckBoxInp} />
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className={pt.lucide}
+                          >
+                            <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+                            <path d="M12 12v9" />
+                            <path d="m16 16-4-4-4 4" />
+                          </svg>
+                        </label>
+                      </td>
+                    </tr>
+                  ))}
+                </table>
+              </>
             ) : (
               <div className="loadCenterDiv" id="loadPadding">
-                <Load />
+                No Orders
               </div>
             )}
-            <table className={pt.trans_table}>
-              <tr>
-                <th>Tracking ID</th>
-                <th>Product</th>
-                <th>Customer</th>
-                <th>Date</th>
-                <th>Amount</th>
-                <th>Payment Mode</th>
-                <th>Status</th>
-                <th>Action</th>
-              </tr>
-
-              {data.map((item, index) => (
-                <tr key={index}>
-                  <td data-cell="tracking Id">{item.trackingId}</td>
-                  <td data-cell="order id">{item.product}</td>
-                  <td data-cell="name">{item.customer}</td>
-                  <td data-cell="date">{item.date}</td>
-                  <td data-cell="amount">{item.amount}</td>
-                  <td data-cell="payment mode">{item.paymentMode}</td>
-                  <td
-                    className={
-                      item.status === "Delivered & Eligible"
-                        ? pt.processed
-                        : item.status === "Delivered"
-                        ? pt.pending
-                        : pt.rejected
-                    }
-                    data-cell="status"
-                  >
-                    {item.status}
-                  </td>
-                  <td data-cell="action">
-                    <label className={pt.labelDiv}>
-                      <input type="checkbox" className={pt.CheckBoxInp} />
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className={pt.lucide}
-                      >
-                        <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
-                        <path d="M12 12v9" />
-                        <path d="m16 16-4-4-4 4" />
-                      </svg>
-                    </label>
-                  </td>
-                </tr>
-              ))}
-            </table>
           </>
         )}
       </div>
