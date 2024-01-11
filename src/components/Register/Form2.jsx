fd2b53af467617da649b5088035502d5ec297ddf
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-11 23:49:23 +0530

    Update Form2.jsx

--- src/components/Register/Form2.jsx
+++ src/components/Register/Form2.jsx
@@ -240,8 +240,8 @@ export default function Form2(props) {
                 <>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="16"
+                    height="16"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
@@ -281,8 +281,8 @@ export default function Form2(props) {
                 <>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="16"
+                    height="16"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
