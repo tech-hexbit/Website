277ca143c98e4ef8e69eb716623c64571c96257c
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-11 22:12:48 +0530

    Update Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -51,7 +51,7 @@ export default function Profile() {
     setShowModal(false);
   };
 
-  console.log(authCtx.user);
+  console.log(authCtx.user.emailVerified);
 
   return (
     <>
@@ -67,31 +67,37 @@ export default function Profile() {
             <div className={PCss.CDiv}>
               {/* email verification */}
               <>
-                <p className={PCss.alert}>
-                  <Link to="#" onClick={handleLinkClick}>
-                    <div className={PCss.icon}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="lucide lucide-alert-circle"
-                      >
-                        <circle cx="12" cy="12" r="10" />
-                        <line x1="12" x2="12" y1="8" y2="12" />
-                        <line x1="12" x2="12.01" y1="16" y2="16" />
-                      </svg>
-                    </div>
-                    Email Verification Pending !! CLICK TO VERIFY
-                  </Link>
-                </p>
+                {authCtx.user.emailVerified ? (
+                  <></>
+                ) : (
+                  <>
+                    <p className={PCss.alert}>
+                      <Link to="#" onClick={handleLinkClick}>
+                        <div className={PCss.icon}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="lucide lucide-alert-circle"
+                          >
+                            <circle cx="12" cy="12" r="10" />
+                            <line x1="12" x2="12" y1="8" y2="12" />
+                            <line x1="12" x2="12.01" y1="16" y2="16" />
+                          </svg>
+                        </div>
+                        Email Verification Pending !! CLICK TO VERIFY
+                      </Link>
+                    </p>
 
-                {showModal && <VerifyEmail onClose={closePopup} />}
+                    {showModal && <VerifyEmail onClose={closePopup} />}
+                  </>
+                )}
               </>
 
               <Routes>
