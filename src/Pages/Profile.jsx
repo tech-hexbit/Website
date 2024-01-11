625a7e81d9dcf4acb56cc2f0aee827ebef4986a3
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-11 22:31:44 +0530

    Update Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -86,6 +86,8 @@ export default function Profile() {
     setShowModal(false);
   };
 
+  console.log(load);
+
   return (
     <>
       <Helmet>
@@ -104,38 +106,39 @@ export default function Profile() {
                   <></>
                 ) : (
                   <>
-                    <p className={PCss.alert}>
-                      <Link to="#" onClick={resendMail}>
-                        <div className={PCss.icon}>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="lucide lucide-alert-circle"
-                          >
-                            <circle cx="12" cy="12" r="10" />
-                            <line x1="12" x2="12" y1="8" y2="12" />
-                            <line x1="12" x2="12.01" y1="16" y2="16" />
-                          </svg>
-                        </div>
-                        Email Verification Pending !!{" "}
+                    {load ? (
+                      <div className="loadCenterDiv" id="loadPadding">
+                        <Load />
+                      </div>
+                    ) : (
+                      <p className={PCss.alert}>
                         <span>
-                          {load ? (
-                            <>
-                              <Load />
-                            </>
-                          ) : (
-                            "CLICK TO VERIFY"
-                          )}
+                          <>
+                            <Link to="#" onClick={resendMail}>
+                              <div className={PCss.icon}>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  class="lucide lucide-alert-circle"
+                                >
+                                  <circle cx="12" cy="12" r="10" />
+                                  <line x1="12" x2="12" y1="8" y2="12" />
+                                  <line x1="12" x2="12.01" y1="16" y2="16" />
+                                </svg>
+                              </div>
+                              Email Verification Pending !! CLICK TO VERIFY
+                            </Link>
+                          </>
                         </span>
-                      </Link>
-                    </p>
+                      </p>
+                    )}
 
                     {showModal && <VerifyEmail onClose={closePopup} />}
                   </>
