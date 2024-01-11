83dccc65f6af9e07961fcba48de6c472543fad6d
sanika30 <sanikathakur30@gmail.com>
2024-01-11 19:36:32 +0530

    update

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -52,33 +52,7 @@ export default function Profile() {
   };
   return (
     <>
-    {/* <p className={PCss.alert}>
-        <Link to="#" onClick={handleLinkClick}>
-          <div className={PCss.icon}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-alert-circle"
-          >
-         
-            <circle cx="12" cy="12" r="10" />
-            <line x1="12" x2="12" y1="8" y2="12" />
-            <line x1="12" x2="12.01" y1="16" y2="16" />
-          </svg>
-          </div>
-          Email Verification Pending !! CLICK TO VERIFY
-        </Link>
-      </p>
-      {showModal && <VerifyEmail onClose={closePopup} />} */}
-      
-
+   
       <Helmet>
         <title>HexBit.io - Profile</title>
       </Helmet>
@@ -118,7 +92,7 @@ export default function Profile() {
         </Link>
       </p>
       {showModal && <VerifyEmail onClose={closePopup} />} 
-      
+
               <Routes>
                 <Route path="/" element={<ProfileMain />} />
                 
