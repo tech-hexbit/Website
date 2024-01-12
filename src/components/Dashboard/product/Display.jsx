aeb08c2f232a4cb37fc12424afe00278a840fca5
nee1612 <neerajits4u@gmail.com>
2024-01-12 17:16:00 +0530

    rem imp

--- src/components/Dashboard/product/Display.jsx
+++ src/components/Dashboard/product/Display.jsx
@@ -17,7 +17,6 @@ import Load from "./../../../MicroInteraction/LoadBlack";
 // css
 import DCss from "./Css/display.module.css";
 import cardDisplay from "./Css/cardDisplay.module.css";
-import Filter from "./Filter";
 
 export default function Display({ filteredlist, setfilteredlist }) {
   const [max, setmax] = useState(false);
