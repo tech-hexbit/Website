82be5797183bfadc031ef636405936d855abe44a
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-16 00:12:48 +0530

    Update ListBox.jsx

--- src/components/MainAdmin/ListBox.jsx
+++ src/components/MainAdmin/ListBox.jsx
@@ -28,6 +28,87 @@ export default function ListBox({ title, label }) {
         </>
       )}
 
+      {title === "Login & security" && (
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.75"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-shield-half"
+          >
+            <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10" />
+            <path d="M12 22V2" />
+          </svg>
+        </>
+      )}
+
+      {title === "Payments & payouts" && (
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.75"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-banknote"
+          >
+            <rect width="20" height="12" x="2" y="6" rx="2" />
+            <circle cx="12" cy="12" r="2" />
+            <path d="M6 12h.01M18 12h.01" />
+          </svg>
+        </>
+      )}
+
+      {title === "Taxes" && (
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.75"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file"
+          >
+            <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
+            <polyline points="14 2 14 8 20 8" />
+          </svg>
+        </>
+      )}
+
+      {title === "Notifications" && (
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.75"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-megaphone"
+          >
+            <path d="m3 11 18-5v12L3 14v-3z" />
+            <path d="M11.6 16.8a3 3 0 1 1-5.8-1.6" />
+          </svg>
+        </>
+      )}
+
       <h3 className={Acss.a1}>{title}</h3>
       <p className={Acss.a2}>{label}</p>
     </div>
