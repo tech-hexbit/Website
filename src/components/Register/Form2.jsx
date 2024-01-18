c483ad4aba5a9af02981609daf657420868463ac
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 21:57:27 +0530

    Update Form2.jsx

--- src/components/Register/Form2.jsx
+++ src/components/Register/Form2.jsx
@@ -175,25 +175,31 @@ export default function Form2(props) {
                       pincodeVerify();
                     }}
                   >
-                    {verifyPin ? (
-                      <>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="lucide lucide-check"
-                        >
-                          <path d="M20 6 9 17l-5-5" />
-                        </svg>
-                      </>
+                    {load ? (
+                      <Load />
                     ) : (
-                      "Verify"
+                      <>
+                        {verifyPin ? (
+                          <>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="20"
+                              height="20"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="lucide lucide-check"
+                            >
+                              <path d="M20 6 9 17l-5-5" />
+                            </svg>
+                          </>
+                        ) : (
+                          "Verify"
+                        )}
+                      </>
                     )}
                   </button>
                 </div>
