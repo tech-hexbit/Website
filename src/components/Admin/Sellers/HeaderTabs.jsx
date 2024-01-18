58ebf9b026001542d13c3c278be6bbbf3e9020e6
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 10:52:30 +0530

    Update HeaderTabs.jsx

--- src/components/Admin/Sellers/HeaderTabs.jsx
+++ src/components/Admin/Sellers/HeaderTabs.jsx
@@ -8,26 +8,28 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
   return (
     <div className={SelCss.typesOfUsers}>
       {/* All Sellers */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-users-round"
-        id={SellerType === "all" ? "Selcolor" : "notSelColor"}
-        onClick={() => {
-          setSellerType("all");
-        }}
-      >
-        <path d="M18 21a8 8 0 0 0-16 0" />
-        <circle cx="10" cy="8" r="5" />
-        <path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3" />
-      </svg>
+      <div className={SelCss.AllSellersIcon}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-users-round"
+          id={SellerType === "all" ? "Selcolor" : "notSelColor"}
+          onClick={() => {
+            setSellerType("all");
+          }}
+        >
+          <path d="M18 21a8 8 0 0 0-16 0" />
+          <circle cx="10" cy="8" r="5" />
+          <path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3" />
+        </svg>
+      </div>
 
       {/* PENDING */}
       <svg
