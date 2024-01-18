2684b594c62a0a1543dd43134f927a50ffc419de
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 19:49:40 +0530

    Update AllSellers.jsx

--- src/components/Admin/Sellers/AllSellers.jsx
+++ src/components/Admin/Sellers/AllSellers.jsx
@@ -34,51 +34,49 @@ export default function AllSellers(props) {
                     >
                       <div className={SelCss.conDelT}>
                         <p>
-                          <strong>StoreID :{val.Store[0]?.StoreID} </strong>
+                          StoreID : <strong>{val.Store[0]?.StoreID} </strong>
                         </p>
                         <p>{val.BusinessName}</p>
                         <p>{val.GSTIN}</p>
                       </div>
 
                       <div className={SelCss.conDel}>
-                        <p>
+                        <>
                           {val.accountVerified ? (
-                            <div className={SelCss.statusIcons}>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="lucide lucide-check-circle"
-                              >
-                                <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-                                <path d="m9 11 3 3L22 4" />
-                              </svg>
-                            </div>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="20"
+                              height="20"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="lucide lucide-check-circle"
+                              className={SelCss.statusIcons}
+                            >
+                              <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
+                              <path d="m9 11 3 3L22 4" />
+                            </svg>
                           ) : (
-                            <div className={SelCss.statusIcons}>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 60 60"
-                                fill="none"
-                                stroke="#FF3D00"
-                                stroke-width="2.4"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="lucide lucide-badge-info"
-                              >
-                                <path d="M30 30V18.75M30 38.3386V38.4375M52.5 30C52.5 42.4264 42.4264 52.5 30 52.5C17.5736 52.5 7.5 42.4264 7.5 30C7.5 17.5736 17.5736 7.5 30 7.5C42.4264 7.5 52.5 17.5736 52.5 30Z" />
-                              </svg>
-                            </div>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="20"
+                              height="20"
+                              viewBox="0 0 60 60"
+                              fill="none"
+                              stroke="#FF3D00"
+                              stroke-width="2.4"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="lucide lucide-badge-info"
+                              className={SelCss.statusIcons}
+                            >
+                              <path d="M30 30V18.75M30 38.3386V38.4375M52.5 30C52.5 42.4264 42.4264 52.5 30 52.5C17.5736 52.5 7.5 42.4264 7.5 30C7.5 17.5736 17.5736 7.5 30 7.5C42.4264 7.5 52.5 17.5736 52.5 30Z" />
+                            </svg>
                           )}
-                        </p>
+                        </>
                         <div
                           className={SelCss.Left_data}
                           style={{ display: "grid" }}
