20384860cf9cba6619f27cffa1b1eb24c6e74255
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 11:01:31 +0530

    Update HeaderTabs.jsx

--- src/components/Admin/Sellers/HeaderTabs.jsx
+++ src/components/Admin/Sellers/HeaderTabs.jsx
@@ -60,9 +60,6 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
           stroke-linejoin="round"
           class="lucide lucide-badge-info"
           id={SellerType === "notVerified" ? "Selcolor" : "notSelColor"}
-          onClick={() => {
-            setSellerType("notVerified");
-          }}
         >
           <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
           <line x1="12" x2="12" y1="16" y2="12" />
@@ -81,33 +78,36 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
       </div>
 
       {/* APPROVED */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-badge-check"
-        id={SellerType === "Verified" ? "Selcolor" : "notSelColor"}
+      <div
+        className={SelCss.AllSellersIcon}
         onClick={() => {
           setSellerType("Verified");
         }}
       >
-        <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-        <path d="m9 12 2 2 4-4" />
-      </svg>
-
-      <p
-        className={
-          SellerType === "Verified" ? SelCss.IconTextYes : SelCss.IconTextNo
-        }
-      >
-        Approved
-      </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-badge-check"
+          id={SellerType === "Verified" ? "Selcolor" : "notSelColor"}
+        >
+          <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+          <path d="m9 12 2 2 4-4" />
+        </svg>
+        <p
+          className={
+            SellerType === "Verified" ? SelCss.IconTextYes : SelCss.IconTextNo
+          }
+        >
+          Approved
+        </p>
+      </div>
     </div>
   );
 }
