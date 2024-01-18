6618d0f375b2472074925d22eb1cd63e7c5d2a83
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 11:14:19 +0530

    Update HeaderTabs.jsx

--- src/components/Admin/Sellers/HeaderTabs.jsx
+++ src/components/Admin/Sellers/HeaderTabs.jsx
@@ -95,7 +95,9 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
           stroke-linecap="round"
           stroke-linejoin="round"
           class="lucide lucide-check-circle"
-          id={SellerType === "Verified" ? SelCss.Selcolor : SelCss.notSelColor}
+          id={
+            SellerType === "Verified" ? SelCss.Selcolor3 : SelCss.notSelColor3
+          }
         >
           <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
           <path d="m9 11 3 3L22 4" />
