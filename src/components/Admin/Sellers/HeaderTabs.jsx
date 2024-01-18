2960d024b04ef22090d0587aae309893d83922d4
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 11:09:42 +0530

    Update HeaderTabs.jsx

--- src/components/Admin/Sellers/HeaderTabs.jsx
+++ src/components/Admin/Sellers/HeaderTabs.jsx
@@ -58,12 +58,12 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="lucide lucide-badge-info"
+          class="lucide lucide-alert-circle"
           id={SellerType === "notVerified" ? "Selcolor" : "notSelColor"}
         >
-          <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-          <line x1="12" x2="12" y1="16" y2="12" />
-          <line x1="12" x2="12.01" y1="8" y2="8" />
+          <circle cx="12" cy="12" r="10" />
+          <line x1="12" x2="12" y1="8" y2="12" />
+          <line x1="12" x2="12.01" y1="16" y2="16" />
         </svg>
 
         <p
@@ -94,12 +94,13 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="lucide lucide-badge-check"
+          class="lucide lucide-check-circle"
           id={SellerType === "Verified" ? "Selcolor" : "notSelColor"}
         >
-          <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-          <path d="m9 12 2 2 4-4" />
+          <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
+          <path d="m9 11 3 3L22 4" />
         </svg>
+
         <p
           className={
             SellerType === "Verified" ? SelCss.IconTextYes : SelCss.IconTextNo
