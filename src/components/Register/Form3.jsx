4bfc4cd5580c44e6dcfd6ef92f2a80b9ef5cc940
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 21:53:21 +0530

    Update Form3.jsx

--- src/components/Register/Form3.jsx
+++ src/components/Register/Form3.jsx
@@ -76,6 +76,10 @@ export default function Form3(props) {
     loadData();
   }, []);
 
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <>
       <div className={FCss.mainDiv}>
