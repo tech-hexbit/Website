29a4f832418d5e55de48e8a045710d6c3d310ca4
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 11:00:56 +0530

    Update HeaderTabs.jsx

--- src/components/Admin/Sellers/HeaderTabs.jsx
+++ src/components/Admin/Sellers/HeaderTabs.jsx
@@ -71,7 +71,9 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
 
         <p
           className={
-            SellerType === "all" ? SelCss.IconTextYes : SelCss.IconTextNo
+            SellerType === "notVerified"
+              ? SelCss.IconTextYes
+              : SelCss.IconTextNo
           }
         >
           Pending
@@ -98,6 +100,14 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
         <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
         <path d="m9 12 2 2 4-4" />
       </svg>
+
+      <p
+        className={
+          SellerType === "Verified" ? SelCss.IconTextYes : SelCss.IconTextNo
+        }
+      >
+        Approved
+      </p>
     </div>
   );
 }
