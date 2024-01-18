dda566e72cfeb5ea6d9c6feed8a208a300a8562a
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 10:59:37 +0530

    Update HeaderTabs.jsx

--- src/components/Admin/Sellers/HeaderTabs.jsx
+++ src/components/Admin/Sellers/HeaderTabs.jsx
@@ -42,26 +42,41 @@ export default function HeaderTabs({ SellerType, setSellerType }) {
       </div>
 
       {/* PENDING */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-badge-info"
-        id={SellerType === "notVerified" ? "Selcolor" : "notSelColor"}
+      <div
+        className={SelCss.AllSellersIcon}
         onClick={() => {
           setSellerType("notVerified");
         }}
       >
-        <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-        <line x1="12" x2="12" y1="16" y2="12" />
-        <line x1="12" x2="12.01" y1="8" y2="8" />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-badge-info"
+          id={SellerType === "notVerified" ? "Selcolor" : "notSelColor"}
+          onClick={() => {
+            setSellerType("notVerified");
+          }}
+        >
+          <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+          <line x1="12" x2="12" y1="16" y2="12" />
+          <line x1="12" x2="12.01" y1="8" y2="8" />
+        </svg>
+
+        <p
+          className={
+            SellerType === "all" ? SelCss.IconTextYes : SelCss.IconTextNo
+          }
+        >
+          Pending
+        </p>
+      </div>
 
       {/* APPROVED */}
       <svg
