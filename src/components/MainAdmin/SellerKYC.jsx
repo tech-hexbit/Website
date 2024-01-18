4ad58fe9c1ba0fb2348d2febe419a7823e784d5a
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-18 10:40:01 +0530

    Update SellerKYC.jsx

--- src/components/MainAdmin/SellerKYC.jsx
+++ src/components/MainAdmin/SellerKYC.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+// state
+import AuthContext from "./.././../store/auth-context";
+
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import Load from "./../../MicroInteraction/LoadBlack";
+
+// css
+import SelCss from "./Css/Sellers.module.css";
 
 export default function SellerKYC() {
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [loadNow, setloadNow] = useState(false);
+  const [SellerType, setSellerType] = useState("all");
+
+  const authCtx = useContext(AuthContext);
+
+  const loadData = async () => {
+    setLoad(true);
+
+    try {
+      const response = await axios.get(`/api/website/admin/usersList`, {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+
+      if (response.data.success) {
+        setData(response.data.sellerList);
+
+        setLoad(false);
+      } else {
+        setLoad(false);
+
+        console.log(e);
+      }
+    } catch (e) {
+      setLoad(false);
+
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+
+    setloadNow(false);
+  }, [, loadNow]);
   return <div>SellerKYC</div>;
 }
