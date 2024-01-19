8bf6098688c855589600634c0166f918a777df79
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-19 23:13:36 +0530

    Update PayRequest.jsx

--- src/components/Dashboard/PayRequest.jsx
+++ src/components/Dashboard/PayRequest.jsx
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
+
+// state
+import AuthContext from "./../../store/auth-context";
 
 // components
 import PaymentList from "./Payment/PaymentList";
@@ -7,20 +10,59 @@ import PaymentQuote from "./Payment/PaymentQuote";
 import SelectedBlock from "./Payment/SelectedBlock";
 import PaymentRequest from "./Payment/PaymentRequest";
 
+// axios
+import axios from "axios";
+
 // css
 import pr from "./Payment/Css/PaymentRequest.module.css";
 import Payt from "../Dashboard/Payment/Css/Payment.module.css";
 
 export default function PayRequest() {
+  const [load, setLoad] = useState(false);
   const [showSel, setSel] = useState({
     total: 0,
     amount: 0,
     bank: "",
     order: [],
   });
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
+
+  const authCtx = useContext(AuthContext);
 
   const raiseReq = async () => {
+    setLoad(true);
+
     console.log(showSel);
+
+    try {
+      const response = await axios.post(
+        "/api/common/Payment/Order/Request/Up",
+        data,
+        {
+          headers: { Authorization: `${authCtx.token}` },
+        }
+      );
+
+      console.log(response.data);
+    } catch (e) {
+      setLoad(false);
+
+      setError({
+        mainColor: "#FDEDED",
+        secondaryColor: "#F16360",
+        symbol: "error",
+        title: "Error",
+        text: "Invalid Credentials",
+        val: true,
+      });
+    }
   };
 
   // scroll to top
