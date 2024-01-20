62e3e6eb59638e5a72ce77d3d8e9bcaf033c5f7c
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-20 12:59:23 +0530

    Update PayRequest.jsx

--- src/components/Dashboard/PayRequest.jsx
+++ src/components/Dashboard/PayRequest.jsx
@@ -13,12 +13,16 @@ import PaymentRequest from "./Payment/PaymentRequest";
 // axios
 import axios from "axios";
 
+// MicroInteraction
+import { Alert } from "./../../MicroInteraction/Alert";
+
 // css
 import pr from "./Payment/Css/PaymentRequest.module.css";
 import Payt from "../Dashboard/Payment/Css/Payment.module.css";
 
 export default function PayRequest() {
   const [load, setLoad] = useState(false);
+  const [loadData, setloadData] = useState(false);
   const [showSel, setSel] = useState({
     total: 0,
     amount: 0,
@@ -39,6 +43,34 @@ export default function PayRequest() {
   const raiseReq = async () => {
     setLoad(true);
 
+    if (showSel.bank === "") {
+      setError({
+        mainColor: "#E5F6FD",
+        secondaryColor: "#1AB1F5",
+        symbol: "info",
+        title: "Information",
+        text: "Select any Bank before proceeding",
+        val: true,
+      });
+      return;
+    }
+
+    if (
+      showSel.total === 0 ||
+      showSel.amount === 0 ||
+      showSel.order.length === 0
+    ) {
+      setError({
+        mainColor: "#E5F6FD",
+        secondaryColor: "#1AB1F5",
+        symbol: "info",
+        title: "Information",
+        text: "Select any Order before proceeding",
+        val: true,
+      });
+      return;
+    }
+
     try {
       let data = {
         bank: showSel.bank,
@@ -55,7 +87,13 @@ export default function PayRequest() {
         }
       );
 
-      console.log(response.data);
+      if (response.data.success) {
+        setLoad(false);
+
+        setloadData(!loadData);
+      } else {
+        setLoad(false);
+      }
     } catch (e) {
       setLoad(false);
 
@@ -76,22 +114,30 @@ export default function PayRequest() {
   }, []);
 
   return (
-    <div className={Payt.main}>
-      <PaymentList />
-      <PaymentQuote />
-      <PaymentTable setSel={setSel} showSel={showSel} />
+    <>
+      <div className={Payt.main}>
+        <PaymentList />
+        <PaymentQuote />
+        <PaymentTable
+          setSel={setSel}
+          showSel={showSel}
+          loadDataSave={loadData}
+        />
 
-      <div className={pr.main}>
-        <h3>Confirm Account Details For Payment Request </h3>
-        <div className={pr.detailWrapper}>
-          <PaymentRequest setSel={setSel} showSel={showSel} />
-          <SelectedBlock
-            total={showSel.total}
-            amount={showSel.amount}
-            raiseReq={raiseReq}
-          />
+        <div className={pr.main}>
+          <h3>Confirm Account Details For Payment Request </h3>
+          <div className={pr.detailWrapper}>
+            <PaymentRequest setSel={setSel} showSel={showSel} />
+            <SelectedBlock
+              total={showSel.total}
+              amount={showSel.amount}
+              raiseReq={raiseReq}
+            />
+          </div>
         </div>
       </div>
-    </div>
+
+      <Alert variant={variants} val={setError} />
+    </>
   );
 }
