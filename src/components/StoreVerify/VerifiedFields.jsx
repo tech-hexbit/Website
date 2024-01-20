e168011e55a72a3cfeebd881067c3afa8457f339
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-21 03:37:39 +0530

    Update VerifiedFields.jsx

--- src/components/StoreVerify/VerifiedFields.jsx
+++ src/components/StoreVerify/VerifiedFields.jsx
@@ -1,10 +1,10 @@
-// import SvCss from "../Css/StoreVerify.module.css";
 import React from "react";
-import SvCss from "../../Pages/Css/StoreVerify.module.css";
 import PropTypes from "prop-types";
-import { BadgeCheck } from "lucide-react";
 
-const VerifiedFields = (props) => {
+// css
+import SvCss from "../../Pages/Css/StoreVerify.module.css";
+
+export default function VerifiedFields(props) {
   return (
     <div className={SvCss.inpDiv}>
       <p className={SvCss.input_label}>{props.label}</p>
@@ -20,11 +20,32 @@ const VerifiedFields = (props) => {
             props.setData({ ...props.showData, [props.name]: e.target.value });
           }}
         />
-        {props.verifyPin ? <BadgeCheck className={SvCss.badge_icon} /> : ""}
+
+        {props.verifyPin ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-badge-check"
+            className={SvCss.badge_icon}
+          >
+            <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+            <path d="m9 12 2 2 4-4" />
+          </svg>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
-};
+}
+
 VerifiedFields.propTypes = {
   label: PropTypes.string,
   disable: PropTypes.bool,
@@ -34,4 +55,3 @@ VerifiedFields.propTypes = {
   placeholder: PropTypes.string,
   verifyPin: PropTypes.bool,
 };
-export default VerifiedFields;
