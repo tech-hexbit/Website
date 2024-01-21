2dd0e0cb926243bc4b949f145df297f31f66a92d
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-21 17:16:33 +0530

    Update Archive.jsx

--- src/components/Dashboard/product/Archive.jsx
+++ src/components/Dashboard/product/Archive.jsx
@@ -63,6 +63,8 @@ export default function Archive() {
     }
   };
 
+  const deleteproduct = async (_id) => {};
+
   useEffect(() => {
     loadData();
   }, []);
@@ -153,13 +155,12 @@ export default function Archive() {
                                   stroke-width="2"
                                   stroke-linecap="round"
                                   stroke-linejoin="round"
-                                  class="lucide lucide-trash-2"
+                                  class="lucide lucide-eye-off"
                                 >
-                                  <path d="M3 6h18" />
-                                  <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                                  <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                                  <line x1="10" x2="10" y1="11" y2="17" />
-                                  <line x1="14" x2="14" y1="11" y2="17" />
+                                  <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
+                                  <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
+                                  <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
+                                  <line x1="2" x2="22" y1="2" y2="22" />
                                 </svg>
                               </div>
                             </div>
