ae82d97f162e3ba033c5484f3ba2b90c3b27229a
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-21 14:27:11 +0530

    Update Products.jsx

--- src/components/Dashboard/Products.jsx
+++ src/components/Dashboard/Products.jsx
@@ -1,4 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import { Alert } from "./../../MicroInteraction/Alert";
+
+// state
+import AuthContext from "./../../store/auth-context";
 
 // components
 import Display from "./product/Display";
@@ -9,18 +18,33 @@ import pdtCSS from "./Css/products.module.css";
 
 export default function Products() {
   const [load, setLoad] = useState(false);
+  const [filterData, setfilterData] = useState({
+    category: [],
+  });
   const [filteredlist, setfilteredlist] = useState({
     productList: [],
     length: 0,
+  });
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
   });
 
   const loadData = async () => {
     setLoad(true);
 
     try {
-      const response = await axios.get("/api/common/product/all/false", {
-        headers: { Authorization: `${authCtx.token}` },
-      });
+      const response = await axios.post(
+        "/api/common/product/all/false",
+        filterData,
+        {
+          headers: { Authorization: `${authCtx.token}` },
+        }
+      );
 
       if (response.data.success) {
         setfilteredlist({
@@ -43,15 +67,27 @@ export default function Products() {
   };
 
   useEffect(() => {
-    console.log(filteredlist);
-  }, [filteredlist]);
+    loadData();
+  }, [, filteredlist]);
 
   return (
-    <div className={pdtCSS.mdiv}>
-      <div className={pdtCSS.filterVis}>
-        <Filter filteredlist={filteredlist} setfilteredlist={setfilteredlist} />
+    <>
+      <div className={pdtCSS.mdiv}>
+        <div className={pdtCSS.filterVis}>
+          <Filter
+            load={load}
+            filteredlist={filteredlist}
+            setfilteredlist={setfilteredlist}
+          />
+        </div>
+        <Display
+          load={load}
+          filteredlist={filteredlist}
+          setfilteredlist={setfilteredlist}
+        />
       </div>
-      <Display filteredlist={filteredlist} setfilteredlist={setfilteredlist} />
-    </div>
+
+      <Alert variant={variants} val={setError} />
+    </>
   );
 }
