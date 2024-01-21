b654dafaf6e342345c6f0f25c43fb77f2d21014d
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-21 16:35:58 +0530

    Update Products.jsx

--- src/components/Dashboard/Products.jsx
+++ src/components/Dashboard/Products.jsx
@@ -19,8 +19,8 @@ import pdtCSS from "./Css/products.module.css";
 
 export default function Products() {
   const [load, setLoad] = useState(false);
-  const [allcategory, setallcategory] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [allcategory, setallcategory] = useState([]);
   const [filterData, setfilterData] = useState({
     category: [],
   });
