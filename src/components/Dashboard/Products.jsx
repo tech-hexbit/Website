bc053207158bcb8e53b7a52a8593ee4ed05591ea
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-21 16:54:59 +0530

    Update Products.jsx

--- src/components/Dashboard/Products.jsx
+++ src/components/Dashboard/Products.jsx
@@ -126,6 +126,9 @@ export default function Products() {
               stroke-linecap="round"
               stroke-linejoin="round"
               class="lucide lucide-history"
+              onClick={() => {
+                setArchive(true);
+              }}
             >
               <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
               <path d="M3 3v5h5" />
