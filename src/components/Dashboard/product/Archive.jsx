5eac57ad21a658190a2de92f1d416d5c8e0a98b4
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-21 17:12:20 +0530

    Update Archive.jsx

--- src/components/Dashboard/product/Archive.jsx
+++ src/components/Dashboard/product/Archive.jsx
@@ -1,5 +1,84 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import Load from "./../../../MicroInteraction/LoadBlack";
+import { Alert } from "./../../../MicroInteraction/Alert";
+
+// state
+import AuthContext from "./../../../store/auth-context";
+
+// css
+import DCss from "./Css/display.module.css";
+import cardDisplay from "./Css/cardDisplay.module.css";
 
 export default function Archive() {
-  return <div>Archive</div>;
+  const [load, setLoad] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filteredlist, setfilteredlist] = useState({
+    productList: [],
+    prodcutsCount: 0,
+  });
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
+
+  const authCtx = useContext(AuthContext);
+
+  const loadData = async () => {
+    setLoad(true);
+
+    try {
+      const response = await axios.get(
+        `/api/common/product/archive?page=${currentPage}`,
+        {
+          headers: { Authorization: `${authCtx.token}` },
+        }
+      );
+
+      console.log(response.data);
+
+      if (response.data.success) {
+        setfilteredlist({
+          ...filteredlist,
+          productList: response.data.orderList,
+          prodcutsCount: response.data.prodcutsCount,
+        });
+
+        setLoad(false);
+      } else {
+        setLoad(false);
+      }
+    } catch (e) {
+      setLoad(false);
+
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      Archive
+      <div>
+        {load ? (
+          <div className="loadCenterDiv">
+            <Load />
+          </div>
+        ) : (
+          <div className={DCss.table}></div>
+        )}
+      </div>
+    </div>
+  );
 }
