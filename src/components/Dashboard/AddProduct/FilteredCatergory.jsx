ad592e38d04accbe61eea92101c6d44c40536c2b
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-23 02:07:08 +0530

    Update FilteredCatergory.jsx

--- src/components/Dashboard/AddProduct/FilteredCatergory.jsx
+++ src/components/Dashboard/AddProduct/FilteredCatergory.jsx
@@ -21,10 +21,7 @@ export default function FilteredCatergory({
             className={fc.gridCard}
             onClick={() => onCategoryClick(category)}
           >
-            <img
-              src="https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt={category.name}
-            />
+            <img src={category.img} alt={category.name} />
             <div className={fc.text}>{category.name}</div>
           </div>
         ))}
