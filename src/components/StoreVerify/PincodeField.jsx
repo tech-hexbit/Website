0b86c988744b3b4e2ed4f295caa317a1d9d1980a
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-24 18:54:58 +0530

    Update PincodeField.jsx

--- src/components/StoreVerify/PincodeField.jsx
+++ src/components/StoreVerify/PincodeField.jsx
@@ -85,7 +85,6 @@ export default function PincodeField({
               >
                 {verifyPin ? (
                   <svg
-                    // className={SvCss.badge_icon}
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
                     height="24"
