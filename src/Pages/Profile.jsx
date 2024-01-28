a1366d8078ba1c1f8711b492aaf65360f52c6a07
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-28 16:07:56 +0530

    Update Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -28,11 +28,10 @@ import ProductsPage from "./../components/ProductsPage/ProductsPage";
 import VerifyEmail from "../components/Dashboard/MainParts/VerifyEmail";
 import HelpDeskTable from "./../components/Dashboard/HelpDesk/HelpDeskFormTable";
 //          || Admin
+import Payment from "../components/Admin/Payment";
 import TicketAdmin from "./../components/Admin/Ticket";
 import SupportAdmin from "./../components/Admin/Support";
 import SellersAdmin from "./../components/Admin/Sellers";
-
-import PaymentTable from "../components/Admin/PayDetails/PaymentTable";
 
 //          || Super Admin
 import FrontPage from "./../components/MainAdmin/FrontPage";
@@ -168,10 +167,7 @@ export default function Profile() {
                     <Route path="/admin/tickets" element={<TicketAdmin />} />
                     <Route path="/admin/support" element={<SupportAdmin />} />
                     <Route path="/admin/sellers" element={<SellersAdmin />} />
-                    <Route
-                      path="/admin/paymentdetails"
-                      element={<PaymentTable />}
-                    />
+                    <Route path="/admin/paymentdetails" element={<Payment />} />
                   </>
                 )}
 
