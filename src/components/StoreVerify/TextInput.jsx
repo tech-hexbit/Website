fe77a87a61a45e2e280dceb59a7398bd61fc867d
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-29 21:12:44 +0530

    Update TextInput.jsx

--- src/components/StoreVerify/TextInput.jsx
+++ src/components/StoreVerify/TextInput.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 
 import AuthContext from "../../store/auth-context";
