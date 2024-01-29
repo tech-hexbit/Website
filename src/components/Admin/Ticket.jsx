11efda2f63b8f5012d4892d04ca1e74ee6415bd7
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-29 10:15:59 +0530

    Update Ticket.jsx

--- src/components/Admin/Ticket.jsx
+++ src/components/Admin/Ticket.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-
 // components
 import TicketTable from "./Ticket/TicketTable";
 import Header from "./Ticket/Header";
 
 export default function Contact() {
-
-
   return (
     <div>
       <Header title="Tickets: All" />
