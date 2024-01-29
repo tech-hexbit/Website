3d7c5bf5675a4449d5caac66b6528d9fb5589352
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-29 13:56:05 +0530

    Update StoreVerifyMain.jsx

--- src/components/StoreVerify/StoreVerifyMain.jsx
+++ src/components/StoreVerify/StoreVerifyMain.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useRef } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // css
@@ -134,7 +134,6 @@ const StoreVerifyMain = (props) => {
           : "Invalid Pan",
         val: true,
       });
-      window.scrollTo(0, 0);
     } else {
       try {
         let data = {
@@ -182,6 +181,10 @@ const StoreVerifyMain = (props) => {
       }
     }
   };
+
+  useEffect(() => {
+    console.log(showData);
+  }, [showData]);
 
   return (
     <>
