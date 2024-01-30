6c18340a96aabe40dcd16c6bfb45d531c34730fb
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-30 09:14:48 +0530

    Update UploadFiles.jsx

--- src/components/StoreVerify/UploadFiles.jsx
+++ src/components/StoreVerify/UploadFiles.jsx
@@ -13,6 +13,7 @@ export default function UploadFiles({
   label,
   placeholder,
   //   handleImage,
+  val,
   image,
   setImageUpload,
   imageUpload,
@@ -39,6 +40,14 @@ export default function UploadFiles({
     fileInp.current.click();
   };
 
+  const onSubmit = async () => {
+    setImageUpload({
+      ...imageUpload,
+      val,
+      img: imageUploadPart,
+    });
+  };
+
   return (
     <div className={FiCss.inputLdivFile}>
       <p className={FiCss.inputLabel}>{label}</p>
@@ -52,14 +61,16 @@ export default function UploadFiles({
         />
         <div className={imageUploadPart ? "showPrev" : "hidePrev"}>
           {imageUploadPart ? (
-            <div className={EtCss.prevImgDiv}>
-              <p className={EtCss.PreviewPTag}>Preview</p>
-              <img
-                src={URL.createObjectURL(imageUploadPart)}
-                alt=""
-                className={EtCss.prevImg}
-              />
-            </div>
+            <>
+              <div className={EtCss.prevImgDiv}>
+                <p className={EtCss.PreviewPTag}>Preview</p>
+                <img
+                  src={URL.createObjectURL(imageUploadPart)}
+                  alt=""
+                  className={EtCss.prevImg}
+                />
+              </div>
+            </>
           ) : (
             ""
           )}
@@ -70,6 +81,30 @@ export default function UploadFiles({
             <p>+</p>
           </div>
         </div>
+
+        {imageUploadPart ? (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-upload"
+              onClick={onSubmit}
+            >
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+              <polyline points="17 8 12 3 7 8" />
+              <line x1="12" x2="12" y1="3" y2="15" />
+            </svg>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
