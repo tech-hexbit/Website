e1844cc31c55d214c92e52046a90aaa29654ad79
Rishav Singh <rishavrajcrj@gmail.com>
2024-01-30 08:41:59 +0530

    Update UploadFiles.jsx

--- src/components/StoreVerify/UploadFiles.jsx
+++ src/components/StoreVerify/UploadFiles.jsx
@@ -1,5 +1,49 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function UploadFiles() {
-  return <div>UploadFiles</div>;
+//css
+import FiCss from "./Css/FileInput.module.css";
+
+export default function UploadFiles({
+  fileInp,
+  label,
+  placeholder,
+  handleImage,
+  image,
+  handleClicksValue,
+}) {
+  return (
+    <div className={FiCss.inputLdivFile}>
+      <p className={FiCss.inputLabel}>{label}</p>
+      <div className={FiCss.inputDivFile}>
+        <input
+          className={FiCss.inputFile}
+          type="file"
+          name="file"
+          placeholder={placeholder}
+          onChange={(e) => {
+            handleImage(e);
+          }}
+          ref={fileInp}
+        />
+        {image ? (
+          <img
+            src={URL.createObjectURL(image)}
+            alt=""
+            className={FiCss.prevImg}
+          />
+        ) : (
+          ""
+        )}
+        <div
+          className={FiCss.addImgDiv}
+          onClick={() => handleClicks(handleClicksValue)}
+        >
+          <div className={FiCss["text-center"]}>
+            <p>+</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
