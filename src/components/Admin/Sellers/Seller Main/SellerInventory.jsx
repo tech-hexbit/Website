63f03dafaa8baa1b9b3166ce59e3aade28739129
Faisal Irfan <97839373+irfanmit@users.noreply.github.com>
2024-01-31 01:52:11 +0530

    adding first commit

--- src/components/Admin/Sellers/Seller Main/SellerInventory.jsx
+++ src/components/Admin/Sellers/Seller Main/SellerInventory.jsx
@@ -29,14 +29,15 @@ export default function SellerInventory() {
 
     try {
       const response = await axios.get(
-        `/api/common/product/all/${showFilter}?page=${currentPage}`,
+        `/api/common/product/allproducts/${showFilter}?page=${currentPage}`,
         {
           headers: { Authorization: `${authCtx.token}` },
         }
       );
 
       if (response.data.success) {
-        setorderlist(response?.data?.orderList);
+        console.log(response.data);
+        setorderlist(response?.data.products)
         setProdcutsCount(response?.data?.length);
 
         setLoad(false);
@@ -74,6 +75,32 @@ export default function SellerInventory() {
 
   return (
     <div className={Ccss.mDiv}>
+    <div className={Ccss.headerFlex}>
+        <div className={Ccss.addCsv}>
+          <button onClick={() => setFilter(!showFilter)}>
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-arrow-down-up"
+              >
+                <path d="m3 16 4 4 4-4" />
+                <path d="M7 20V4" />
+                <path d="m21 8-4-4-4 4" />
+                <path d="M17 4v16" />
+              </svg>
+            </p>
+            <p className={Ccss.hideTxt}>Low Inventory</p>
+          </button>
+        </div>
+      </div>
       <div className={Ccss.middlecontent}>
         <div className={Ccss.middle}></div>
         <div id="wrap" className={Ccss.tableCat}>
