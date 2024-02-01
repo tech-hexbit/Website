879fdf1b18c2ed6cc6dc93b11be4c403b3730a7c
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-01 17:51:19 +0530

    Create BankInfo.jsx

---
+++ src/components/Dashboard/Profile/BankInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// css
+import PICss from "./Css/PersonalInfo.module.css";
+
+export default function BankInfo() {
+  return (
+    <div className={PICss.personalinfotab}>
+      <div className={PICss.heading}>Bank info</div>
+
+      <div className={PICss.box}></div>
+    </div>
+  );
+}
