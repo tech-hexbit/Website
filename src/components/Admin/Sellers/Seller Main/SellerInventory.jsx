982faffaabf367202c68bb80dfcd00f116831db2
Faisal Irfan <97839373+irfanmit@users.noreply.github.com>
2024-02-01 21:10:50 +0530

    adding some final changes

--- src/components/Admin/Sellers/Seller Main/SellerInventory.jsx
+++ src/components/Admin/Sellers/Seller Main/SellerInventory.jsx
@@ -57,6 +57,7 @@ export default function SellerInventory() {
   const maxPage = () => {
     if (prodcutsCount > 0) {
       if (currentPage >= Math.ceil(prodcutsCount / 10)) {
+        setCurrentPage(Math.ceil(prodcutsCount / 10));
         setmax(true);
       } else {
         setmax(false);
@@ -66,9 +67,11 @@ export default function SellerInventory() {
     }
   };
 
+
+
   useEffect(() => {
     loadData();
-  }, [currentPage, showFilter]);
+  }, [currentPage, showFilter])
 
   useEffect(() => {
     maxPage();
@@ -77,25 +80,24 @@ export default function SellerInventory() {
   return (
     <div className={Ccss.mDiv}>
     <div className={Ccss.headerFlex}>
-    <div  className={Ccss.search_btn}>
     {/* label>Search page</label> */}
-   
-    <input value={searchInput} placeholder="Search page" onChange={(e) => setSearchInput(e.target.value)}/>
-    <button onClick={() => (searchInput - 1) * 10 >= prodcutsCount  ? setCurrentPage(Math.ceil(prodcutsCount / 10)) : setCurrentPage(searchInput)}>
-    <svg xmlns="http://www.w3.org/2000/svg"
-    x="0px" 
-    y="0px" 
-    width="20"
-    height="20" 
-    viewBox="0 0 30 30" 
-    stroke-width="2"
-    >
-<path
- d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
-</svg>
-</button>
+    <div className={osCss.searchParent}>
 
+     <div className={Ccss.search}>
+                  <input
+                    type="text"
+                    value={searchInput}
+                    placeholder="Search page"
+                    onChange={(e) => setSearchInput(e.target.value)}
+                  />
+                  <div className={Ccss.searchBtn} onClick={() => (searchInput - 1) * 10 >= prodcutsCount  ? 
+                  setCurrentPage(Math.ceil(prodcutsCount / 10)) 
+                  : setCurrentPage(searchInput)}>
+                    Search
+                  </div>
+                </div>
     </div>
+
         <div className={Ccss.addCsv}>
           <button onClick={() => setFilter(!showFilter)}>
             <p>
