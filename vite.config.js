8ec38c5946f4493e1eeb7e5b9927797d29773297
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-02 08:28:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "es2018", // Add this line to specify the target environment
+  },
+});
