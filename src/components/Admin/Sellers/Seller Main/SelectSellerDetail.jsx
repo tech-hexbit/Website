580fe32f94556d3c2b0f27daf154a99161f440a3
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-02 09:37:45 +0530

    Update SelectSellerDetail.jsx

--- src/components/Admin/Sellers/Seller Main/SelectSellerDetail.jsx
+++ src/components/Admin/Sellers/Seller Main/SelectSellerDetail.jsx
@@ -1,9 +1,9 @@
 import { React, useState } from "react";
 
 // components
-import Sellers from "../../Sellers";
-import SellerOrder from "./SellerOrder";
 import SellerInventory from "./SellerInventory";
+import SellerOrder from "./SellerOrder";
+import Sellers from "../../Sellers";
 
 // css
 import SSDCss from "./CSS/selectSellerDetail.module.css";
@@ -14,10 +14,10 @@ function SelectSellerDetail() {
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
   };
-
   return (
     <div className={SSDCss.main}>
       <div className={SSDCss.mainSec}>
+        <p className={SSDCss.topText}>DETAILS OF STORE A</p>
         <div className={SSDCss.slide}>
           <div
             className={`${SSDCss.slideSec} ${
