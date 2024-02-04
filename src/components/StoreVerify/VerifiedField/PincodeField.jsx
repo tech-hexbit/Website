438d848ba68b0cb30a639835f2865546787f12f8
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-04 19:30:37 +0530

    Update PincodeField.jsx

--- src/components/StoreVerify/VerifiedField/PincodeField.jsx
+++ src/components/StoreVerify/VerifiedField/PincodeField.jsx
@@ -89,9 +89,9 @@ export default function PincodeField({
                 setData({ ...showData, Pincode: e.target.value });
               }}
             />
-            <div>
-              <button className={PfCss.verifyButton} onClick={pincodeVerify}>
-                {verifyPin ? (
+            {verifyPin ? (
+              <div>
+                <button className={PfCss.verifyButton} onClick={pincodeVerify}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -107,11 +107,11 @@ export default function PincodeField({
                     <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
                     <path d="m9 12 2 2 4-4" />
                   </svg>
-                ) : (
-                  <>{load ? <Load /> : "Verify"}</>
-                )}
-              </button>
-            </div>
+                </button>
+              </div>
+            ) : (
+              <>{load ? <Load /> : "Verify"}</>
+            )}
           </div>
         </div>
       ) : (
