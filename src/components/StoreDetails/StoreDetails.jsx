2058267039cea80aa7b74c0f9ecded9007f4d3ac
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-06 02:44:41 +0530

    Update StoreDetails.jsx

--- src/components/StoreDetails/StoreDetails.jsx
+++ src/components/StoreDetails/StoreDetails.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-export default function StoreDetails() {
-  return <div>StoreDetails</div>;
+// components
+import InputType1 from "./Input/InputType1";
+
+// css
+import PrCss from "./Css/Particulars.module.css";
+
+export default function StoreDetails({ showData, setData }) {
+  return (
+    <div className={PrCss.mDiv}>
+      <p className={PrCss.AboutYou}>Store Info</p>
+    </div>
+  );
 }
+
+StoreDetails.propTypes = {
+  showData: PropTypes.object.isRequired,
+  setData: PropTypes.func.isRequired,
+};
