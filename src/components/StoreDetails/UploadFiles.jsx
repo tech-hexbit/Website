94eab0a9dda8e1bc79943985da324afe424c4380
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-06 02:28:02 +0530

    Update UploadFiles.jsx

--- src/components/StoreDetails/UploadFiles.jsx
+++ src/components/StoreDetails/UploadFiles.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function UploadFiles() {
-  return <div>UploadFiles</div>;
+// css
+import PrCss from "./Css/Particulars.module.css";
+
+export default function UploadFiles({ images, setImages }) {
+  return (
+    <div className={PrCss.mDiv}>
+      <p className={PrCss.AboutYou}>Upload Files</p>
+    </div>
+  );
 }
+
+UploadFiles.propTypes = {
+  showData: PropTypes.object.isRequired,
+  setData: PropTypes.func.isRequired,
+};
