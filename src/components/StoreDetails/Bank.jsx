9b00f3ee03f16939631a7c8682bb3796260aa4f4
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-05 22:04:23 +0530

    Update Bank.jsx

--- src/components/StoreDetails/Bank.jsx
+++ src/components/StoreDetails/Bank.jsx
@@ -16,15 +16,97 @@ import AuthContext from "./../../store/auth-context";
 
 // css
 import PrCss from "./Css/Particulars.module.css";
+import ItCss from "./Input/Css/InputType1.module.css";
 
 export default function Bank({ showData, setData }) {
   const [load, setLoad] = useState(false);
   const [verifyPin, setVerifyPin] = useState(false);
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
+
+  const authCtx = useContext(AuthContext);
+
+  const bankVerify = async () => {
+    setLoad(true);
+
+    if (verifyPin) {
+      return;
+    }
+
+    try {
+      if (showData.AccountNo === 0 || showData.IfscCode === "") {
+        setError({
+          mainColor: "#FFC0CB",
+          secondaryColor: "#FF69B4",
+          symbol: "error",
+          title: "Check it out",
+          text: "Please Fill All The Details",
+          val: true,
+        });
+
+        return;
+      }
+
+      let data = {
+        phone: authCtx.user.Phone,
+        name: showData.AcHolderName,
+        bankAccount: showData.AccountNo,
+        ifsc: showData.IfscCode,
+      };
+
+      const response = await axios.post("/api/verification/bank", data);
+
+      if (response.data.success) {
+      } else {
+      }
+    } catch (error) {
+      console.log(error);
+
+      setError({
+        mainColor: "#FDEDED",
+        secondaryColor: "#F16360",
+        symbol: "error",
+        title: "Error",
+        text: "An unexpected error occurred",
+        val: true,
+      });
+
+      setLoad(false);
+    }
+  };
 
   return (
     <>
       <div className={PrCss.mDiv}>
         <p className={PrCss.AboutYou}>Bank Info</p>
+
+        {/* Account Number */}
+        <VerifiedFeilds
+          type="number"
+          Label="Account Number"
+          disabled={verifyPin}
+          showData={showData}
+          setData={setData}
+          field="AccountNo"
+          placeholder="2456XXXXXXX1416"
+        />
+
+        {/* IFSC CODE */}
+        <VerifiedFeilds
+          type="text"
+          Label="IFSC CODE"
+          disabled={verifyPin}
+          showData={showData}
+          setData={setData}
+          field="IfscCode"
+          placeholder="SCBLXXXXX18"
+        />
 
         {/* Account Holder Name */}
         <VerifiedFeilds
@@ -58,7 +140,15 @@ export default function Bank({ showData, setData }) {
           field="BranchName"
           placeholder="Mihijam Bazar"
         />
+
+        {!verifyPin && showData.AccountNo !== "" && showData.IfscCode !== "" ? (
+          <div onClick={bankVerify}>Verify</div>
+        ) : (
+          ""
+        )}
       </div>
+
+      <Alert variant={variants} val={setError} />
     </>
   );
 }
