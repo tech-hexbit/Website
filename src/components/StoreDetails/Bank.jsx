ff6d320f010a44ddf0690a3ed654500d425dfdc7
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-05 20:43:54 +0530

    Update Bank.jsx

--- src/components/StoreDetails/Bank.jsx
+++ src/components/StoreDetails/Bank.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 
 // components
 import VerifiedFeilds from "./Input/VerifiedFeilds";
 
+// MicroInteraction
+import Load from "./../../MicroInteraction/LoadBlack";
+import { Alert } from "./../../MicroInteraction/Alert";
+
+//axios
+import axios from "axios";
+
+// store
+import AuthContext from "./../../store/auth-context";
+
 // css
 import PrCss from "./Css/Particulars.module.css";
 
 export default function Bank({ showData, setData }) {
+  const [load, setLoad] = useState(false);
+  const [verifyPin, setVerifyPin] = useState(false);
+
   return (
     <div className={PrCss.mDiv}>
       <p className={PrCss.AboutYou}>Bank Info</p>
     </div>
   );
 }
+
+Bank.propTypes = {
+  showData: PropTypes.object.isRequired,
+  setData: PropTypes.func.isRequired,
+};
