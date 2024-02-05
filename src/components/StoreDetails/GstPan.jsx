5487fc7b6c039a37b7a0991ca97aae5983f2baec
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-05 23:24:23 +0530

    Update GstPan.jsx

--- src/components/StoreDetails/GstPan.jsx
+++ src/components/StoreDetails/GstPan.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+
+// components
+import VerifiedFeilds from "./Input/VerifiedFeilds";
+
+// MicroInteraction
+import Load from "./../../MicroInteraction/LoadBlack";
+import { Alert } from "./../../MicroInteraction/Alert";
+
+//axios
+import axios from "axios";
+
+// store
+import AuthContext from "./../../store/auth-context";
+
+// css
+import PrCss from "./Css/Particulars.module.css";
 
 export default function GstPan() {
-  return <div>GstPan</div>;
+  const [load, setLoad] = useState(false);
+  const [verifyPin, setVerifyPin] = useState(false);
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
+
+  const authCtx = useContext(AuthContext);
+
+  return (
+    <div className={PrCss.mDiv}>
+      <p className={PrCss.AboutYou}>Bank Info</p>
+    </div>
+  );
 }
