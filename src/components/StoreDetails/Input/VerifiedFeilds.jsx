ab90fa9563ba5323f439a707baa46f88a7e04f25
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-05 17:39:40 +0530

    Update VerifiedFeilds.jsx

--- src/components/StoreDetails/Input/VerifiedFeilds.jsx
+++ src/components/StoreDetails/Input/VerifiedFeilds.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-export default function VerifiedFeilds() {
-  return <div>VerifiedFeilds</div>;
+// css
+import PrCss from "./Css/InputType1.module.css";
+import VrCss from "./Css/VerifiedFeilds.module.css";
+
+export default function VerifiedFeilds({
+  Label,
+  type,
+  field,
+  placeholder,
+  showData,
+  setData,
+}) {
+  return (
+    <div className={PrCss.inpDiv}>
+      <p className={PrCss.inputLabel}>{Label}</p>
+      <input
+        type={type}
+        name="days"
+        value={showData[field]}
+        id=""
+        placeholder={placeholder}
+        onChange={(e) => {
+          setData({ ...showData, [field]: e.target.value });
+        }}
+      />
+    </div>
+  );
 }
+
+VerifiedFeilds.propTypes = {
+  Label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  field: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  showData: PropTypes.object.isRequired,
+};
