1bc810b857400fd10c25cac5affbf5d0c4334cad
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-05 12:48:18 +0530

    Update StoreVerify.jsx

--- src/Pages/StoreVerify.jsx
+++ src/Pages/StoreVerify.jsx
@@ -16,6 +16,24 @@ export default function StoreVerify() {
       <div className={SvCss.Ldiv}>
         <div className={SvCss.boxDiv}>
           <StoreVerifyMain />
+
+          <div onClick={scrollToTop} className={SvCss.scrollToTop}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-move-up"
+            >
+              <path d="M8 6L12 2L16 6" />
+              <path d="M12 2V22" />
+            </svg>
+          </div>
         </div>
       </div>
     </>
