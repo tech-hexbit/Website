95dede7c3ea0d62bcfe39fccb0b9e0ca7327082e
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-05 13:12:09 +0530

    Update Particulars.jsx

--- src/components/StoreDetails/Particulars.jsx
+++ src/components/StoreDetails/Particulars.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// components
+import InputType1 from "./Input/InputType1";
+
 // css
 import PrCss from "./Css/Particulars.module.css";
 
 export default function Particulars({}) {
-  return <div className={PrCss.mDiv}>Particulars</div>;
+  return (
+    <div className={PrCss.mDiv}>
+      <InputType1 />
+    </div>
+  );
 }
