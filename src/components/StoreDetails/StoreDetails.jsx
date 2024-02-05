f26266e0e3df8abacb33d86a1e7a1fe43e49fa39
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-06 02:57:28 +0530

    Update StoreDetails.jsx

--- src/components/StoreDetails/StoreDetails.jsx
+++ src/components/StoreDetails/StoreDetails.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 // components
 import InputType1 from "./Input/InputType1";
+import DaysField from "./DaysField";
 
 // css
 import PrCss from "./Css/Particulars.module.css";
@@ -11,6 +12,8 @@ export default function StoreDetails({ showData, setData }) {
   return (
     <div className={PrCss.mDiv}>
       <p className={PrCss.AboutYou}>Store Info</p>
+
+      <DaysField showData={showData} setData={setData} />
     </div>
   );
 }
