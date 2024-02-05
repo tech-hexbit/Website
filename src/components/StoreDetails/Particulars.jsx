234d59707df75bdc8e592ac4b3552aac4bf1bc8c
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-05 13:16:12 +0530

    Update Particulars.jsx

--- src/components/StoreDetails/Particulars.jsx
+++ src/components/StoreDetails/Particulars.jsx
@@ -7,10 +7,22 @@ import InputType1 from "./Input/InputType1";
 // css
 import PrCss from "./Css/Particulars.module.css";
 
-export default function Particulars({}) {
+export default function Particulars({ showData, setData }) {
   return (
     <div className={PrCss.mDiv}>
-      <InputType1 />
+      <InputType1
+        type="text"
+        Label="First Name"
+        showData={showData}
+        setData={setData}
+        field="FirstName"
+        placeholder="John"
+      />
     </div>
   );
 }
+
+Particulars.propTypes = {
+  showData: PropTypes.object.isRequired,
+  setData: PropTypes.func.isRequired,
+};
