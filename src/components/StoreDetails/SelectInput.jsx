17f2d4a6e168b2279b787ce61b75f496eed48f7f
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-06 16:39:26 +0530

    Update SelectInput.jsx

--- src/components/StoreDetails/SelectInput.jsx
+++ src/components/StoreDetails/SelectInput.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// components
+import InputType1 from "./Input/InputType1";
+
 //css
 import OfCss from "./Css/OndcField.module.css";
 
@@ -14,14 +17,29 @@ const SelectInput = ({ setData, showData }) => {
   };
 
   return (
-    <div className={OfCss.inpDiv}>
-      <div className={OfCss.inputLabel}>Location Availability Mode</div>
-      <select name="languages" id="lang" onChange={handleSelectChangeLocation}>
-        <option value="select">Select Availability</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-      </select>
-    </div>
+    <>
+      <div className={OfCss.inpDiv}>
+        <div className={OfCss.inputLabel}>Location Availability Mode</div>
+        <select
+          name="languages"
+          id="lang"
+          onChange={handleSelectChangeLocation}
+        >
+          <option value="select">Select Availability</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+      </div>
+      {/* Store Description */}
+      <InputType1
+        type="text"
+        Label="Store Description"
+        showData={showData}
+        setData={setData}
+        field="Description"
+        placeholder="about the store"
+      />{" "}
+    </>
   );
 };
 
