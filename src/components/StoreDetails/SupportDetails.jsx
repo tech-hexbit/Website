f171c6b7691aada5584a87ee14e226eabaed2d65
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-06 09:59:26 +0530

    Update SupportDetails.jsx

--- src/components/StoreDetails/SupportDetails.jsx
+++ src/components/StoreDetails/SupportDetails.jsx
@@ -8,7 +8,31 @@ import InputType1 from "./Input/InputType1";
 import PrCss from "./Css/Particulars.module.css";
 
 export default function SupportDetails({ showData, setData }) {
-  return <div>SupportDetails</div>;
+  return (
+    <div className={PrCss.mDiv}>
+      <p className={PrCss.AboutYou}>Support Details</p>
+
+      {/* Contact Number */}
+      <InputType1
+        type="text"
+        Label="Support Number"
+        showData={showData}
+        setData={setData}
+        field="ContactDetails"
+        placeholder="99XXXXXX12"
+      />
+
+      {/* Contact Email */}
+      <InputType1
+        type="email"
+        Label="Support Email"
+        showData={showData}
+        setData={setData}
+        field="SupportEmail"
+        placeholder="support@company.com"
+      />
+    </div>
+  );
 }
 
 SupportDetails.propTypes = {
