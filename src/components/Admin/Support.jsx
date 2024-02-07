0d223fce8d139618bc1b3d27833d3e6cf2b41d0f
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-07 21:19:04 +0530

    Update Support.jsx

--- src/components/Admin/Support.jsx
+++ src/components/Admin/Support.jsx
@@ -17,17 +17,15 @@ import Load from "./../../MicroInteraction/LoadBlack";
 
 // css
 import SupCss from "./Css/Support.module.css";
-import { Link } from "react-router-dom";
 
 export default function Support() {
   const [data, setData] = useState([]);
   const [load, setLoad] = useState(false);
   const [showAdd, setAdd] = useState(false);
   const [showRef, setRef] = useState(false);
-  const [showCurr, setCurr] = useState("Support");
-
-  const [showEdit, setShowEdit] = useState(false);
   const [editdata, setEditdata] = useState(null);
+  const [showCurr, setCurr] = useState("Support");
+  const [showEdit, setShowEdit] = useState(false);
 
   const authCtx = useContext(AuthContext);
 
@@ -45,7 +43,6 @@ export default function Support() {
       });
 
       if (response.data.success) {
-        console.log(response.data.qnaEntries);
         setData(response.data.qnaEntries);
 
         setLoad(false);
