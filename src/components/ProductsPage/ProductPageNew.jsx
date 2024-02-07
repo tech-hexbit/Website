566e234e2795e92057d70aeb53fae3c14dff1556
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-07 19:45:41 +0530

    Update ProductPageNew.jsx

--- src/components/ProductsPage/ProductPageNew.jsx
+++ src/components/ProductsPage/ProductPageNew.jsx
@@ -27,8 +27,17 @@ function ProductPageNew(props) {
   const [res, setres] = useState();
   const [load, setLoad] = useState(false);
   const [change, setChange] = useState(false);
+  const [loadDel, setLoadDel] = useState(false);
   const [sliderData, setSliderData] = useState([]);
   const [sliderImages, setSliderImages] = useState([]);
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
 
   const { id } = useParams();
 
@@ -102,177 +111,203 @@ function ProductPageNew(props) {
   }, [, change]);
 
   const deleteproduct = async (_id) => {
+    setLoadDel(true);
+
     try {
       const response = await axios.delete(`/api/common/product/delete/${_id}`, {
         headers: { Authorization: `${authCtx.token}` },
       });
 
       if (response.status === 200) {
-        loadData();
+        setLoadDel(false);
       } else {
         console.log("error");
+
+        setLoadDel(false);
+
+        setError({
+          mainColor: "#FDEDED",
+          secondaryColor: "#F16360",
+          symbol: "error",
+          title: "Error",
+          text: "Unable to Delete",
+          val: true,
+        });
       }
     } catch (error) {
       console.log(error);
+
+      setLoadDel(false);
+
+      setError({
+        mainColor: "#FDEDED",
+        secondaryColor: "#F16360",
+        symbol: "error",
+        title: "Error",
+        text: "An unexpected error occurred",
+        val: true,
+      });
     }
-    navigate("/me/products");
   };
 
   return (
-    <div>
-      <div className={PPN.prodDetailMain}>
-        <div className={PPN.arcHeaderDiv}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="60"
-            height="60"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-chevron-left"
-            className={PPN.arcHeadBackIcon}
-            onClick={goBack}
-          >
-            <path d="m15 18-6-6 6-6" />
-          </svg>
+    <>
+      <div>
+        <div className={PPN.prodDetailMain}>
+          <div className={PPN.arcHeaderDiv}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="60"
+              height="60"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-chevron-left"
+              className={PPN.arcHeadBackIcon}
+              onClick={goBack}
+            >
+              <path d="m15 18-6-6 6-6" />
+            </svg>
 
-          <Header name="Product Details" />
-        </div>
-      </div>
-      <hr className={PPN.below} />
-
-      <div className={PPN.main}>
-        {load ? (
-          <div className="loadCenterDiv">
-            <Load />
+            <Header name="Product Details" />
           </div>
-        ) : (
-          <>
-            {res ? (
-              <div className={PPN.main}>
-                <div className={PPN.left}>
-                  <div className={PPN.pics}>
-                    <div className={PPN.productImgMain}>
-                      <img src={sliderData} className={PPCss.productImg} />
-                    </div>
+        </div>
+        <hr className={PPN.below} />
 
-                    {sliderImages.length > 1 && (
-                      <div className={PPN.rightLeft}>
-                        <div className={PPN.sliderSvgLeft}>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="22"
-                            height="22"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="black"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="lucide lucide-chevron-left"
-                            onClick={handlePrevious}
-                          >
-                            <path d="m15 18-6-6 6-6" />
-                          </svg>
-                        </div>
-                        <div className={PPN.sliderSvgRight}>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="22"
-                            height="22"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="lucide lucide-chevron-right"
-                            onClick={handleNext}
-                          >
-                            <path d="m9 18 6-6-6-6" />
-                          </svg>
-                        </div>
+        <div className={PPN.main}>
+          {load ? (
+            <div className="loadCenterDiv">
+              <Load />
+            </div>
+          ) : (
+            <>
+              {res ? (
+                <div className={PPN.main}>
+                  <div className={PPN.left}>
+                    <div className={PPN.pics}>
+                      <div className={PPN.productImgMain}>
+                        <img src={sliderData} className={PPCss.productImg} />
                       </div>
-                    )}
-                    <div className={PPN.secPic}>
-                      {sliderImages.map((data, key) => (
-                        <img
-                          src={data}
-                          key={key}
-                          onClick={() => handleClick(key)}
-                          className={PPN.productImg}
+
+                      {sliderImages.length > 1 && (
+                        <div className={PPN.rightLeft}>
+                          <div className={PPN.sliderSvgLeft}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="22"
+                              height="22"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="black"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="lucide lucide-chevron-left"
+                              onClick={handlePrevious}
+                            >
+                              <path d="m15 18-6-6 6-6" />
+                            </svg>
+                          </div>
+                          <div className={PPN.sliderSvgRight}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="22"
+                              height="22"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="lucide lucide-chevron-right"
+                              onClick={handleNext}
+                            >
+                              <path d="m9 18 6-6-6-6" />
+                            </svg>
+                          </div>
+                        </div>
+                      )}
+                      <div className={PPN.secPic}>
+                        {sliderImages.map((data, key) => (
+                          <img
+                            src={data}
+                            key={key}
+                            onClick={() => handleClick(key)}
+                            className={PPN.productImg}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                    <div className={PPN.btns}>
+                      <div className={PPN.goToInv}>
+                        <Link to="/me/inventory" className={PPN.goToInvText}>
+                          Go to Inventory
+                        </Link>
+                      </div>
+                      <div
+                        className={PPN.del}
+                        onClick={() => deleteproduct(res._id)}
+                      >
+                        {loadDel ? <Load /> : <>{"Delete"}</>}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className={PPN.right}>
+                    <div className={PPN.prodDet}>
+                      <p className={PPN.name}>{res.descriptor.name}</p>
+                      <p className={PPN.prodId}>Product Id: {res._id} </p>
+                      <p className={PPN.prodId}>
+                        Published On: {res.when.date}{" "}
+                      </p>
+                      <div className={PPN.prodBox}>
+                        <Box
+                          title="Selling Price"
+                          value={`₹ ${res.price.maximum_value}`}
+                          up="price.maximum_value"
+                          id={res._id}
+                          setChange={setChange}
+                          placeholderLabel="Updated Price"
                         />
-                      ))}
-                    </div>
-                  </div>
-                  <div className={PPN.btns}>
-                    <div className={PPN.goToInv}>
-                      <Link to="/me/inventory" className={PPN.goToInvText}>
-                        Go to Inventory
-                      </Link>
-                    </div>
-                    <div
-                      className={PPN.del}
-                      onClick={() => deleteproduct(res._id)}
-                    >
-                      Delete
-                    </div>
-                  </div>
-                </div>
+                        <Box
+                          title="Orders"
+                          value={res.quantity.maximum.count}
+                          up="quantity.maximum.count"
+                          id={res._id}
+                          setChange={setChange}
+                          placeholderLabel="Updated Stock"
+                        />
+                        <Box
+                          title="Stock"
+                          value={res.quantity.maximum.count}
+                          up="quantity.maximum.count"
+                          id={res._id}
+                          setChange={setChange}
+                          placeholderLabel="Updated Stock"
+                        />
+                        <Box
+                          title="Tax %"
+                          value={res.quantity.maximum.count}
+                          up="quantity.maximum.count"
+                          id={res._id}
+                          setChange={setChange}
+                          placeholderLabel="Updated Stock"
+                        />
+                        <Box
+                          title="Discount %"
+                          value={res.quantity.maximum.count}
+                          up="quantity.maximum.count"
+                          id={res._id}
+                          setChange={setChange}
+                          placeholderLabel="Updated Stock"
+                        />
+                      </div>
 
-                <div className={PPN.right}>
-                  <div className={PPN.prodDet}>
-                    <p className={PPN.name}>{res.descriptor.name}</p>
-                    <p className={PPN.prodId}>Product Id: {res._id} </p>
-                    <p className={PPN.prodId}>Published On: {res.when.date} </p>
-                    <div className={PPN.prodBox}>
-                      <Box
-                        title="Selling Price"
-                        value={`₹ ${res.price.maximum_value}`}
-                        up="price.maximum_value"
-                        id={res._id}
-                        setChange={setChange}
-                        placeholderLabel="Updated Price"
-                      />
-                      <Box
-                        title="Orders"
-                        value={res.quantity.maximum.count}
-                        up="quantity.maximum.count"
-                        id={res._id}
-                        setChange={setChange}
-                        placeholderLabel="Updated Stock"
-                      />
-                      <Box
-                        title="Stock"
-                        value={res.quantity.maximum.count}
-                        up="quantity.maximum.count"
-                        id={res._id}
-                        setChange={setChange}
-                        placeholderLabel="Updated Stock"
-                      />
-                      <Box
-                        title="Tax %"
-                        value={res.quantity.maximum.count}
-                        up="quantity.maximum.count"
-                        id={res._id}
-                        setChange={setChange}
-                        placeholderLabel="Updated Stock"
-                      />
-                      <Box
-                        title="Discount %"
-                        value={res.quantity.maximum.count}
-                        up="quantity.maximum.count"
-                        id={res._id}
-                        setChange={setChange}
-                        placeholderLabel="Updated Stock"
-                      />
-                    </div>
-
-                    {/* Color Box */}
-                    {/* <div className={PPN.prodColor}>
+                      {/* Color Box */}
+                      {/* <div className={PPN.prodColor}>
                       <ColorBox
                         imgSrc={
                           response.data.ProductDetail.descriptor.images[0]
@@ -282,32 +317,35 @@ function ProductPageNew(props) {
                       />
                     </div> */}
 
-                    {/* Size */}
-                    {/* <div className={PPN.prodSize}>
+                      {/* Size */}
+                      {/* <div className={PPN.prodSize}>
                       <SizeBox label="S" />
                     </div> */}
-                    <div className={PPN.Desc}>
-                      <Des res={res} id={res._id} setChange={setChange} />
-                    </div>
+                      <div className={PPN.Desc}>
+                        <Des res={res} id={res._id} setChange={setChange} />
+                      </div>
 
-                    {/* Review */}
-                    {/* <div className={PPN.prodCom}>
+                      {/* Review */}
+                      {/* <div className={PPN.prodCom}>
                       <p className={PPN.prodComText}>Ratings & Reviews:</p>
                       <Rating />
                       <Rating />
                       <Rating />
                       <Rating />
                     </div> */}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ) : (
-              <p>No Orders</p>
-            )}
-          </>
-        )}
+              ) : (
+                <p>No Orders</p>
+              )}
+            </>
+          )}
+        </div>
       </div>
-    </div>
+
+      <Alert variant={variants} val={setError} />
+    </>
   );
 }
 
