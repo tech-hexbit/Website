9fe69ccdc638317de59f626d31c2465c2698e009
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-07 19:14:01 +0530

    Update Display.jsx

--- src/components/Dashboard/product/Display.jsx
+++ src/components/Dashboard/product/Display.jsx
@@ -29,20 +29,27 @@ export default function Display({
   setfilteredlist,
 }) {
   const [max, setmax] = useState(false);
+  const [loadDel, setLoadDel] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [showProductDel, setProductDel] = useState({ state: false, id: "" });
 
   const authCtx = useContext(AuthContext);
 
   const deleteproduct = async (_id) => {
+    setLoadDel(true);
+
     try {
       const response = await axios.delete(`/api/common/product/delete/${_id}`, {
         headers: { Authorization: `${authCtx.token}` },
       });
 
       if (response.status === 200) {
+        setLoadDel(false);
+
         loadData();
       } else {
+        setLoadDel(false);
+
         console.log("error");
 
         setError({
@@ -56,6 +63,8 @@ export default function Display({
       }
     } catch (error) {
       console.log(error);
+
+      setLoadDel(false);
 
       setError({
         mainColor: "#FDEDED",
@@ -195,12 +204,7 @@ export default function Display({
                                 to={`/me/products/${val._id}`}
                                 className="LinkStyle"
                               >
-                                <div
-                                  className={DCss.col1}
-                                  // onClick={() => {
-                                  //   setProductDel({ state: true, id: val._id });
-                                  // }}
-                                >
+                                <div className={DCss.col1}>
                                   <div className={DCss.image}>
                                     <img
                                       src={val.descriptor.images[0]}
@@ -243,24 +247,28 @@ export default function Display({
                                   className={DCss.deleteDiv}
                                   onClick={() => deleteproduct(val._id)}
                                 >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    class="lucide lucide-trash-2"
-                                  >
-                                    <path d="M3 6h18" />
-                                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                                    <line x1="10" x2="10" y1="11" y2="17" />
-                                    <line x1="14" x2="14" y1="11" y2="17" />
-                                  </svg>
+                                  {loadDel ? (
+                                    <Load />
+                                  ) : (
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="24"
+                                      height="24"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      stroke-width="2"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      class="lucide lucide-trash-2"
+                                    >
+                                      <path d="M3 6h18" />
+                                      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                      <line x1="10" x2="10" y1="11" y2="17" />
+                                      <line x1="14" x2="14" y1="11" y2="17" />
+                                    </svg>
+                                  )}
                                 </div>
                               </div>
                             </td>
@@ -340,24 +348,28 @@ export default function Display({
                                   className={cardDisplay.deleteBtn}
                                   onClick={() => deleteproduct(val._id)}
                                 >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    class="lucide lucide-trash-2"
-                                  >
-                                    <path d="M3 6h18" />
-                                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                                    <line x1="10" x2="10" y1="11" y2="17" />
-                                    <line x1="14" x2="14" y1="11" y2="17" />
-                                  </svg>
+                                  {loadDel ? (
+                                    <Load />
+                                  ) : (
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="24"
+                                      height="24"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      stroke-width="2"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      class="lucide lucide-trash-2"
+                                    >
+                                      <path d="M3 6h18" />
+                                      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                      <line x1="10" x2="10" y1="11" y2="17" />
+                                      <line x1="14" x2="14" y1="11" y2="17" />
+                                    </svg>
+                                  )}
                                 </div>
                               </div>
                             );
