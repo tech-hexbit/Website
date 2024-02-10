477ba6e93acf5c5b1f341235453368f9b6d2544b
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-10 19:36:38 +0530

    Update BankData.jsx

--- src/components/Dashboard/Profile/BankData.jsx
+++ src/components/Dashboard/Profile/BankData.jsx
@@ -165,98 +165,96 @@ export default function BankData({
 
   return (
     <>
-      <div className={PICss.nestedFieldLargeDiv}>
-        <div className={PICss.nestedFieldSmallDiv}>
-          <div>Bank Details</div>
-          <div className={PICss.inpDiv}>
-            <div className={PICss.inputLabel}>A/c Holder Name.</div>
-            <div className={PICss.inputDivVerified}>
-              <input
-                placeholder="Account Holder Name"
-                type="text"
-                name="AccountHolderName"
-                value={formData.AccountHolderName}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className={PICss.inpDiv}>
-            <div className={PICss.inputLabel}>IFSC CODE</div>
-            <div className={PICss.inputDivVerified}>
-              <input
-                placeholder="11-character IFSC Code"
-                type="text"
-                name="IfscCode"
-                value={formData.IfscCode}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className={PICss.inpDiv}>
-            <div className={PICss.inputLabel}>Account No.</div>
-            <div className={PICss.inputDivVerified}>
-              <input
-                placeholder="Enter Account Number"
-                type="number"
-                name="AccountNumber"
-                value={formData.AccountNumber}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
+      {/* <div className={PICss.nestedFieldLargeDiv}> */}
+      {/* <div className={PICss.nestedFieldSmallDiv}> */}
+      <div>Bank Details</div>
+      <div className={PICss.inpDiv}>
+        <div className={PICss.inputLabel}>A/c Holder Name.</div>
+        <div className={PICss.inputDivVerified}>
+          <input
+            placeholder="Account Holder Name"
+            type="text"
+            name="AccountHolderName"
+            value={formData.AccountHolderName}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className={PICss.inpDiv}>
+        <div className={PICss.inputLabel}>IFSC CODE</div>
+        <div className={PICss.inputDivVerified}>
+          <input
+            placeholder="11-character IFSC Code"
+            type="text"
+            name="IfscCode"
+            value={formData.IfscCode}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className={PICss.inpDiv}>
+        <div className={PICss.inputLabel}>Account No.</div>
+        <div className={PICss.inputDivVerified}>
+          <input
+            placeholder="Enter Account Number"
+            type="number"
+            name="AccountNumber"
+            value={formData.AccountNumber}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
 
-          <div className={PICss.inpDiv}>
-            <div className={PICss.inputLabel}>Bank Name</div>
-            <div className={PICss.inputDivVerified}>
-              <input
-                placeholder="Name of Bank"
-                type="text"
-                name="BankName"
-                value={formData.BankName}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className={PICss.inpDiv}>
-            <div className={PICss.inputLabel}>Branch Name</div>
-            <div className={PICss.inputDivVerified}>
-              <input
-                placeholder="Account Branch Name"
-                type="text"
-                name="Branch"
-                value={formData.Branch}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className={PICss.inpDiv}>
-            <div className={PICss.inputLabel}>City</div>
-            <div className={PICss.inputDivVerified}>
-              <input
-                placeholder="City"
-                type="text"
-                name="City"
-                value={formData.City}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className={PICss.inpDiv}>
-            <p className={PICss.inputLabel}></p>
-            <div className={PICss.inputDivFile}>
-              {ver ? (
-                <button className={PICss.verifyButton} onClick={saveData}>
-                  {load ? <Load /> : "Save Info"}
-                </button>
-              ) : (
-                <>
-                  <button className={PICss.verifyButton} onClick={handleVerify}>
-                    {load ? <Load /> : "Verify"}
-                  </button>
-                </>
-              )}
-            </div>
-          </div>
+      <div className={PICss.inpDiv}>
+        <div className={PICss.inputLabel}>Bank Name</div>
+        <div className={PICss.inputDivVerified}>
+          <input
+            placeholder="Name of Bank"
+            type="text"
+            name="BankName"
+            value={formData.BankName}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className={PICss.inpDiv}>
+        <div className={PICss.inputLabel}>Branch Name</div>
+        <div className={PICss.inputDivVerified}>
+          <input
+            placeholder="Account Branch Name"
+            type="text"
+            name="Branch"
+            value={formData.Branch}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className={PICss.inpDiv}>
+        <div className={PICss.inputLabel}>City</div>
+        <div className={PICss.inputDivVerified}>
+          <input
+            placeholder="City"
+            type="text"
+            name="City"
+            value={formData.City}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className={PICss.inpDiv}>
+        <p className={PICss.inputLabel}></p>
+        <div className={PICss.inputDivFile}>
+          {ver ? (
+            <button className={PICss.verifyButton} onClick={saveData}>
+              {load ? <Load /> : "Save Info"}
+            </button>
+          ) : (
+            <>
+              <button className={PICss.verifyButton} onClick={handleVerify}>
+                {load ? <Load /> : "Verify"}
+              </button>
+            </>
+          )}
         </div>
       </div>
 
