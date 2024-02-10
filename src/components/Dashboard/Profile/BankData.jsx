6096e3baa5892b6d730933dbde6bf6e715fb92bd
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-10 18:52:42 +0530

    Create BankData.jsx

---
+++ src/components/Dashboard/Profile/BankData.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, useContext } from "react";
+
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import Load from "./../../../MicroInteraction/LoadBlack";
+
+// state
+import AuthContext from "../../../store/auth-context";
+
+// css
+import PICss from "./Css/PersonalInfo.module.css";
+
+export default function BankData() {
+  return <div>BankData</div>;
+}
