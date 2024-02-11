51a5c8baf377f9b2c92b82789c3dbb50d71471d0
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-11 21:03:44 +0530

    Update ProductCategory.jsx

--- src/components/Dashboard/AddProduct/ProductCategory.jsx
+++ src/components/Dashboard/AddProduct/ProductCategory.jsx
@@ -33,8 +33,60 @@ export default function ProductCategory({ setData, showData }) {
           <option value="Selected" selected disabled hidden>
             Select
           </option>
-          <option value="true">True</option>
-          <option value="false">False</option>
+          <option value="Fruits and Vegetables">Fruits and Vegetables</option>
+          <option value="Masala & Seasoning">Masala & Seasoning</option>
+          <option value="Oil & Ghee">Oil & Ghee</option>
+          <option value="Eggs, Meat & Fish">Eggs, Meat & Fish</option>
+          <option value="Cleaning & Household">Cleaning & Household</option>
+          <option value="Bakery, Cakes & Dairy">Bakery, Cakes & Dairy</option>
+          <option value="Pet Care">Pet Care</option>
+          <option value="Stationery">Stationery</option>
+          <option value="Dairy and Cheese">Dairy and Cheese</option>
+          <option value="Snacks, Dry Fruits, Nuts">
+            Snacks, Dry Fruits, Nuts
+          </option>
+          <option value="Pasta, Soup and Noodles">
+            Pasta, Soup and Noodles
+          </option>
+          <option value="Cereals and Breakfast">Cereals and Breakfast</option>
+          <option value="Sauces, Spreads and Dips">
+            Sauces, Spreads and Dips
+          </option>
+          <option value="Chocolates and Biscuits">
+            Chocolates and Biscuits
+          </option>
+          <option value="Cooking and Baking Needs">
+            Cooking and Baking Needs
+          </option>
+          <option value="Tinned and Processed Food">
+            Tinned and Processed Food
+          </option>
+          <option value="Atta, Flours and Sooji">Atta, Flours and Sooji</option>
+          <option value="Rice and Rice Products">Rice and Rice Products</option>
+          <option value="Dals and Pulses">Dals and Pulses</option>
+          <option value="Salt, Sugar and Jaggery">
+            Salt, Sugar and Jaggery
+          </option>
+          <option value="Energy and Soft Drinks">Energy and Soft Drinks</option>
+          <option value="Water">Water</option>
+          <option value="Tea and Coffee">Tea and Coffee</option>
+          <option value="Fruit Juices and Fruit Drinks">
+            Fruit Juices and Fruit Drinks
+          </option>
+          <option value="Snacks and Namkeen">Snacks and Namkeen</option>
+          <option value="Ready to Cook and Eat">Ready to Cook and Eat</option>
+          <option value="Pickles and Chutney">Pickles and Chutney</option>
+          <option value="Indian Sweets">Indian Sweets</option>
+          <option value="Frozen Vegetables">Frozen Vegetables</option>
+          <option value="Frozen Snacks">Frozen Snacks</option>
+          <option value="Gift Voucher">Gift Voucher</option>
+          <option value="Gourmet & World Foods">Gourmet & World Foods</option>
+          <option value="Foodgrains">Foodgrains</option>
+          <option value="Beverages">Beverages</option>
+          <option value="Beauty & Hygiene">Beauty & Hygiene</option>
+          <option value="Kitchen Accessories">Kitchen Accessories</option>
+          <option value="Baby Care">Baby Care</option>
+          <option value="Snacks & Branded Foods">Snacks & Branded Foods</option>
         </select>
       </div>
     </>
