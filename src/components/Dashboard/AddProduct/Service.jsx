2f6d288a88d6cff612a3515c69b155b55564e5d0
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-11 21:21:30 +0530

    Update Service.jsx

--- src/components/Dashboard/AddProduct/Service.jsx
+++ src/components/Dashboard/AddProduct/Service.jsx
@@ -11,15 +11,23 @@ export default function Service({ setData, showData }) {
   return (
     <>
       <p className={PrCss.AboutYou}>Product Services</p>
-
       {/* Net Quantity/Measure Of Commodity */}
       <InpTp1
         type="text"
         Label="Net Quantity/Measure Of Commodity (in Kg)"
         showData={showData}
         setData={setData}
-        field="netQuantity"
+        field="net_quantity_or_measure_of_commodity_in_pkg"
         placeholder="121 Kg"
+      />
+      {/* Month/Year of Manufacture Packing Import */}
+      <InpTp1
+        type="text"
+        Label="Month/Year of Manufacture Packing Import"
+        showData={showData}
+        setData={setData}
+        field="month_year_of_manufacture_packing_import"
+        placeholder="08/2022"
       />
     </>
   );
