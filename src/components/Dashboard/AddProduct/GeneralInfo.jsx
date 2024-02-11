19d2199126f1b66e30ba44badcc3f40634d74281
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-11 20:26:48 +0530

    Update GeneralInfo.jsx

--- src/components/Dashboard/AddProduct/GeneralInfo.jsx
+++ src/components/Dashboard/AddProduct/GeneralInfo.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+//component
+import InpTp1 from "./Input/InpTp1";
 
 export default function GeneralInfo() {
   return <div>GeneralInfo</div>;
