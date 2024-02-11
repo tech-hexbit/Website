8648bdea4875ad9fafc4425be9dc692aff77f350
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-11 20:20:28 +0530

    Update ProdParticulars.jsx

--- src/components/Dashboard/AddProduct/ProdParticulars.jsx
+++ src/components/Dashboard/AddProduct/ProdParticulars.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function ProdParticulars() {
-  return <div>ProdParticulars</div>;
+//component
+import InpTp1 from "./Input/InpTp1";
+import TxtArea from "./Input/TxtArea";
+
+export default function ProdParticulars({ setData, showData }) {
+  return (
+    <>
+      {" "}
+      {/* Protuct Title */}
+      <InpTp1
+        type="text"
+        Label="Title"
+        showData={showData}
+        setData={setData}
+        field="name"
+        placeholder="Title - XX"
+      />
+      {/* Short Description */}
+      <InpTp1
+        type="text"
+        Label="Short Description"
+        showData={showData}
+        setData={setData}
+        field="short_desc"
+        placeholder="Write product short description here..."
+      />
+      {/* Long Description */}
+      <TxtArea
+        Label="Long Description"
+        showData={showData}
+        setData={setData}
+        field="long_desc"
+        placeholder="Write long product description here..."
+      />
+    </>
+  );
 }
+
+ProdParticulars.propTypes = {
+  showData: PropTypes.object.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
