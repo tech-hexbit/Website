91afa41e92860bca37dbf1cc9d8d50ea093a32b8
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-11 20:28:37 +0530

    Update GeneralInfo.jsx

--- src/components/Dashboard/AddProduct/GeneralInfo.jsx
+++ src/components/Dashboard/AddProduct/GeneralInfo.jsx
@@ -4,6 +4,13 @@ import PropTypes from "prop-types";
 //component
 import InpTp1 from "./Input/InpTp1";
 
+// css
+import PrCss from "./Css/Lable.module.css";
+
 export default function GeneralInfo() {
-  return <div>GeneralInfo</div>;
+  return (
+    <>
+      <p className={PrCss.AboutYou}>General Info</p>
+    </>
+  );
 }
