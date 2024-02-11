ee5862a303f98aa6a0b6fedfaf9ae82d272fae6e
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-11 20:51:56 +0530

    Create ProductCategory.jsx

---
+++ src/components/Dashboard/AddProduct/ProductCategory.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//component
+import InpTp1 from "./Input/InpTp1";
+
+// css
+import PrCss from "./Css/Lable.module.css";
+import ItCss from "./Input/Css/InputType1.module.css";
+
+export default function ProductCategory({ setData, showData }) {
+  return (
+    <>
+      <p className={PrCss.AboutYou}>Publish Info</p>
+    </>
+  );
+}
+
+ProductCategory.propTypes = {
+  showData: PropTypes.object.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
