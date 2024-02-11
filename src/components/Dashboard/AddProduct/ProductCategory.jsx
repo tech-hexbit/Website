178eed3156d331cdd006f09970ce2e89ad375007
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-11 20:54:12 +0530

    Update ProductCategory.jsx

--- src/components/Dashboard/AddProduct/ProductCategory.jsx
+++ src/components/Dashboard/AddProduct/ProductCategory.jsx
@@ -10,9 +10,35 @@ import PrCss from "./Css/Lable.module.css";
 import ItCss from "./Input/Css/InputType1.module.css";
 
 export default function ProductCategory({ setData, showData }) {
+  const handleSelectChange = (e) => {
+    const value = e.target.value;
+
+    value
+      ? setData({ ...showData, veg: true, non_veg: false })
+      : setData({ ...showData, veg: false, non_veg: true });
+  };
+
   return (
     <>
       <p className={PrCss.AboutYou}>Publish Info</p>
+
+      {/* Category */}
+      <div className={ItCss.inpDiv}>
+        <p className={ItCss.inputLabel}>Category</p>
+
+        <select
+          name="Category"
+          id=""
+          className={ItCss.inp}
+          onChange={handleSelectChange}
+        >
+          <option value="Selected" selected disabled hidden>
+            Select
+          </option>
+          <option value="true">True</option>
+          <option value="false">False</option>
+        </select>
+      </div>
     </>
   );
 }
