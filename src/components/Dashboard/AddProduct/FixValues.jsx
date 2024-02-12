0970747d5432b8a500e037bcf815b9b163b71b87
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-12 09:04:00 +0530

    Update FixValues.jsx

--- src/components/Dashboard/AddProduct/FixValues.jsx
+++ src/components/Dashboard/AddProduct/FixValues.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 // axios
@@ -12,7 +12,60 @@ import { Alert } from "./../../../MicroInteraction/Alert";
 import AuthContext from "../../../store/auth-context";
 
 export default function FixValues({ setData, showData }) {
+  const [store, setStore] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
+
   const authCtx = useContext(AuthContext);
+
+  const loadInfo = async () => {
+    setLoad(true);
+
+    try {
+      const response = await axios.get(`/api/common/Store/GetStoreInfo`, {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+
+      if (response.data.success) {
+        setLoad(false);
+
+        setStore(response.data.StoreID);
+      } else {
+        setLoad(false);
+
+        setError({
+          mainColor: "#FDEDED",
+          secondaryColor: "#F16360",
+          symbol: "error",
+          title: "Error",
+          text: "Unable to fetch Info",
+          val: true,
+        });
+      }
+    } catch (e) {
+      setLoad(false);
+
+      setError({
+        mainColor: "#FDEDED",
+        secondaryColor: "#F16360",
+        symbol: "error",
+        title: "Error",
+        text: "An unexpected error occurred",
+        val: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    loadInfo();
+  }, []);
 
   return <div>FixValues</div>;
 }
