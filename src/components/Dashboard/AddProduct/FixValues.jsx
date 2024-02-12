d670b8efeb091272a6570b18f6f6d2ce270a8103
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-12 08:59:24 +0530

    Update FixValues.jsx

--- src/components/Dashboard/AddProduct/FixValues.jsx
+++ src/components/Dashboard/AddProduct/FixValues.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// axios
+import axios from "axios";
+
+// MicroInteraction
+import Load from "./../../../MicroInteraction/Load";
+import { Alert } from "./../../../MicroInteraction/Alert";
+
+// state
+import AuthContext from "../../../store/auth-context";
+
 export default function FixValues({ setData, showData }) {
+  const authCtx = useContext(AuthContext);
+
   return <div>FixValues</div>;
 }
 
