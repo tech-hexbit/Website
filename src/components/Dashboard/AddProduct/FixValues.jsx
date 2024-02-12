30ec917d244467ba808bd12c76759f1ea9136cca
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-12 08:54:17 +0530

    Update FixValues.jsx

--- src/components/Dashboard/AddProduct/FixValues.jsx
+++ src/components/Dashboard/AddProduct/FixValues.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function FixValues() {
+export default function FixValues({ setData, showData }) {
   return <div>FixValues</div>;
 }
+
+FixValues.propTypes = {
+  showData: PropTypes.object.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
