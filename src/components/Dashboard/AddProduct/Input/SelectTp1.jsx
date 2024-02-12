c0ae0888591edeb4a23afa6f4107621cdd0016c5
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-12 09:14:46 +0530

    Create SelectTp1.jsx

---
+++ src/components/Dashboard/AddProduct/Input/SelectTp1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// css
+import ItCss from "./Css/InputType1.module.css";
+
+export default function Select({ name }) {
+  return <div>Select</div>;
+}
