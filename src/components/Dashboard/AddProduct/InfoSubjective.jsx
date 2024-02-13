ea7ca66366d0c5fcdb158b1434932c74b1cd95ab
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-13 23:22:20 +0530

    Update InfoSubjective.jsx

--- src/components/Dashboard/AddProduct/InfoSubjective.jsx
+++ src/components/Dashboard/AddProduct/InfoSubjective.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+//component
+import InpTp1 from "./Input/InpTp1";
+import TxtArea from "./Input/TxtArea";
+
+// css
+import PrCss from "./Css/Lable.module.css";
 
 export default function InfoSubjective() {
-  return <div>InfoSubjective</div>;
+  return (
+    <>
+      <p className={PrCss.AboutYou}>Subjective Info</p>
+    </>
+  );
 }
