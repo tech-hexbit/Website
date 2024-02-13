10e83ba57acf1c6088fc98bd430049464dc42592
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-13 23:23:42 +0530

    Update InfoSubjective.jsx

--- src/components/Dashboard/AddProduct/InfoSubjective.jsx
+++ src/components/Dashboard/AddProduct/InfoSubjective.jsx
@@ -8,10 +8,25 @@ import TxtArea from "./Input/TxtArea";
 // css
 import PrCss from "./Css/Lable.module.css";
 
-export default function InfoSubjective() {
+export default function InfoSubjective({ setData, showData }) {
   return (
     <>
       <p className={PrCss.AboutYou}>Subjective Info</p>
+
+      {/* Net Quantity */}
+      <InpTp1
+        type="text"
+        Label="Title"
+        showData={showData}
+        setData={setData}
+        field="net_quantity"
+        placeholder="Title - XX"
+      />
     </>
   );
 }
+
+InfoSubjective.propTypes = {
+  showData: PropTypes.object.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
