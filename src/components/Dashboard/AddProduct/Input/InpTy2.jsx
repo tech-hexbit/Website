c8fe9c05b6050a9d2ac6661a4fa22b1fc269fc48
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-14 09:15:00 +0530

    Create InpTy2.jsx

---
+++ src/components/Dashboard/AddProduct/Input/InpTy2.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+
+// css
+import ItCss from "./Css/InputType1.module.css";
+
+export default function InpTy2({
+  Label,
+  type,
+  value,
+  field,
+  setData,
+  showData,
+  placeholder,
+}) {
+  useEffect(() => {
+    if (value !== "") {
+      setData({
+        ...showData,
+        [field]: value,
+      });
+    }
+  }, [value]);
+  return (
+    <div className={ItCss.inpDiv}>
+      <p className={ItCss.inputLabel}>{Label}</p>
+
+      <input
+        type={type}
+        name="days"
+        value={showData[field]}
+        id=""
+        placeholder={placeholder}
+        onChange={(e) => {
+          setData({ ...showData, [field]: e.target.value });
+        }}
+      />
+    </div>
+  );
+}
+
+InpTy2.propTypes = {
+  type: PropTypes.string.isRequired,
+  Label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  field: PropTypes.string.isRequired,
+  showData: PropTypes.object.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
