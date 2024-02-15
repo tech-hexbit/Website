021ca12ae248ae3ff857b22a55f99499c6649d33
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-15 17:53:13 +0530

    Update Form.jsx

--- src/components/Dashboard/AddProduct/Form.jsx
+++ src/components/Dashboard/AddProduct/Form.jsx
@@ -43,7 +43,6 @@ export default function Form({ domain }) {
 
   const [data, setData] = useState({
     name: "",
-    // symbol: "",
     images: [],
     long_desc: "",
     short_desc: "",
@@ -101,7 +100,6 @@ export default function Form({ domain }) {
 
     const {
       name,
-      // symbol,
       images,
       long_desc,
       short_desc,
@@ -125,7 +123,6 @@ export default function Form({ domain }) {
       ondcOrgreturnable,
       domain,
       manufacturer_or_packer_address,
-      // net_quantity,
       fulfillment_id,
       nutritional_info,
       other_FSSAI_license_no,
@@ -138,7 +135,6 @@ export default function Form({ domain }) {
 
     if (
       name !== "" &&
-      // symbol !== "" &&
       images !== "" &&
       long_desc !== "" &&
       short_desc !== "" &&
@@ -162,7 +158,6 @@ export default function Form({ domain }) {
       ondcOrgreturnable !== "" &&
       domain !== "" &&
       manufacturer_or_packer_address !== "" &&
-      // net_quantity !== "" &&
       fulfillment_id !== "" &&
       nutritional_info !== "" &&
       other_FSSAI_license_no !== "" &&
@@ -207,42 +202,41 @@ export default function Form({ domain }) {
             val: true,
           });
 
-          // setData({
-          //   name: "",
-          //   // symbol: "",
-          //   images: [],
-          //   long_desc: "",
-          //   short_desc: "",
-          //   veg: "",
-          //   non_veg: "",
-          //   Discounts: "",
-          //   brand_name: "",
-          //   maximumCount: 0,
-          //   maximum_value: 0,
-          //   manufacturer_or_packer_name: "",
-          //   tags: [],
-          //   category_id: "",
-          //   additives_info: "",
-          //   month_year_of_manufacture_packing_import: "",
-          //   net_quantity_or_measure_of_commodity_in_pkg: "",
-          //   ondcOrgavailable_on_cod: "",
-          //   ondcOrgreturn_window: "",
-          //   ondcOrgseller_pickup_return: "",
-          //   ondcOrgtime_to_ship: "",
-          //   ondcOrgcancellable: "",
-          //   ondcOrgreturnable: "",
-          //   domain: "",
-          //   manufacturer_or_packer_address: "",
-          //   //net_quantity: "",
-          //   fulfillment_id: 1,
-          //   nutritional_info: "",
-          //   other_FSSAI_license_no: "",
-          //   importer_FSSAI_license_no: "",
-          //   brand_owner_FSSAI_license_no: "",
-          //   ondcOrgcontact_details_consumer_care: "",
-          //   common_or_generic_name_of_commodity: "",
-          //   StoreID: authCtx.user.Store[0].StoreID._id,
-          // });
+          setData({
+            name: "",
+            images: [],
+            long_desc: "",
+            short_desc: "",
+            veg: "",
+            non_veg: "",
+            Discounts: "",
+            brand_name: "",
+            maximumCount: 0,
+            maximum_value: 0,
+            manufacturer_or_packer_name: "",
+            tags: [],
+            category_id: "",
+            additives_info: "",
+            month_year_of_manufacture_packing_import: "",
+            net_quantity_or_measure_of_commodity_in_pkg: "",
+            ondcOrgavailable_on_cod: "",
+            ondcOrgreturn_window: "",
+            ondcOrgseller_pickup_return: "",
+            ondcOrgtime_to_ship: "",
+            ondcOrgcancellable: "",
+            ondcOrgreturnable: "",
+            domain: "",
+            manufacturer_or_packer_address: "",
+            //net_quantity: "",
+            fulfillment_id: 1,
+            nutritional_info: "",
+            other_FSSAI_license_no: "",
+            importer_FSSAI_license_no: "",
+            brand_owner_FSSAI_license_no: "",
+            ondcOrgcontact_details_consumer_care: "",
+            common_or_generic_name_of_commodity: "",
+            StoreID: authCtx.user.Store[0].StoreID._id,
+          });
         } else {
           setLoad(false);
 
