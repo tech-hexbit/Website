ccab74823171c46d3417dc9b0f17726dbdec3348
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-15 18:43:53 +0530

    Update AddProdRET10.jsx

--- src/components/Dashboard/AddProduct/Domin/AddProdRET10.jsx
+++ src/components/Dashboard/AddProduct/Domin/AddProdRET10.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //component
 import Form from "./Form";
 
-export default function AddProdRET10() {
+export default function AddProdRET10({ domain }) {
   return (
     <>
       <Form domain={domain} />
