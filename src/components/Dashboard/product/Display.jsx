3267ce8308767f70d2c4ff83e40fe764a4693b42
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-15 20:42:42 +0530

    Update Display.jsx

--- src/components/Dashboard/product/Display.jsx
+++ src/components/Dashboard/product/Display.jsx
@@ -9,6 +9,7 @@ import axios from "axios";
 
 // components
 import Filter from "./Filter";
+import DeleteFun from "./DeleteFun";
 
 // MicroInteraction
 import Load from "./../../../MicroInteraction/LoadBlack";
@@ -34,48 +35,6 @@ export default function Display({
   const [showProductDel, setProductDel] = useState({ state: false, id: "" });
 
   const authCtx = useContext(AuthContext);
-
-  const deleteproduct = async (_id) => {
-    setLoadDel(true);
-
-    try {
-      const response = await axios.delete(`/api/common/product/delete/${_id}`, {
-        headers: { Authorization: `${authCtx.token}` },
-      });
-
-      if (response.status === 200) {
-        setLoadDel(false);
-
-        loadData();
-      } else {
-        setLoadDel(false);
-
-        console.log("error");
-
-        setError({
-          mainColor: "#FDEDED",
-          secondaryColor: "#F16360",
-          symbol: "error",
-          title: "Error",
-          text: "Unable to Delete",
-          val: true,
-        });
-      }
-    } catch (error) {
-      console.log(error);
-
-      setLoadDel(false);
-
-      setError({
-        mainColor: "#FDEDED",
-        secondaryColor: "#F16360",
-        symbol: "error",
-        title: "Error",
-        text: "An unexpected error occurred",
-        val: true,
-      });
-    }
-  };
 
   const filter = (event) => {
     setSearchTerm(event.target.value.toLowerCase());
@@ -242,35 +201,7 @@ export default function Display({
                               </div>
                             </td>
                             <td className={DCss.row} id={DCss.col6}>
-                              <div className={DCss.dots}>
-                                <div
-                                  className={DCss.deleteDiv}
-                                  onClick={() => deleteproduct(val._id)}
-                                >
-                                  {loadDel ? (
-                                    <Load />
-                                  ) : (
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      width="24"
-                                      height="24"
-                                      viewBox="0 0 24 24"
-                                      fill="none"
-                                      stroke="currentColor"
-                                      stroke-width="2"
-                                      stroke-linecap="round"
-                                      stroke-linejoin="round"
-                                      class="lucide lucide-trash-2"
-                                    >
-                                      <path d="M3 6h18" />
-                                      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                                      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                                      <line x1="10" x2="10" y1="11" y2="17" />
-                                      <line x1="14" x2="14" y1="11" y2="17" />
-                                    </svg>
-                                  )}
-                                </div>
-                              </div>
+                              <DeleteFun id={val._id} />
                             </td>
                           </tr>
                         </>
