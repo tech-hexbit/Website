40b2f588d7b28ed69b1b24b2346de802347c1cdf
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-15 18:44:15 +0530

    Delete SelectTp1.jsx

--- src/components/Dashboard/AddProduct/Input/SelectTp1.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-// css
-import ItCss from "./Css/InputType1.module.css";
-
-export default function Select({ name, optionArr }) {
-  return <div>Select</div>;
-}
