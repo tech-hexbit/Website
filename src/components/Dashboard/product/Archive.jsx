4e6f0c76a78b48210a4b69d41d82a2e1be3b19d8
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-15 20:57:58 +0530

    Update Archive.jsx

--- src/components/Dashboard/product/Archive.jsx
+++ src/components/Dashboard/product/Archive.jsx
@@ -253,30 +253,12 @@ export default function Archive({ setArchive }) {
                                     </p>
                                   </div>
                                 </div>
-                                <div
-                                  className={cardDisplay.deleteBtn}
-                                  onClick={() => deleteproduct(val._id)}
-                                >
-                                  {loadDel ? (
-                                    <Load />
-                                  ) : (
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      width="24"
-                                      height="24"
-                                      viewBox="0 0 24 24"
-                                      fill="none"
-                                      stroke="currentColor"
-                                      stroke-width="2"
-                                      stroke-linecap="round"
-                                      stroke-linejoin="round"
-                                      class="lucide lucide-rotate-ccw"
-                                    >
-                                      <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
-                                      <path d="M3 3v5h5" />
-                                    </svg>
-                                  )}
-                                </div>
+
+                                <DeleteFun
+                                  id={val._id}
+                                  loadData={loadData}
+                                  code={1}
+                                />
                               </div>
                             );
                           })}
