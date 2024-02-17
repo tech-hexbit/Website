44dcc152bbbfce145770ac231c7220cf56e432be
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-17 14:18:49 +0530

    Update StoreInfo.jsx

--- src/components/Dashboard/Profile/StoreInfo.jsx
+++ src/components/Dashboard/Profile/StoreInfo.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+// css
+import PICss from "./Css/PersonalInfo.module.css";
 
 export default function StoreInfo() {
   return <div>StoreInfo</div>;
