db7da0241d10ee4e8fc218aed0528a9a666046b1
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-24 08:38:09 +0530

    Update Cashfree.jsx

--- src/cashfree/Cashfree.jsx
+++ src/cashfree/Cashfree.jsx
@@ -18,13 +18,19 @@ export default function Cashfree() {
 
   const getSessionId = async (e) => {
     try {
-      const response = await axios.post(
-        "/api/common/payment/cashfree/payment",
-        {
-          headers: { Authorization: `${authCtx.token}` },
-          version,
-        }
-      );
+      let data = {
+        customer_id: "CID89898",
+        customer_email: "waleedsdev@gmail.com",
+        customer_phone: "7498608775",
+        customer_name: "Waleed Shaikh",
+        order_amount: 123,
+        order_id: "2024-02-22-965076",
+      };
+
+      const response = await axios.post("/api/payment/cashfree/payment", data, {
+        headers: { Authorization: `${authCtx.token}` },
+        version,
+      });
       console.log(response);
 
       setSessionId(response.data);
@@ -34,24 +40,26 @@ export default function Cashfree() {
   };
 
   const handlePayment = () => {
-    let checkoutOptions = {
-      paymentSessionId: sessionId,
-      returnUrl: "http://localhost:8000/api/status/{order_id} ",
-    };
-    cashfree.checkout(checkoutOptions).then(function (result) {
-      if (result.error) {
-        alert(result.error.message);
-      }
-      if (result.redirect) {
-        console.log("Redirection");
-        console.log(result);
-      }
-    });
+    getSessionId();
+
+    // let checkoutOptions = {
+    //   paymentSessionId: sessionId,
+    //   returnUrl: "http://localhost:8000/api/status/{order_id} ",
+    // };
+    // cashfree.checkout(checkoutOptions).then(function (result) {
+    //   if (result.error) {
+    //     alert(result.error.message);
+    //   }
+    //   if (result.redirect) {
+    //     console.log("Redirection");
+    //     console.log(result);
+    //   }
+    // });
   };
 
   return (
     <>
-      <button>Cashfree</button>
+      <button onClick={handlePayment}>Cashfree</button>
       {/* <button onClick={getSessionId}>Cashfree</button> */}
     </>
   );
