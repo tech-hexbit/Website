c052eed18e46170a3def7b3d8434851275c03a23
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-24 18:22:40 +0530

    Update util.js

--- src/cashfree/util.js
+++ src/cashfree/util.js
@@ -1,5 +1,18 @@
 import { load } from "@cashfreepayments/cashfree-js";
 
-export const cashfree = await load({
-  mode: "sandbox", //or production
-});
+async function initializeCashfree() {
+  const cashfree = await load({
+    mode: "sandbox", //or production
+  });
+
+  // You can use the 'cashfree' object here
+  return cashfree;
+}
+
+initializeCashfree()
+  .then((cashfree) => {
+    // Use 'cashfree' object here if needed
+  })
+  .catch((error) => {
+    console.error("Error initializing Cashfree:", error);
+  });
