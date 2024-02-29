91af3d9ca424ce8ec87f27be91ae6377e44ff185
Rishav Singh <rishavrajcrj@gmail.com>
2024-02-29 15:57:10 +0530

    Update Logistics.jsx

--- src/components/Dashboard/Sales/Details/Logistics.jsx
+++ src/components/Dashboard/Sales/Details/Logistics.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+// css
+import odcss from "./Css/Orderdetails.module.css";
+
+// img
+import LogisticsGif from "./../../assets/Logistic/Logistics.gif";
+
 export default function Logistics() {
-  return <div>Logistics</div>;
+  return (
+    <>
+      <div className={odcss["overlap-group"]}>
+        <div className={odcss["text-wrapper"]}>Logistics details</div>
+        <div className={odcss["logistic-img"]}>
+          <img src={LogisticsGif} alt="" className={odcss.LogisticsGif} />
+        </div>
+      </div>
+
+      <div className={odcss["text-l"]}>
+        <div className={odcss.name}>
+          <div className={odcss.div}>Soon To be Alloted</div>
+        </div>
+      </div>
+    </>
+  );
 }
