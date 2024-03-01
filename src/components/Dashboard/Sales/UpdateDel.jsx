6d378304ecddc2238974dc7acd53b94fd9ed8980
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-01 10:21:20 +0530

    Update UpdateDel.jsx

--- src/components/Dashboard/Sales/UpdateDel.jsx
+++ src/components/Dashboard/Sales/UpdateDel.jsx
@@ -105,27 +105,6 @@ export default function OrderLayUpdate(props) {
       <div className={OLCss.contendDivDel}>
         <p className={OLCss.UpdateOrderPTag}>UPDATE ORDER STATUS</p>
 
-        {/* Close Btn
-          // <svg
-          //   xmlns="http://www.w3.org/2000/svg"
-          //   width="24"
-          //   height="24"
-          //   viewBox="0 0 24 24"
-          //   fill="none"
-          //   stroke="currentColor"
-          //   stroke-width="2"
-          //   stroke-linecap="round"
-          //   stroke-linejoin="round"
-          //   class="lucide lucide-x"
-          //   className={OLCss.closeBtn}
-          //   onClick={() => {
-          //     props.setEdit(false);
-          //   }}
-          // >
-          //   <path d="M18 6 6 18" />
-          //   <path d="m6 6 12 12" />
-          // </svg> */}
-
         <div className={OLCss.BtnDivMain}>
           <div
             className={OLCss.BtnDiv}
