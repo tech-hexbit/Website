19d989f368fbc2d385501920087379bca4dc5c6e
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-03 16:48:33 +0530

    Update Logistics.jsx

--- src/components/Dashboard/Sales/Details/Logistics.jsx
+++ src/components/Dashboard/Sales/Details/Logistics.jsx
@@ -11,7 +11,26 @@ export default function Logistics({ res }) {
   return (
     <>
       <div className={odcss["overlap-group"]}>
-        <div className={odcss["text-wrapper"]}>Logistics details</div>
+        <div className={odcss["text-wrapper"]}>
+          Logistics details
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-pencil"
+            >
+              <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+              <path d="m15 5 4 4" />
+            </svg>
+          </span>
+        </div>
         <div className={odcss["logistic-img"]}>
           <img src={LogisticsGif} alt="" className={odcss.LogisticsGif} />
         </div>
