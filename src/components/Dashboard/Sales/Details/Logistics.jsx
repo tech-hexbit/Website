1e12bc9931bef9a2041ef0a25604f5bc88381e07
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-03 16:53:35 +0530

    Update Logistics.jsx

--- src/components/Dashboard/Sales/Details/Logistics.jsx
+++ src/components/Dashboard/Sales/Details/Logistics.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // css
 import odcss from "./../../Css/Orderdetails.module.css";
@@ -7,13 +7,15 @@ import odcss from "./../../Css/Orderdetails.module.css";
 import LogisticsGif from "./../../../../assets/Logistic/Logistics.gif";
 
 export default function Logistics({ res }) {
+  const [showEdit, setEdit] = useState(false);
+
   // console.log(res.state);
   return (
     <>
       <div className={odcss["overlap-group"]}>
         <div className={odcss["text-wrapper"]}>
           Logistics details
-          <span>
+          <span className={odcss.editIconSpan}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="20"
@@ -25,27 +27,38 @@ export default function Logistics({ res }) {
               stroke-linecap="round"
               stroke-linejoin="round"
               class="lucide lucide-pencil"
+              className={odcss.editIcon}
+              onClick={() => {
+                setEdit(!showEdit);
+              }}
             >
               <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
               <path d="m15 5 4 4" />
             </svg>
           </span>
         </div>
-        <div className={odcss["logistic-img"]}>
-          <img src={LogisticsGif} alt="" className={odcss.LogisticsGif} />
-        </div>
 
-        <div className={odcss["text-l"]}>
-          <div className={odcss.name}>
-            <div className={odcss.div}>
-              {res.state === "Created" || res.state === "In-progress" ? (
-                <>Soon To be Alloted</>
-              ) : (
-                <>Details of logistics</>
-              )}
+        {showEdit ? (
+          ""
+        ) : (
+          <>
+            <div className={odcss["logistic-img"]}>
+              <img src={LogisticsGif} alt="" className={odcss.LogisticsGif} />
             </div>
-          </div>
-        </div>
+
+            <div className={odcss["text-l"]}>
+              <div className={odcss.name}>
+                <div className={odcss.div}>
+                  {res.state === "Created" || res.state === "In-progress" ? (
+                    <>Soon To be Alloted</>
+                  ) : (
+                    <>Details of logistics</>
+                  )}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
