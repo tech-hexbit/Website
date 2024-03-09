fe1b4d49bf8d42e47a33e1238b7ef845b803b386
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-09 21:38:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// cookie
+import CookieConsent from "react-cookie-consent";
+
 // css
 import "./index.css";
 
@@ -10,6 +13,18 @@ import { AuthContextProvider } from "./store/auth-context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
+    <CookieConsent
+      location="bottom"
+      buttonText="Sure man!!"
+      cookieName="myAwesomeCookieName2"
+      style={{ background: "#2B373B" }}
+      buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+      expires={150}
+    >
+      This website uses cookies to enhance the user experience.{" "}
+      <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+    </CookieConsent>
+
     <App />
   </AuthContextProvider>
 );
