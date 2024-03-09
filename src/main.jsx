222e007399b1a8ce8148dd6fbe5fdaf15ee7ca46
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-09 22:04:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import "./index.css";
 // store
 import { AuthContextProvider } from "./store/auth-context";
 
-console.log(getCookieConsentValue("myAwesomeCookieName2"));
+// console.log(getCookieConsentValue("myAwesomeCookieName2"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
