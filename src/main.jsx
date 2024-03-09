eca1d0b61d29cb89620c14780f12380f2870ddc9
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-09 21:48:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,14 +15,22 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
     <CookieConsent
       location="bottom"
-      buttonText="Sure man!!"
+      buttonText="Accept"
       cookieName="myAwesomeCookieName2"
       style={{ background: "#2B373B" }}
-      buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+      buttonStyle={{ color: "#4e503b", fontSize: "11px" }}
       expires={150}
     >
-      This website uses cookies to enhance the user experience.{" "}
-      <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+      <span style={{ fontSize: "11px" }}>
+        <b>Use of Cookies</b>
+      </span>
+      <br />
+      <span style={{ fontSize: "11px" }}>
+        We and our third party partners use cookie technology to make your
+        shopping experience faster, safer and more relevant, and to deliver
+        personalized advertising on and off our website. This website uses
+        cookies to enhance the user experience.
+      </span>
     </CookieConsent>
 
     <App />
