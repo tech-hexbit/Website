011098aa3a529ee956afba18078c9e83a023723b
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-10 19:23:14 +0530

    Update TrackingHeader.jsx

--- src/components/Tracking/TrackingHeader.jsx
+++ src/components/Tracking/TrackingHeader.jsx
@@ -119,7 +119,7 @@ export default function TrackingHeader({ data }) {
                 </a>
               </p>
               <p className={THCss.infoPTag}>
-                Tracking ID: <span>{data.logistics.id}</span>
+                ID: <span>{data.logistics.id}</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -136,6 +136,11 @@ export default function TrackingHeader({ data }) {
                   <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
                   <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
                 </svg>
+              </p>
+
+              <p>
+                Logistics Patner:{" "}
+                <span>{data.logistics.logisticsPatnerName}</span>
               </p>
             </div>
           </div>
