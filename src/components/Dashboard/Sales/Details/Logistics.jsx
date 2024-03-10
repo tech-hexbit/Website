525f70e33674722a4765cbbbc03c15467adc8924
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-10 09:23:28 +0530

    Update Logistics.jsx

--- src/components/Dashboard/Sales/Details/Logistics.jsx
+++ src/components/Dashboard/Sales/Details/Logistics.jsx
@@ -15,7 +15,7 @@ import odcss from "./../../Css/Orderdetails.module.css";
 
 export default function Logistics({ res, _id }) {
   const [load, setLoad] = useState(false);
-  const [showEdit, setEdit] = useState(false);
+  const [showEdit, setEdit] = useState(true);
   const [variants, setError] = useState({
     mainColor: "",
     secondaryColor: "",
@@ -114,109 +114,160 @@ export default function Logistics({ res, _id }) {
         </div>
 
         {showEdit ? (
-          <>
-            <label for="TrakingID">Traking ID</label>
-            <input
-              type="text"
-              name="id"
-              id="TrakingID"
-              placeholder="58405917356"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="TrakingURL">Traking URL</label>
-            <input
-              type="text"
-              name="url"
-              id="TrakingURL"
-              placeholder="https://abcLogistic.com"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="LogisticsPatner">Logistics Patner Name</label>
-            <input
-              type="text"
-              name="logisticsPatnerName"
-              id="LogisticsPatner"
-              placeholder="Blue Dart"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="Location">Current Location</label>
-            <input
-              type="text"
-              name="currentLocation"
-              id="Location"
-              placeholder="Abc, City"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="EstimatedTime">Estimated Time to Delivery</label>
-            <input
-              type="text"
-              name="estimatedTATHR"
-              id="EstimatedTime"
-              placeholder="15 Hr"
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="estimatedTATMINS"
-              id=""
-              placeholder="25 Mins"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="AgentName">Agent Name</label>
-            <input
-              type="text"
-              name="agentName"
-              id="AgentName"
-              placeholder="Ramu"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="AgentNumber">Agent Number</label>
-            <input
-              type="number"
-              name="agentNumber"
-              id="AgentNumber"
-              placeholder="98XXXXXXXX60"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="VehicleCategory">Vehicle Category</label>
-            <input
-              type="text"
-              name="vehicleCategory"
-              id="VehicleCategory"
-              placeholder="mini-truck"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="VehicleSize">Vehicle Size</label>
-            <input
-              type="text"
-              name="vehicleSize"
-              id="VehicleSize"
-              placeholder="small"
-              onChange={handleChange}
-            />
-            <br />
-            <label for="VehicleRegistration">Vehicle Registration</label>
-            <input
-              type="text"
-              name="vehicleRegistration"
-              id="VehicleRegistration"
-              placeholder="2020"
-              onChange={handleChange}
-            />
+          <div className={odcss.formDiv}>
+            {/* Traking ID */}
+            <div>
+              <label for="TrakingID" className={odcss.labelCl}>
+                Traking ID
+              </label>
+              <input
+                type="text"
+                name="id"
+                id="TrakingID"
+                placeholder="58405917356"
+                onChange={handleChange}
+              />
+            </div>
 
-            <br />
-            <br />
+            {/* Traking URL */}
+            <div>
+              <label for="TrakingURL" className={odcss.labelCl}>
+                Traking URL
+              </label>
+              <input
+                type="text"
+                name="url"
+                id="TrakingURL"
+                placeholder="https://abcLogistic.com"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Logistics Patner Name */}
+            <div>
+              <label for="LogisticsPatner" className={odcss.labelCl}>
+                Logistics Patner Name
+              </label>
+              <input
+                type="text"
+                name="logisticsPatnerName"
+                id="LogisticsPatner"
+                placeholder="Blue Dart"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Current Location */}
+            <div>
+              <label for="Location" className={odcss.labelCl}>
+                Current Location
+              </label>
+              <input
+                type="text"
+                name="currentLocation"
+                id="Location"
+                placeholder="Abc, City"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/*  Estimated Time to Delivery */}
+            <div>
+              <label for="EstimatedTime" className={odcss.labelCl}>
+                Estimated Time to Delivery
+              </label>
+
+              {/*Hr */}
+              <input
+                type="text"
+                name="estimatedTATHR"
+                id="EstimatedTime"
+                placeholder="15 Hr"
+                onChange={handleChange}
+              />
+
+              {/* Min */}
+              <input
+                type="text"
+                name="estimatedTATMINS"
+                id=""
+                placeholder="25 Mins"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/*  Agent Name */}
+            <div>
+              <label for="AgentName" className={odcss.labelCl}>
+                Agent Name
+              </label>
+              <input
+                type="text"
+                name="agentName"
+                id="AgentName"
+                placeholder="Ramu"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Agent Number */}
+            <div>
+              <label for="AgentNumber" className={odcss.labelCl}>
+                Agent Number
+              </label>
+              <input
+                type="number"
+                name="agentNumber"
+                id="AgentNumber"
+                placeholder="98XXXXXXXX60"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Vehicle Category */}
+            <div>
+              <label for="VehicleCategory" className={odcss.labelCl}>
+                Vehicle Category
+              </label>
+              <input
+                type="text"
+                name="vehicleCategory"
+                id="VehicleCategory"
+                placeholder="mini-truck"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Vehicle Size */}
+            <div>
+              <label for="VehicleSize" className={odcss.labelCl}>
+                Vehicle Size
+              </label>
+              <input
+                type="text"
+                name="vehicleSize"
+                id="VehicleSize"
+                placeholder="small"
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Vehicle Registration */}
+            <div>
+              <label for="VehicleRegistration" className={odcss.labelCl}>
+                Vehicle Registration
+              </label>
+              <input
+                type="text"
+                name="vehicleRegistration"
+                id="VehicleRegistration"
+                placeholder="2020"
+                onChange={handleChange}
+              />
+            </div>
 
             <button onClick={saveData}>Save</button>
-          </>
+          </div>
         ) : (
           <>
             {res.logistics.id === "" ? (
