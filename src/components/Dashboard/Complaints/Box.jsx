feaf179d54581a1eb8908750d7c3a6a86127a7a7
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-16 22:33:54 +0530

    Update Box.jsx

--- src/components/Dashboard/Complaints/Box.jsx
+++ src/components/Dashboard/Complaints/Box.jsx
@@ -47,6 +47,23 @@ export default function Box({ key, val }) {
         </td>
         <td data-cell="Status">
           {val.respondent_actions[0].respondent_action}
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-pencil"
+            className={BoxCss.editBtn}
+          >
+            <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+            <path d="m15 5 4 4" />
+          </svg>
         </td>
         <td data-cell="Action">
           View Details
