b7e57f8acff8f86ecd6cb9b25ab5b41d071ce7bb
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-16 22:07:16 +0530

    Update Box.jsx

--- src/components/Dashboard/Complaints/Box.jsx
+++ src/components/Dashboard/Complaints/Box.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // css
 import BoxCss from "./Css/Box.module.css";
 import pt from "./../Payment/Css/PaymentTable.module.css";
 
 export default function Box({ key, val }) {
-  const currentTime = new Date();
-  const expectedTime = new Date(val.timeStramp);
-  expectedTime.setHours(
-    expectedTime.getHours() +
-      parseInt(val.expected_response_time.duration.slice(2, -1))
-  );
+  const [isOverOneDay, setIsOverOneDay] = useState(false);
 
-  const isOverOneDay = currentTime > expectedTime;
+  useEffect(() => {
+    const currentTime = new Date();
+
+    const expectedTime = new Date(val.timeStramp);
+
+    expectedTime.setHours(
+      expectedTime.getHours() +
+        parseInt(val.expected_response_time.duration.slice(2, -1))
+    );
+
+    setIsOverOneDay(currentTime > expectedTime);
+  }, []);
 
   return (
     <>
@@ -20,10 +26,10 @@ export default function Box({ key, val }) {
         <td data-cell="ID">{val._id.slice(-4)}</td>
         <td data-cell="ID">#{val._id.slice(-4)}</td>
         <td data-cell="ID">{val.when.date}</td>
-        <td data-cell="ID">{val.expected_resolution_time.duration}</td>
         <td data-cell="ID" style={{ color: isOverOneDay ? "red" : "green" }}>
-          {isOverOneDay ? "red" : "green"}
+          {val.expected_resolution_time.duration}
         </td>
+        <td data-cell="ID">isOverOneDay</td>
         <td data-cell="ID">{val.respondent_actions[0].respondent_action}</td>
         <td data-cell="">
           <div className={BoxCss.viewDelDiv}>
