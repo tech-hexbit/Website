043031a0b97aed8d9092fc44017fde854d6b24bc
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-16 21:30:12 +0530

    Update Box.jsx

--- src/components/Dashboard/Complaints/Box.jsx
+++ src/components/Dashboard/Complaints/Box.jsx
@@ -4,18 +4,60 @@ import React from "react";
 import BoxCss from "./Css/Box.module.css";
 import pt from "./../Payment/Css/PaymentTable.module.css";
 
-export default function Box() {
+export default function Box({ key, val }) {
   return (
-    <table className={pt.trans_table}>
-      <tr>
-        <th>Tracking ID</th>
-        <th>Customer</th>
-        <th>Date</th>
-        <th>Amount</th>
-        <th>Payment Mode</th>
-        <th>Status</th>
-        <th>Action</th>
+    <>
+      <tr key={key} className={pt.payRes}>
+        <td data-cell="ID">{val._id.slice(-4)}</td>
+        <td data-cell="ID">#{val._id.slice(-4)}</td>
+        <td data-cell="ID">{val.when.date}</td>
+        <td data-cell="ID">#{val._id.slice(-4)}</td>
+        <td data-cell="ID">#{val._id.slice(-4)}</td>
+        <td data-cell="ID">#{val._id.slice(-4)}</td>
+        <td data-cell="ID">
+          {val.issue_actions.complainant_actions[0].complainant_action ===
+          "OPEN" ? (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-badge-alert"
+                className={BoxCss.openSvg}
+              >
+                <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                <line x1="12" x2="12" y1="8" y2="12" />
+                <line x1="12" x2="12.01" y1="16" y2="16" />
+              </svg>
+            </>
+          ) : (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-check-check"
+                className={BoxCss.closeSvg}
+              >
+                <path d="M18 6 7 17l-5-5" />
+                <path d="m22 10-7.5 7.5L13 16" />
+              </svg>
+            </>
+          )}
+        </td>
       </tr>
-    </table>
+    </>
   );
 }
