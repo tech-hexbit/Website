f43e877b7e48f548ba11a18c9f98c6b2dcb715b5
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-16 22:30:03 +0530

    Update Box.jsx

--- src/components/Dashboard/Complaints/Box.jsx
+++ src/components/Dashboard/Complaints/Box.jsx
@@ -33,66 +33,66 @@ export default function Box({ key, val }) {
     setExpectedTime(expectedTimeState.toString());
   }, []);
 
-  useEffect(() => {
-    console.log(expectedTime);
-  }, [expectedTime]);
-
   return (
     <>
       <tr key={key} className={pt.payRes}>
         <td data-cell="ID">{val._id.slice(-4)}</td>
-        <td data-cell="ID">#{val._id.slice(-4)}</td>
-        <td data-cell="ID">{val.when.date}</td>
-        <td data-cell="ID" style={{ color: isOverOneDay ? "red" : "green" }}>
-          {val.expected_resolution_time.duration}
+        <td data-cell="Customer">{val.complainant_info.person.name}</td>
+        <td data-cell="Date">{val.when.date}</td>
+        <td
+          data-cell="DeadLine"
+          style={{ color: isOverOneDay ? "red" : "green" }}
+        >
+          {expectedTime}
         </td>
-        <td data-cell="ID">{expectedTime}</td>
-        <td data-cell="ID">{val.respondent_actions[0].respondent_action}</td>
-        <td data-cell="">
-          <div className={BoxCss.viewDelDiv}>
-            View Details
-            {val.issue_actions.complainant_actions[0].complainant_action ===
-            "OPEN" ? (
-              <>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="14"
-                  height="14"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-badge-alert"
-                  className={BoxCss.openSvg}
-                >
-                  <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-                  <line x1="12" x2="12" y1="8" y2="12" />
-                  <line x1="12" x2="12.01" y1="16" y2="16" />
-                </svg>
-              </>
-            ) : (
-              <>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="14"
-                  height="14"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-check-check"
-                  className={BoxCss.closeSvg}
-                >
-                  <path d="M18 6 7 17l-5-5" />
-                  <path d="m22 10-7.5 7.5L13 16" />
-                </svg>
-              </>
-            )}
-          </div>
+        <td data-cell="Status">
+          {val.respondent_actions[0].respondent_action}
+        </td>
+        <td data-cell="Action">
+          {/* <div className={BoxCss.viewDelDiv}> */}
+          View Details
+          {val.issue_actions.complainant_actions[0].complainant_action ===
+          "OPEN" ? (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-badge-alert"
+                className={BoxCss.openSvg}
+              >
+                <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                <line x1="12" x2="12" y1="8" y2="12" />
+                <line x1="12" x2="12.01" y1="16" y2="16" />
+              </svg>
+            </>
+          ) : (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-check-check"
+                className={BoxCss.closeSvg}
+              >
+                <path d="M18 6 7 17l-5-5" />
+                <path d="m22 10-7.5 7.5L13 16" />
+              </svg>
+            </>
+          )}
+          {/* </div> */}
         </td>
       </tr>
     </>
