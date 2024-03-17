90d8521c0cc80f7536344406d05f40ff3270e4c8
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-17 10:02:46 +0530

    Update Details.jsx

--- src/components/Dashboard/Complaints/Details.jsx
+++ src/components/Dashboard/Complaints/Details.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
 
+// css
+import DelCss from "./Css/Details.module.css";
+
 export default function Details({ selectedItem, closeOverlay, setLoad, load }) {
+  const [load, setLoad] = useState(false);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    console.log(selectedItem);
+    if (selectedItem.length === 0) {
+      return;
+    }
+
+    loadData();
   }, [, selectedItem]);
 
   return <div>Details</div>;
