acd12d53cd5d6793489fe951b694da8e8aa3c2bb
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-17 12:05:20 +0530

    Create UpdateInfo.jsx

---
+++ src/components/Dashboard/Complaints/UpdateInfo.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+
+// MicroInteraction
+import Load from "./../../../MicroInteraction/LoadBlack";
+import { Alert } from "./../../../MicroInteraction/Alert";
+
+// state
+import AuthContext from "./../../../store/auth-context";
+
+// axios
+import axios from "axios";
+
+// css
+import DelCss from "./Css/Details.module.css";
+
+export default function UpdateInfo() {
+  return <div>UpdateInfo</div>;
+}
