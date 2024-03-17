62fcc240a7bc3f8a4f226d686bde3844ea3faab5
Rishav Singh <rishavrajcrj@gmail.com>
2024-03-17 10:03:18 +0530

    Update Details.jsx

--- src/components/Dashboard/Complaints/Details.jsx
+++ src/components/Dashboard/Complaints/Details.jsx
@@ -4,8 +4,50 @@ import React, { useState, useEffect } from "react";
 import DelCss from "./Css/Details.module.css";
 
 export default function Details({ selectedItem, closeOverlay, setLoad, load }) {
-  const [load, setLoad] = useState(false);
   const [data, setData] = useState([]);
+  const [load, setLoad] = useState(false);
+
+  const loadData = async () => {
+    setLoad(true);
+
+    try {
+      const response = await axios.get("/api/website/Issue/get/list/full", {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+
+      if (response.data.success) {
+        setLoad(false);
+
+        console.log(response.data.issueList);
+
+        setData(response.data.issueList);
+      } else {
+        setLoad(false);
+
+        setError({
+          mainColor: "#FDEDED",
+          secondaryColor: "#F16360",
+          symbol: "error",
+          title: "Error",
+          text: "An unexpected error occurred",
+          val: true,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+
+      setLoad(false);
+
+      setError({
+        mainColor: "#FDEDED",
+        secondaryColor: "#F16360",
+        symbol: "error",
+        title: "Error",
+        text: "An unexpected error occurred",
+        val: true,
+      });
+    }
+  };
 
   useEffect(() => {
     if (selectedItem.length === 0) {
