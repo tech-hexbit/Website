35100d8777e7568bbb92f57857d0078480691754
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-14 23:32:47 +0530

    Update Alert.jsx

--- src/components/Admin/Sellers/Seller Main/Seller Components/Alert.jsx
+++ src/components/Admin/Sellers/Seller Main/Seller Components/Alert.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
 
 // axios
 import axios from "axios";
@@ -7,28 +7,21 @@ import axios from "axios";
 import AuthContext from "../../../../../store/auth-context";
 
 // Css
-import SCss from '../CSS/sellerDetail.module.css'
-import SDCss from '../CSS/selectSellerDetail.module.css';
+import SCss from "../CSS/sellerDetail.module.css";
+import SDCss from "../CSS/selectSellerDetail.module.css";
 
-function Alert({props}) {
+function Alert({ props }) {
   const [load, setLoad] = useState(false);
   const [showVer, setVer] = useState(false);
-  const [variants, setError] = useState({
-    mainColor: "",
-    secondaryColor: "",
-    symbol: "",
-    title: "",
-    text: "",
-    val: false,
-  });
 
   const authCtx = useContext(AuthContext);
+
   const saveVer = async () => {
-    console.log("done")
     let showData = {
       email: props.show.val.Email,
       state: true,
     };
+
     try {
       const response = await axios.post(
         "/api/website/admin/setVerification",
@@ -43,15 +36,12 @@ function Alert({props}) {
 
         setVer(false);
 
-        console.log("first");
-
-        setError({
+        authCtx.showAlert({
           mainColor: "#EDFEEE",
           secondaryColor: "#5CB660",
           symbol: "check_circle",
           title: "Success",
           text: "Successfully Verified",
-          val: true,
         });
 
         props.hide(false);
@@ -59,13 +49,12 @@ function Alert({props}) {
       } else {
         setLoad(false);
 
-        setError({
+        authCtx.showAlert({
           mainColor: "#FDEDED",
           secondaryColor: "#F16360",
           symbol: "error",
           title: "Error",
           text: "An unexpected error occurred",
-          val: true,
         });
       }
     } catch (error) {
@@ -73,65 +62,76 @@ function Alert({props}) {
 
       setLoad(false);
 
-      setError({
+      authCtx.showAlert({
         mainColor: "#FDEDED",
         secondaryColor: "#F16360",
         symbol: "error",
         title: "Error",
         text: "An unexpected error occurred",
-        val: true,
       });
     }
   };
   return (
-    <div className={SCss.alert} >
-    {props.show.val.accountVerified ? (
-      <>
-      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#15e506" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-check"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><path d="m9 12 2 2 4-4"/></svg>
-      <p className={SCss.ver}>VERIFIED SELLER</p>
-      </>
-
-    ) : (
-      <>
-        {showVer ? (
-          <>
-            <div class="checkbox-wrapper-3">
-              <input type="checkbox" id="cbx-3" onClick={saveVer} />
-              <label for="cbx-3" class="toggle">
-                <span></span>
-              </label>
-            </div>
-          </>
-        ) : (
-          
-          <>
+    <div className={SCss.alert}>
+      {props.show.val.accountVerified ? (
+        <>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            width="25"
+            height="25"
             viewBox="0 0 24 24"
             fill="none"
-            stroke="#d70303"
+            stroke="#15e506"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-badge-help"
-            className={SDCss.notVer}
-            onClick={() => {
-              setVer(true);
-            }}
+            class="lucide lucide-badge-check"
           >
             <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-            <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-            <line x1="12" x2="12.01" y1="17" y2="17" />
+            <path d="m9 12 2 2 4-4" />
           </svg>
-          <p className={SCss.unVer}>UNVERIFIED SELLER</p>
-          </>
-        )}
-      </>
-    )}
-  </div>
-  )
+          <p className={SCss.ver}>VERIFIED SELLER</p>
+        </>
+      ) : (
+        <>
+          {showVer ? (
+            <>
+              <div class="checkbox-wrapper-3">
+                <input type="checkbox" id="cbx-3" onClick={saveVer} />
+                <label for="cbx-3" class="toggle">
+                  <span></span>
+                </label>
+              </div>
+            </>
+          ) : (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#d70303"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-badge-help"
+                className={SDCss.notVer}
+                onClick={() => {
+                  setVer(true);
+                }}
+              >
+                <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+                <line x1="12" x2="12.01" y1="17" y2="17" />
+              </svg>
+              <p className={SCss.unVer}>UNVERIFIED SELLER</p>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Alert
+export default Alert;
