5e90b268fd20e7dfeb34c81499bbeb27cf77ff28
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-15 11:30:57 +0530

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,21 +3,27 @@ import React, { useEffect } from "react";
 // css
 import EC from "./Css/Error.module.css";
 
+// del
+import LoadingPage from "./../MicroInteraction/Loading";
+
 const Error = () => {
   return (
-    <div id={EC.notfound}>
-      <div class={EC.notfound}>
-        <div class={EC.notfound404}>
-          <h3>Oops! Page not found</h3>
-          <h1>
-            <span>4</span>
-            <span>0</span>
-            <span>4</span>
-          </h1>
-        </div>
-        <h2>we are sorry, but the page you requested was not found</h2>
-      </div>
-    </div>
+    // <div id={EC.notfound}>
+    //   <div class={EC.notfound}>
+    //     <div class={EC.notfound404}>
+    //       <h3>Oops! Page not found</h3>
+    //       <h1>
+    //         <span>4</span>
+    //         <span>0</span>
+    //         <span>4</span>
+    //       </h1>
+    //     </div>
+    //     <h2>we are sorry, but the page you requested was not found</h2>
+    //   </div>
+    // </div>
+    <>
+      <LoadingPage />
+    </>
   );
 };
 
