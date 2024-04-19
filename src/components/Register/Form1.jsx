841841be96893cd68e9372b8fe7020a3745d1964
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-19 08:07:24 +0530

    Update Form1.jsx

--- src/components/Register/Form1.jsx
+++ src/components/Register/Form1.jsx
@@ -237,7 +237,7 @@ export default function Form1(props) {
                         stroke-linecap="round"
                         stroke-linejoin="round"
                         class="lucide lucide-badge-check"
-                        className={FCss.verIcon}
+                        className={FCss.verIconPhone}
                       >
                         <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
                         <path d="m9 12 2 2 4-4" />
