d721dff103f79c01ad4272ea3af3b4631c55eff9
Rishav Singh <rishavrajcrj@gmail.com>
2024-05-04 15:19:42 +0530

    Update RET12.jsx

--- src/components/Dashboard/AddProduct/Domin/Forms/RET12.jsx
+++ src/components/Dashboard/AddProduct/Domin/Forms/RET12.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// css
+import PrCss from "./../Css/Lable.module.css";
+
 export default function RET12({ domain }) {
   return <div>RET12</div>;
 }
