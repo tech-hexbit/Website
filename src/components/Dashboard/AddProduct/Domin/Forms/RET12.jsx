2f0735a9f667d0ce77e5e1cdb04cdfce64bc2fe1
Rishav Singh <rishavrajcrj@gmail.com>
2024-05-04 17:45:37 +0530

    Update RET12.jsx

--- src/components/Dashboard/AddProduct/Domin/Forms/RET12.jsx
+++ src/components/Dashboard/AddProduct/Domin/Forms/RET12.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// data
+import inpData from "./../Json/Ret12.json";
+
 // css
 import PrCss from "./../Css/Lable.module.css";
 
 export default function RET12({ domain }) {
-  return <div>RET12</div>;
+  return (
+    <>
+      <p className={PrCss.AboutYou}>Product Details</p>
+      RET12
+    </>
+  );
 }
 
 RET12.propTypes = {
