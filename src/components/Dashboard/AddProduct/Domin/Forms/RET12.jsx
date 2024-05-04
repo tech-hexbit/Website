b4c15e55d9c8543fe0d1c913b08754e13d9acfb4
Rishav Singh <rishavrajcrj@gmail.com>
2024-05-04 15:12:11 +0530

    Update RET12.jsx

---
+++ src/components/Dashboard/AddProduct/Domin/Forms/RET12.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function RET12({ domain }) {
+  return <div>RET12</div>;
+}
+
+RET12.propTypes = {
+  domain: PropTypes.string.isRequired,
+};
