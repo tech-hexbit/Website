f9d9e95029cbd466ea97cdce3f3caa04bb2f5c74
Rishav Singh <rishavrajcrj@gmail.com>
2024-06-16 12:09:17 +0530

    Update PartialCancel.jsx

--- src/components/Dashboard/Sales/PartialCancel.jsx
+++ src/components/Dashboard/Sales/PartialCancel.jsx
@@ -218,28 +218,7 @@ export default function PartialCancel({ setReturn, rtoReturn, data }) {
           )}
 
           <>
-            <div className={RTOCss.mDiv}>
-              Return (RTO) Info
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-x"
-                className={RTOCss.closeBtn}
-                onClick={() => {
-                  setReturn(false);
-                }}
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
-            </div>
+            <div className={RTOCss.mDiv}>Return (RTO) Info</div>
 
             <div>
               <p>Select Reason Desc</p>
